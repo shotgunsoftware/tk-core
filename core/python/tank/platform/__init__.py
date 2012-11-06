3b947c08604aae8c7c05abaabf36312d7ae9f91b
manne ohrstrom <manne@shotgunsoftware.com>
2012-11-06 07:05:20 +0000

    #19351 better python2.5 support (__module__ -> __name__)

--- core/python/tank/platform/__init__.py
+++ core/python/tank/platform/__init__.py
@@ -35,17 +35,27 @@ def import_framework(framework, module):
         # get the caller's stack frame
         caller = sys._getframe(1)
         # get the package name from the caller
-        calling_package_str = caller.f_globals["__package__"]
+        # for example: 0b3d7089471e42a998027fa668adfbe4.tk_multi_about.environment_browser
+        calling_name_str = caller.f_globals["__name__"]
+        # now the module imported by Bundle.import_module is 
+        # 0b3d7089471e42a998027fa668adfbe4.tk_multi_about
+        # e.g. always the two first items in the name
+        chunks = calling_name_str.split(".")
+        calling_package_str = "%s.%s" % (chunks[0], chunks[1])
         # get the caller's module from sys.modules
         parent_module = sys.modules[calling_package_str]
     except:
-        raise Exception("import_framework could not determine the calling module layout!")
+        raise Exception("import_framework could not determine the calling module layout! " 
+                        "You can only use this method on items imported using the import_module() "
+                        "method!")
     
     # ok we got our module
     try:
         current_bundle = parent_module._tank_bundle
     except:
-        raise Exception("import_framework could not access current app/engine on calling module %s" % parent_module)
+        raise Exception("import_framework could not access current app/engine on calling module %s. "
+                        "You can only use this method on items imported using the import_module() "
+                        "method!" % parent_module)
 
     if framework not in current_bundle.frameworks:
         raise Exception("import_framework: %s does not have a framework %s associated!" % (current_bundle, framework))
