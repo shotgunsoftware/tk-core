debde3263cbd931801a68d6c17d3c011fd4ed911
manne ohrstrom <manne@shotgunsoftware.com>
2012-12-05 14:14:28 +0000

    added the create_event_log_entry signature to tank.util

--- core/python/tank/util/__init__.py
+++ core/python/tank/util/__init__.py
@@ -3,6 +3,6 @@ Copyright (c) 2012 Shotgun Software, Inc
 ----------------------------------------------------
 """
 
-from .shotgun import register_publish, find_publish
+from .shotgun import register_publish, find_publish, create_event_log_entry
 from .path import append_path_to_env_var
 from .login import get_shotgun_user
