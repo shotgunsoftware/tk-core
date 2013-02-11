7f86d2626784370b0c07ff525201aecfabcf1caa
manne ohrstrom <manne@shotgunsoftware.com>
2013-02-11 13:53:19 +0000

    #20187 added more tests

--- tests/folder_tests/test_user_sandbox.py
+++ tests/folder_tests/test_user_sandbox.py
@@ -13,6 +13,11 @@ from tank import folder
 from tank_test.tank_test_base import *
 
 
+def login_foo():
+    return "foo"
+
+def login_bar():
+    return "bar"
 
 
 class TestHumanUser(TankTestBase):
@@ -26,18 +31,29 @@ class TestHumanUser(TankTestBase):
                      "code": "shot_code",
                      "project": self.project}
 
-        cur_login = tank.util.login.get_login_name()
-        
         self.humanuser = {"type": "HumanUser",
                           "id": 2,
-                          "login": cur_login}
+                          "login": "foo"}
 
-        self.add_to_sg_mock_db([self.shot, self.humanuser])
+        self.humanuser2 = {"type": "HumanUser",
+                          "id": 4,
+                          "login": "bar"}
 
-        self.user_path = os.path.join(self.project_root, cur_login, "shot_code")
+        self.add_to_sg_mock_db([self.shot, self.humanuser, self.humanuser2])
+
+        self.user_path = os.path.join(self.project_root, "foo", "shot_code")
+        self.user_path2 = os.path.join(self.project_root, "bar", "shot_code")
+
+        self.TankCurLoginBackup = tank.util.login.get_login_name 
+
+    def tearDown(self):
+        tank.util.login.get_login_name = self.TankCurLoginBackup
 
     def test_not_made_default(self):
+        
         self.assertFalse(os.path.exists(self.user_path))
+        tank.util.login.g_shotgun_user_cache = None
+        tank.util.login.get_login_name = login_foo
 
         folder.process_filesystem_structure(self.tk, 
                                             self.shot["type"], 
@@ -50,6 +66,9 @@ class TestHumanUser(TankTestBase):
 
     def test_made_string(self):
         self.assertFalse(os.path.exists(self.user_path))
+        
+        tank.util.login.g_shotgun_user_cache = None
+        tank.util.login.get_login_name = login_foo
 
         folder.process_filesystem_structure(self.tk, 
                                             self.shot["type"], 
@@ -58,8 +77,25 @@ class TestHumanUser(TankTestBase):
                                             engine="tk-maya")
 
         self.assertTrue(os.path.exists(self.user_path))
+        
+        tank.util.login.g_shotgun_user_cache = None
+        tank.util.login.get_login_name = login_bar        
 
+        folder.process_filesystem_structure(self.tk, 
+                                            self.shot["type"], 
+                                            self.shot["id"], 
+                                            preview=False,
+                                            engine="tk-maya")
 
+        self.assertTrue(os.path.exists(self.user_path2))
+
+        # test user context
+        ctx_foo = self.tk.context_from_path(self.user_path)        
+        ctx_bar = self.tk.context_from_path(self.user_path2)
+        
+        self.assertEquals(ctx_foo.filesystem_locations, [self.user_path])
+        self.assertEquals(ctx_bar.filesystem_locations, [self.user_path2])        
+        
     def test_login_not_in_shotgun(self):
         # make sure that if there is no loncal login matching, raise
         # an error when file system folders are created
