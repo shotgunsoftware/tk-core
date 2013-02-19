1b91aff3f392843d9366933c18e870c81b15b8e7
Patrick Boucher <pboucher@patrickboucher.com>
2013-02-19 12:46:55 -0500

    #20039 - Added tank.util.prepend_path_to_env_var

--- python/tank/util/__init__.py
+++ python/tank/util/__init__.py
@@ -4,5 +4,5 @@ Copyright (c) 2012 Shotgun Software, Inc
 """
 
 from .shotgun import register_publish, find_publish, create_event_log_entry
-from .path import append_path_to_env_var
+from .path import append_path_to_env_var, prepend_path_to_env_var
 from .login import get_shotgun_user
--- python/tank/util/path.py
+++ python/tank/util/path.py
@@ -2,15 +2,36 @@
 Copyright (c) 2012 Shotgun Software, Inc
 ----------------------------------------------------
 
-Helper methods that extracts information about the current user.
+Helper methods that do path management.
 
 """
 
 import os, sys
 
+
 def append_path_to_env_var(env_var_name, path):
     """
-    Appends the path to the given environment variable.
+    Append the path to the given environment variable.
+    Creates the env var if it doesn't exist already.
+    will concatenate paths using : on linux and ; on windows
+    """
+    
+    return add_path_to_env_var(env_var_name, path, prepend=False)
+
+
+def prepend_path_to_env_var(env_var_name, path):
+    """
+    Prepend the path to the given environment variable.
+    Creates the env var if it doesn't exist already.
+    will concatenate paths using : on linux and ; on windows
+    """
+    
+    return add_path_to_env_var(env_var_name, path, prepend=True)
+
+
+def add_path_to_env_var(env_var_name, path, prepend=False):
+    """
+    Append or prepend the path to the given environment variable.
     Creates the env var if it doesn't exist already.
     will concatenate paths using : on linux and ; on windows
     """
@@ -25,6 +46,9 @@ def append_path_to_env_var(env_var_name, path):
     paths = [x for x in paths if x != ""]
     # Do not add path if it already exists in the list
     if path not in paths:
-        paths.append(path)
+        if prepend:
+            paths.insert(0, path)
+        else:
+            paths.append(path)
     # and put it back
     os.environ[env_var_name] = env_var_sep.join(paths)
