c5a277c3306e2889a486759e8dbe3a3a985c0669
manne ohrstrom <manne@shotgunsoftware.com>
2013-03-10 15:34:25 +0000

    tweaks to scripts

--- scripts/tank_cmd.py
+++ scripts/tank_cmd.py
@@ -2,24 +2,67 @@
 # Copyright (c) 2012 Shotgun Software, Inc
 # ----------------------------------------------------
 
-# Run an action from Shotgun. Expected usage:
+
+####################################################################################
+# possible modes
+# 
+# tank -h, --help              ----> help menu
+# tank core_command args       ----> core command special stuff
 #
-# run_action.py <project_root> <name of action> <entity type> <comma separated list of entity ids>
+# tank /foo/bar [params]       ----> context from path
+# tank Shot:34 [params]        ----> context from entity
+# tank Shot:foo [params]       ----> context from entity
+# tank [params]                ----> context from CWD
 #
-# For example:
-#
-# run_action.py bogus_project_root launch_maya Task 9
-# run_action.py bogus_project_root create_folders Shot 9,11,13,15
+# params are always on the form -x xyz or --xx=xyz
+
+
 
 import sys
+import os
 import logging
 import tank
 
+CORE_COMMANDS = ["setup_project", "fork", "install_core", "update_core", "join", "leave"]
+
+def show_help(log):
+    log.info("help")
+
+
+
+def run_core_command(log, command, args):
+    log.info("Core command %s %s" % (command, str(args)))
+
+def run_engine(log, context_str, args):
+    log.info("context: %s" %context_str)
+    log.info("args:    %s" % str(args))
 
 if __name__ == "__main__":    
 
-    print "hello from tank.py!"
-    print sys.argv
+    # set up logging channel for this script
+    log = logging.getLogger("tank.setup_project")
+    log.setLevel(logging.INFO)
+    
+    ch = logging.StreamHandler()
+    formatter = logging.Formatter("%(levelname)s %(message)s")
+    ch.setFormatter(formatter)
+    log.addHandler(ch)
+
+    if len(sys.argv) == 1:
+        # engine mode, shell engine, using CWD
+        run_engine(log, os.getcwd(), [])
+     
+    elif sys.argv[1] == "-h" or sys.argv[1] == "--help":
+        show_help(log)
+        
+    elif sys.argv[1] in CORE_COMMANDS:
+        run_core_command(log, sys.argv[1], sys.argv[2:])
+    
+    else:
+        # engine mode
+        run_engine(log, sys.argv[1], sys.argv[2:])
+        
+
     
     
     
--- scripts/tank_cmd.sh
+++ scripts/tank_cmd.sh
@@ -3,33 +3,19 @@
 # Copyright (c) 2012 Shotgun Software, Inc
 # ----------------------------------------------------
 
+# this script is called by the main tank script
+# the first parameter contains the path to the pipeline config root
+# additional pameters are passed into the python script
 
-# get absolute location of this script
-SELF_PATH=$(cd -P -- "$(dirname -- "$0")" && pwd -P) && SELF_PATH=$SELF_PATH/$(basename -- "$0")
- 
-# resolve symlinks
-while [ -h $SELF_PATH ]; do
-    # 1) cd to directory of the symlink
-    # 2) cd to the directory of where the symlink points
-    # 3) get the pwd
-    # 4) append the basename
-    DIR=$(dirname -- "$SELF_PATH")
-    SYM=$(readlink $SELF_PATH)
-    SELF_PATH=$(cd $DIR && cd $(dirname -- "$SYM") && pwd)/$(basename -- "$SYM")
-done
-
-# chop off the file name
-SELF_PATH=$(dirname $SELF_PATH)
-
-
+core_install_root="$1/install/core"
 
 # now add tank to the pythonpath
-export PYTHONPATH="$SELF_PATH/../python":${PYTHONPATH}
+export PYTHONPATH="$core_install_root/python":${PYTHONPATH}
 
 # now figure out which interpreter to use for Tank
 # this is stored in a config file
 curr_platform=`uname`
-interpreter_config_file="$SELF_PATH/../../../config/core/interpreter_${curr_platform}.cfg"
+interpreter_config_file="$1/config/core/interpreter_${curr_platform}.cfg"
 
 if [ ! -f "$interpreter_config_file" ];
 then
@@ -46,5 +32,8 @@ then
     exit 1
 fi
 
+# get rid of the location param @ $1, no longer needed
+shift
+
 # execute the python script which does the actual work
-exec $interpreter "$SELF_PATH/tank_cmd.py" $@
+exec $interpreter "$core_install_root/scripts/tank_cmd.py" $@
