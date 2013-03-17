3a849e13d5a35c633bda44c7c6af44489517ce8d
manne ohrstrom <manne@shotgunsoftware.com>
2013-03-17 11:49:49 +0000

    cleaned up constatns file

--- python/tank/platform/constants.py
+++ python/tank/platform/constants.py
@@ -13,8 +13,6 @@ import glob
 from tank_vendor import yaml
 from ..errors import TankError 
 
-###############################################################################################
-# bundle (apps and engines) constants
 
 # metadata file for engines and apps
 BUNDLE_METADATA_FILE = "info.yml"
@@ -50,27 +48,23 @@ BUNDLE_PYTHON_FOLDER = "python"
 # app store approvals mode
 APP_STORE_QA_MODE_ENV_VAR = "TANK_QA_ENABLED"
 
-###############################################################################################
-# engine constants
-
 # the file to look for that defines and bootstraps an engine
 ENGINE_FILE = "engine.py"
+
 # inside the engine location, the folder in which to look for apps
 ENGINE_APPS_LOCATION = "apps"
+
 # inside the engine config location, the folder in which to look for hooks
 ENGINE_HOOKS_LOCATIONS = "hooks"
+
 # app settings location
 ENGINE_APP_SETTINGS_LOCATION = "app_settings"
+
 # inside the engine config location, the folder in which to look for environments
 ENGINE_ENV_LOCATIONS = "env"
+
 # the key in the configuration for an engine which holds the environment
 ENGINE_CONFIG_ENVIRONMENT_KEY = "environment"
-
-# special shotgun engine stuff
-# possible names for shotgun engine
-SHOTGUN_ENGINES = ["tk-shotgun", "sg_shotgun", "shotgun"]
-# special shotgun environment
-SHOTGUN_ENVIRONMENT = "shotgun"
 
 # hook that is executed when a tank instance initializes.
 TANK_INIT_HOOK_NAME = "tank_init"
@@ -90,13 +84,11 @@ TANK_BUNDLE_DEFAULT_HOOK_SETTING = "default"
 # hooks that are used during folder creation.
 PROCESS_FOLDER_CREATION_HOOK_NAME = "process_folder_creation"
 
-# the name of the built in create folders hook
-CREATE_FOLDERS_CORE_HOOK_NAME = "create_folder"
+# hook to choose the environment file given a context
 PICK_ENVIRONMENT_CORE_HOOK_NAME = "pick_environment"
-PROCESS_FOLDER_NAME_HOOK_NAME = "process_folder_name"
 
-###############################################################################################
-# environment cfg constants
+# hook to decide what how folders on disk should be named
+PROCESS_FOLDER_NAME_HOOK_NAME = "process_folder_name"
 
 # the configuration key inside an environment which holds all the app configs
 ENVIRONMENT_CFG_APPS_SECTION = "apps"
@@ -104,28 +96,16 @@ ENVIRONMENT_CFG_APPS_SECTION = "apps"
 # the key that holds the app location dict
 ENVIRONMENT_LOCATION_KEY = "location"
 
-###############################################################################################
-# app constants
-
 # the file to look for that defines and bootstraps an app
 APP_FILE = "app.py"
-
-###############################################################################################
-# framework constants
 
 # the file to look for that defines and bootstraps a framework
 FRAMEWORK_FILE = "framework.py"
 
-###############################################################################################
-# core stuff
-
-CACHE_DB_FILENAME = "path_cache.db"
-
-###############################################################################################
-# folder creation (schema) stuff
-
-# ensure that shotgun fields used for folder creation contains sensible chars
+# regex pattern that all folder names must validate against
 VALID_SG_ENTITY_NAME_REGEX = "^[0-9A-Za-z_\-\.]+$"
+
+# a human readable explanation of the regex above - used in error messages
 VALID_SG_ENTITY_NAME_EXPLANATION = ("letters, numbers and the characters period(.), "
                                     "dash(-) and underscore(_)")
 
