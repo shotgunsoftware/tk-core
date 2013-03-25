15d8b3ff89b2f3e1a4e54daf87fc8699a2c384e1
manne ohrstrom <manne@shotgunsoftware.com>
2013-03-25 07:14:08 +0000

    #20778 added uncached display name lookup method

--- python/tank/util/shotgun.py
+++ python/tank/util/shotgun.py
@@ -114,6 +114,29 @@ def create_sg_app_store_connection(studio_root):
     (api_handle, script_user) = __create_sg_connection(shotgun_cfg_path, evaluate_script_user=True)
     return (api_handle, script_user)
 
+def get_entity_type_display_name(tk, entity_type_code):
+    """
+    Returns the display name for an entity type given its type name.
+    For example, if a custom entity is named "Workspace" in the 
+    Shotgun preferences, but is addressed as CustomEntity03 in the 
+    Shotgun API, this method will resolve 
+    CustomEntity03 -> Workspace.
+    
+    :param tk: tank handle
+    :param entity_type code: API entity type name
+    :returns: display name
+    """
+    # now resolve the entity types into display names using the schema_entity_read method.
+    entity_type_lookup = tk.shotgun.schema_entity_read()
+    # returns a dictionary on the following form:
+    # { 'Booking': {'name': {'editable': False, 'value': 'Booking'}}, ... }
+
+    if entity_type_code in entity_type_lookup:
+        display_name = entity_type_lookup[entity_type_code]["name"]["value"]
+    else:
+        display_name = entity_type_code
+    
+    return display_name
 
 def find_publish(tk, list_of_paths, filters=None, fields=None):
     """
