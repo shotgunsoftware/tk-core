a90f90f4c4901a1d212f9007e91fcf7f9faa15c9
manne ohrstrom <manne@shotgunsoftware.com>
2013-05-04 00:35:15 +0100

    fixed an issue with extra output after having read interpreter file.

--- scripts/tank_cmd.bat
+++ scripts/tank_cmd.bat
@@ -6,7 +6,6 @@ rem ----------------------------------------------------
 rem -- this script is called by the main tank script
 rem -- the first parameter contains the path to the pipeline config root
 rem -- additional pameters are passed into the python script
-
 rem -- now add tank to the pythonpath
 set PYTHONPATH=%1install\core\python;%PYTHONPATH%
 
@@ -16,11 +15,11 @@ set INTERPRETER_CONFIG_FILE=%1config\core\interpreter_Windows.cfg
 IF NOT EXIST "%INTERPRETER_CONFIG_FILE%" GOTO NO_INTERPRETER_CONFIG
 
 rem -- now get path to python interpreter by reading config file
-set /p INTERPRETER= < "%INTERPRETER_CONFIG_FILE%"
-IF NOT EXIST "%INTERPRETER%" GOTO NO_INTERPRETER
+for /f %%G in (%INTERPRETER_CONFIG_FILE%) do (SET PYTHON_INTERPRETER=%%G)
+IF NOT EXIST %PYTHON_INTERPRETER% GOTO NO_INTERPRETER
 
 rem -- execute the python script which does the actual work.
-%INTERPRETER% "%1install\core\scripts\tank_cmd.py" %*
+%PYTHON_INTERPRETER% "%1install\core\scripts\tank_cmd.py" %*
 
 rem -- pass along the return code
 exit /b %ERRORLEVEL%
@@ -30,5 +29,5 @@ echo "Cannot find interpreter configuration file %INTERPRETER_CONFIG_FILE%!"
 exit /b 1
 
 :NO_INTERPRETER
-echo "Could not find interpreter %INTERPRETER% specified in configuration file!"
+echo "Could not find interpreter %PYTHON_INTERPRETER% specified in configuration file!"
 exit /b 1
