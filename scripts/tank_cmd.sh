2fcb4341970e1e93a237713044a21bf48581b01e
manne ohrstrom <manne@shotgunsoftware.com>
2013-06-20 08:43:25 +0100

    merged pull request 21 - fixes and tweaks to cygwin support.

--- scripts/tank_cmd.sh
+++ scripts/tank_cmd.sh
@@ -8,25 +8,26 @@
 
 # this script is called by the main tank script
 # the first parameter contains the path to the pipeline config root
-# additional pameters are passed into the python script
+# additional parameters are passed into the python script
 
+# first set the pythonpath and get a path to the install root
 
-curr_platform=`uname`
+uname_os_str=`uname`
 
-# Pretty sloppy but takes care of allowing the tank.sh script to run via
-# git-bash as well as cygwin.
-# now add tank to the pythonpath
-if [[ "$curr_platform" == MINGW32_NT* ]];
+if [[ "$uname_os_str" == MINGW32_NT* ]];
 then
 	curr_platform="Windows"
-	core_install_root="$( pwd -W )/install/core"
+	# see http://stackoverflow.com/questions/12015348/msys-path-conversion-or-cygpath-for-msys/12063651#12063651
+	core_install_root=`sh -c "(cd $1 2</dev/null && pwd -W) || echo $1 | sed 's/\\//\\\\/g;s/^\\\\\([a-z]\)\\\\/\\1:\\\\/'"`
+	core_install_root="${core_install_root}/install/core"
 	export PYTHONPATH="$core_install_root/python;"${PYTHONPATH}
-elif [[ "$curr_platform" ==  CYGWIN_NT* ]];
+elif [[ "$uname_os_str" ==  CYGWIN_NT* ]];
 then
 	curr_platform="Windows"
 	core_install_root="$( cygpath -lw $1 )/install/core"
 	export PYTHONPATH="$core_install_root/python;"${PYTHONPATH}
 else
+    curr_platform=`uname`
 	core_install_root="$1/install/core"
 	export PYTHONPATH="$core_install_root/python":${PYTHONPATH}
 fi
@@ -44,6 +45,12 @@ fi
 # now get path to python interpreter by reading config file
 interpreter=`cat "$interpreter_config_file"`
 
+# Convert windows interpreter paths to forward-slash
+if [[ "$uname_os_str" == CYGWIN_NT* ]];
+then
+	interpreter=$( cygpath -u $interpreter )
+fi
+
 # and check that it exists...
 if [ ! -f "$interpreter" ];
 then
@@ -51,11 +58,6 @@ then
     exit 1
 fi
 
-# Cygwin wants the python path back in it's unix style :(.
-if [[ `uname` == CYGWIN_NT* ]];
-then
-	interpreter=$( cygpath -u $interpreter )
-fi
 
 # execute the python script which does the actual work
 exec $interpreter "$core_install_root/scripts/tank_cmd.py" "$@"
--- scripts/tank_cmd_login.sh
+++ scripts/tank_cmd_login.sh
@@ -9,20 +9,38 @@
 # this is useful if you are starting from a blank environment,
 # for example the java applet. Make sure to keep the two scripts in sync.
 #
-# note 2: because we need to pass an arg to bash, we cannot use env.
+# note 2: because we need to pass an arg to bash, we cannot use env
+#         for the shebang.
 
 # this script is called by the main tank script
 # the first parameter contains the path to the pipeline config root
-# additional pameters are passed into the python script
+# additional parameters are passed into the python script
 
-core_install_root="$1/install/core"
+# first set the pythonpath and get a path to the install root
 
-# now add tank to the pythonpath
-export PYTHONPATH="$core_install_root/python":${PYTHONPATH}
+uname_os_str=`uname`
+
+if [[ "$uname_os_str" == MINGW32_NT* ]];
+then
+	curr_platform="Windows"
+	# see http://stackoverflow.com/questions/12015348/msys-path-conversion-or-cygpath-for-msys/12063651#12063651
+	core_install_root=`sh -c "(cd $1 2</dev/null && pwd -W) || echo $1 | sed 's/\\//\\\\/g;s/^\\\\\([a-z]\)\\\\/\\1:\\\\/'"`
+	core_install_root="${core_install_root}/install/core"
+	export PYTHONPATH="$core_install_root/python;"${PYTHONPATH}
+elif [[ "$uname_os_str" ==  CYGWIN_NT* ]];
+then
+	curr_platform="Windows"
+	core_install_root="$( cygpath -lw $1 )/install/core"
+	export PYTHONPATH="$core_install_root/python;"${PYTHONPATH}
+else
+    curr_platform=`uname`
+	core_install_root="$1/install/core"
+	export PYTHONPATH="$core_install_root/python":${PYTHONPATH}
+fi
 
 # now figure out which interpreter to use for Tank
 # this is stored in a config file
-curr_platform=`uname`
+
 interpreter_config_file="$1/config/core/interpreter_${curr_platform}.cfg"
 
 if [ ! -f "$interpreter_config_file" ];
@@ -33,6 +51,13 @@ fi
 
 # now get path to python interpreter by reading config file
 interpreter=`cat "$interpreter_config_file"`
+
+# Convert windows interpreter paths to forward-slash
+if [[ "$uname_os_str" == CYGWIN_NT* ]];
+then
+    interpreter=$( cygpath -u $interpreter )
+fi
+
 # and check that it exists...
 if [ ! -f "$interpreter" ];
 then
