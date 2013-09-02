ad80f78fcac18216ed9f308992f0400589ba07d0
manne ohrstrom <manne@shotgunsoftware.com>
2013-09-02 14:48:34 +0200

    wip validation logic for folder creation.

--- python/tank/folder/folder_io.py
+++ python/tank/folder/folder_io.py
@@ -18,12 +18,10 @@ Known constraints:
 
 import os
 
-from tank_vendor import yaml
-
-from ..path_cache import PathCache
 from ..platform import constants
 from ..errors import TankError
 
+from ..path_cache import PathCache
     
 
 
@@ -42,30 +40,96 @@ class FolderIOReceiver(object):
         self._secondary_cache_entries = list()
         self._path_cache = PathCache(tk.pipeline_configuration)
         
+    ####################################################################################
+    # internal methods
+        
+    def _validate_folder(self, path, entity_type, entity_id, entity_name):
+        """
+        Consistency checks happening prior to folder creation. May raise a TankError
+        if an inconsistency is detected.
+        
+        :param path: The path calculated
+        :param entity_type: The entity type to associate with the path
+        :param entity_id: The entity id to associate with the path
+        :param entity_name: The name for the entity to associate with the name 
+        """
+    
+        # Check 1. make sure that there isn't already a record with the same
+        # name in the database and file system. If this is the case, and this record
+        # has since been deleted, remove it from the database and proceed with the processing.
+        # Use Case: Shot ABC is created. Folders are created. Shot is then deleted in Shotgun
+        # Shot is then recreated in Shotgun and folder creation is executed. The old ABC
+        # folder should now "link" up with the new ABC shot instead.
+        entity_in_db = self._path_cache.get_entity(path)
+        if entity_in_db is not None:
+            # found a match in the path db!
+            if entity_in_db["id"] != entity_id or entity_in_db["type"] != entity_type:
+                
+                # there is already a record in the database for this path,
+                # but associated with another entity! Display an error message
+                # and ask that the user investigates using special tank commands
+    
+                msg =  "Folder creation aborted! No folders have been created. "
+                msg += "The path '%s' cannot be processed because it is already associated " % path
+                msg += "with %s '%s' (id %s) in Shotgun. " % (entity_in_db["id"], entity_in_db["type"], entity_in_db["name"])
+                msg += "You are now trying to associate it with %s '%s' (id %s). " % (entity_type, entity_id, entity_name)
+                msg += "Please run the command 'tank check_folders %s %s' " % (entity_type, entity_id)
+                msg += "to get a more detailed overview of why you are getting this message "
+                msg += "and what you can do to resolve this situation."
+                raise TankError(msg)
+                
+        # Check 2. Check if a folder for this shot has already been created,
+        # but with another name. This can happen if someone
+        # - creates a shot AAA
+        # - creates folders on disk for Shot AAA
+        # - renamed the shot to BBB
+        # - tries to create folders. Now we don't want to create folders for BBB,
+        #   since we already have a location on disk for this shot. 
+        #
+        # note: this can also happen if the folder creation rules change.
+    
+        for p in self._path_cache.get_paths(entity_type, entity_id, primary_only=False):
+            # so we got a path that matches our entity
+            if p != path and os.path.dirname(p) == os.path.dirname(path):
+                # this path is identical to our path we are about to create except for the name. 
+                # there is still a folder on disk. Abort folder creation
+                # with a descriptive error message
+                msg = "Folder creation aborted! No folders have been created. "
+                msg += "The path '%s' cannot be created because another " % path
+                msg += "path '%s' is already associated with %s %s. " % (p, entity_type, entity_name)
+                msg += "This typically happens if an item in Shotgun is renamed or "
+                msg += "if the path naming in the folder creation configuration "
+                msg += "is changed. "
+                msg += "Please run the command 'tank check_folders %s %s' " % (entity_type, entity_id)
+                msg += "to get a more detailed overview of why you are getting this message "
+                msg += "and what you can do to resolve this situation."
+                
+                raise TankError(msg)
+    
+    
+    ####################################################################################
+    # methods to call to actually execute the folder creation logic
+        
     def execute_folder_creation(self):
         """
         Runs the actual execution. Returns a list of paths
         which were calculated to be created.
         """
         
-        # first we do some pre processing to properly cover some edge cases   
-        if not self._preview_mode:
-                 
-            for i in self._items:
+        # request that the path cache is synced against shotgun
+        self._path_cache.synchronize()        
+        
+        # do some pre processing to properly cover some edge cases   
+        for i in self._items:
 
-                if i.get("action") == "entity_folder":
-                    
-                    path = i.get("path")
-                    entity_type = i.get("entity").get("type")
-                    entity_id = i.get("entity").get("id")
-                    entity_name = i.get("entity").get("name")
-                    
-                    folder_preflight_checks(self._tk, 
-                                            self._path_cache, 
-                                            path, 
-                                            entity_type, 
-                                            entity_id, 
-                                            entity_name)
+            if i.get("action") == "entity_folder":
+                
+                path = i.get("path")
+                entity_type = i.get("entity").get("type")
+                entity_id = i.get("entity").get("id")
+                entity_name = i.get("entity").get("name")
+                
+                self._validate_folder(path, entity_type, entity_id, entity_name)
                                 
         # now request the IO operations to take place
         created_folders = self._tk.execute_hook(constants.PROCESS_FOLDER_CREATION_HOOK_NAME, 
@@ -73,21 +137,23 @@ class FolderIOReceiver(object):
                                                 preview_mode=self._preview_mode)
         
         # now handle the path cache
-        entries = []
-        
         if not self._preview_mode:    
+
+            entries = []
+            
             for i in self._items:
                 if i.get("action") == "entity_folder":
                     path = i.get("path")
                     entity = i.get("entity")
-                    entries.append( {"entity": entity, "path": path, "secondary": False} )
+                    entries.append( {"entity": entity, "path": path, "primary": True} )
                     
             for i in self._secondary_cache_entries:
                 path = i.get("path")
                 entity = i.get("entity")
-                entries.append( {"entity": entity, "path": path, "secondary": True} )
+                entries.append( {"entity": entity, "path": path, "primary": False} )
                 
-        self._path_cache.add_mappings(entries)
+            # register with path cache / shotgun
+            self._path_cache.add_mappings(entries)
 
 
         # note that for backwards compatibility, we are returning all folders, not 
@@ -144,55 +210,3 @@ class FolderIOReceiver(object):
 
 
 
-def folder_preflight_checks(tk, path_cache, path, entity_type, entity_id, entity_name):
-    """
-    Consistency checks happening prior to folder creation and ultimately
-    insertion into the path cache
-    """
-    # 1. make sure that there isn't already a record with the same
-    # name in the database and file system. If this is the case, and this record
-    # has since been deleted, remove it from the database and proceed with the processing.
-    # Use Case: Shot ABC is created. Folders are created. Shot is then deleted in Shotgun
-    # Shot is then recreated in Shotgun and folder creation is executed. The old ABC
-    # folder should now "link" up with the new ABC shot instead.
-    entity_in_db = path_cache.get_entity(path)
-    if entity_in_db is not None:
-        # found a match in the path db!
-        if entity_in_db["id"] != entity_id or entity_in_db["type"] != entity_type:
-            # there is already a record in the database for this path,
-            # but associated with another entity!
-            
-            # if that entity is retired, then delete the entry from the db
-            sg_data = tk.shotgun.find_one(entity_in_db["type"], [["id", "is", entity_in_db["id"]]])
-            if sg_data is None:
-                # could not find this in shotgun! means that it was retired.
-                # so delete it from the path cache too. This will delete all 
-                # entries in the path cache that are associated with the path.
-                path_cache.delete_path_tree(path)
-
-    # 2. make sure that we clear out any entries that are old. Basically,
-    # if someone has created a shot ABC on disk, then renamed it to ABD,
-    # then renamed the folder from /foo/ABC to /foo/ABD, we want to clear
-    # out the original entry for this record, which was pointing at /foo/ABC.
-    for p in path_cache.get_paths(entity_type, entity_id, primary_only=False):
-        # so we got a path that matches our entity
-        if p != path and os.path.dirname(p) == os.path.dirname(path):
-            # this path is identical to our path we are about to create
-            # except for the name. 
-            if not os.path.exists(p):
-                # the path in the database has been deleted from disk,
-                # delete the record from the path cache
-                path_cache.delete_path_tree(p)
-            
-            else:
-                # there is still a folder on disk. Abort folder creation
-                # with a descriptive error message
-                msg = "Folder creation aborted! No folders have been created. "
-                msg += "The path '%s' cannot be created because another " % path
-                msg += "path '%s' is already associated with %s %s. " % (p, entity_type, entity_name)
-                msg += "This typically happens if an item in Shotgun is renamed or "
-                msg += "if the path naming in the folder creation configuration "
-                msg += "is changed. To resolve this problem, either revert the name "
-                msg += "changes in Shotgun or move the folder '%s' to '%s' " % (p, path)
-                msg += "and run the folder creation again."
-                raise TankError(msg)
