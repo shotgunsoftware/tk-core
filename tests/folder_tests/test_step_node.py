66b82c3839c6bc7a4cb45b78c7a28adad12c80af
manne ohrstrom <manne@shotgunsoftware.com>
2013-09-18 23:30:15 +0100

    Squashed commit of the following:
    commit 49143fdbb323caa4a1a671872c36bab62c4da627
    Merge: 0072ed8 6e43eba
    Author: manne ohrstrom <manne@shotgunsoftware.com>
    Date:   Wed Sep 18 23:29:05 2013 +0100
    
        Merge branch 'master' into 22962_custom_step
    
    commit 0072ed863be17a46b1e9083ec0d364f12a8669b1
    Author: manne ohrstrom <manne@shotgunsoftware.com>
    Date:   Tue Sep 17 19:02:16 2013 +0100
    
        Removed stray print statement.
    
    commit 793056eb4edf24b2f98e3bc569be90289e02b5a5
    Author: manne ohrstrom <manne@shotgunsoftware.com>
    Date:   Mon Sep 16 14:02:04 2013 +0100
    
        #22962 Added unit tests.
    
    commit ad1e33129ae70b8920fd3a9e31a5ec45fad3bfac
    Author: manne ohrstrom <manne@shotgunsoftware.com>
    Date:   Mon Sep 16 13:50:10 2013 +0100
    
        #22962 Added new interface parameters and ability to customize.
    
    commit 533d122b34a88e3d69bf17a797da60e8d31edb93
    Author: manne ohrstrom <manne@shotgunsoftware.com>
    Date:   Mon Sep 16 12:05:02 2013 +0100
    
        #22962 Pass 1: Refactored existing code to allow for custom steps.

--- tests/folder_tests/test_step_node.py
+++ tests/folder_tests/test_step_node.py
@@ -502,3 +502,167 @@ class TestSchemaCreateFoldersStepAndUserSandbox(TankTestBase):
         assert_paths_to_create(expected_paths)
                                 
                                 
+# test against a step node where create_with_parent is false
+# and we are using a custom entity for step
+
+class TestSchemaCreateFoldersCustomStep(TankTestBase):
+    def setUp(self):
+        """Sets up entities in mocked shotgun database and creates Mock objects
+        to pass in as callbacks to Schema.create_folders. The mock objects are
+        then queried to see what paths the code attempted to create.
+        """
+        super(TestSchemaCreateFoldersCustomStep, self).setUp()
+        self.setup_fixtures("shotgun_single_custom_step_core")
+        self.seq = {"type": "Sequence",
+                    "id": 2,
+                    "code": "seq_code",
+                    "project": self.project}
+        self.shot = {"type": "Shot",
+                     "id": 1,
+                     "code": "shot_code",
+                     "sg_sequence": self.seq,
+                     "project": self.project}
+        self.step = {"type": "FooStep",
+                     "id": 3,
+                     "code": "step_code",
+                     "short_name": "step_short_name"}
+
+        self.step2 = {"type": "FooStep",
+                     "id": 33,
+                     "code": "step_code_2",
+                     "short_name": "step_short_name_2"}
+        
+        self.asset = {"type": "Asset",
+                    "id": 4,
+                    "sg_asset_type": "assettype",
+                    "code": "assetname",
+                    "project": self.project}
+        
+        self.task = {"type": "Task",
+                     "id": 23,
+                     "entity": self.shot,
+                     "step": None,
+                     "alt_step_link": self.step,
+                     "project": self.project}
+
+        self.task2 = {"type": "Task",
+                     "id": 25,
+                     "entity": self.shot,
+                     "step": None,
+                     "alt_step_link": self.step2,
+                     "project": self.project}
+
+        entities = [self.shot, 
+                    self.seq, 
+                    self.step,
+                    self.step2, 
+                    self.project, 
+                    self.asset, 
+                    self.task,
+                    self.task2]
+
+        # Add these to mocked shotgun
+        self.add_to_sg_mock_db(entities)
+
+        self.tk = tank.Tank(self.project_root)
+
+        # add mock schema data so that a list of the asset type enum values can be returned
+        data = {}
+        data["properties"] = {}
+        data["properties"]["valid_values"] = {}
+        data["properties"]["valid_values"]["value"] = ["assettype"]
+        data["data_type"] = {}
+        data["data_type"]["value"] = "list"        
+        self.add_to_sg_schema_db("Asset", "sg_asset_type", data)
+
+        self.schema_location = os.path.join(self.project_root, "tank", "config", "core", "schema")
+
+        self.FolderIOReceiverBackup = folder.folder_io.FolderIOReceiver.execute_folder_creation
+        folder.folder_io.FolderIOReceiver.execute_folder_creation = execute_folder_creation_proxy
+
+    def tearDown(self):
+        
+        folder.folder_io.FolderIOReceiver.execute_folder_creation = self.FolderIOReceiverBackup
+
+
+    def test_shot(self):
+        """Tests paths used in making a shot are as expected."""
+        
+        expected_paths = []
+        
+        sequence_path = os.path.join(self.project_root, "sequences", self.seq["code"])
+        sequences_path = os.path.join(self.project_root, "sequences")        
+        shot_path = os.path.join(sequence_path, self.shot["code"])
+
+        expected_paths.extend( [self.project_root, sequences_path, sequence_path, shot_path] )
+
+        folder.process_filesystem_structure(self.tk, 
+                                            self.shot["type"], 
+                                            self.shot["id"], 
+                                            preview=False,
+                                            engine=None)        
+        
+        assert_paths_to_create(expected_paths)
+
+
+    def test_step_a(self):
+        """Tests paths used in making a shot are as expected."""
+
+        folder.process_filesystem_structure(self.tk, 
+                                            self.task["type"], 
+                                            self.task ["id"], 
+                                            preview=False,
+                                            engine=None)        
+        
+        expected_paths = []
+
+        sequence_path = os.path.join(self.project_root, "sequences", self.seq["code"])   
+        sequences_path = os.path.join(self.project_root, "sequences")     
+        shot_path = os.path.join(sequence_path, self.shot["code"])
+        step_path = os.path.join(shot_path, self.step["short_name"])
+        
+        expected_paths.extend( [self.project_root, sequences_path, sequence_path, shot_path, step_path] )
+        
+        # add non-entity paths
+        expected_paths.append(os.path.join(step_path, "publish"))
+        expected_paths.append(os.path.join(step_path, "images"))
+        expected_paths.append(os.path.join(step_path, "review"))
+        expected_paths.append(os.path.join(step_path, "work"))
+        expected_paths.append(os.path.join(step_path, "work", "snapshots"))
+        expected_paths.append(os.path.join(step_path, "work", "workspace.mel"))
+        expected_paths.append(os.path.join(step_path, "out"))
+
+        assert_paths_to_create(expected_paths)
+                                
+                                
+    def test_step_b(self):
+        """Tests paths used in making a shot are as expected."""
+
+        folder.process_filesystem_structure(self.tk, 
+                                            self.task2["type"], 
+                                            self.task2["id"], 
+                                            preview=False,
+                                            engine=None)        
+        
+        expected_paths = []
+
+        sequence_path = os.path.join(self.project_root, "sequences", self.seq["code"])     
+        sequences_path = os.path.join(self.project_root, "sequences")   
+        shot_path = os.path.join(sequence_path, self.shot["code"])
+        step_path = os.path.join(shot_path, self.step2["short_name"])
+        
+        expected_paths.extend( [self.project_root, sequences_path, sequence_path, shot_path, step_path] )
+        
+        # add non-entity paths
+        expected_paths.append(os.path.join(step_path, "publish"))
+        expected_paths.append(os.path.join(step_path, "images"))
+        expected_paths.append(os.path.join(step_path, "review"))
+        expected_paths.append(os.path.join(step_path, "work"))
+        expected_paths.append(os.path.join(step_path, "work", "snapshots"))
+        expected_paths.append(os.path.join(step_path, "work", "workspace.mel"))
+        expected_paths.append(os.path.join(step_path, "out"))
+
+        assert_paths_to_create(expected_paths)
+                                
+                                
+
