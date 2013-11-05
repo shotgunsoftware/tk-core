a7304c0bb33210eb79bbcd9d1bc33d8c22628063
manne ohrstrom <manne@shotgunsoftware.com>
2013-11-05 15:24:18 +0000

    #23382 added sgtk proxy

---
+++ setup/tank_api_proxy/sgtk/__init__.py
@@ -0,0 +1,49 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+
+# thin proxy wrapper which finds the real sgtk and replaces itself with that
+
+import os
+import sys
+
+
+# first look for our parent file
+current_folder = os.path.abspath(os.path.dirname(__file__))
+parent_file_name =  "core_%s.cfg" % sys.platform
+parent_cfg_path = os.path.join(current_folder, "..", "..", parent_file_name)
+parent_cfg_path = os.path.abspath(parent_cfg_path)
+
+if not os.path.exists(parent_cfg_path):
+    raise Exception("Sgtk: Cannot find referenced core configuration file '%s'!" % parent_cfg_path)
+
+# now read our parent file
+fh = open(parent_cfg_path, "rt")
+try:
+    parent_path = fh.readline()
+finally:
+    fh.close()
+
+parent_python_path = os.path.join(parent_path, "install", "core", "python") 
+
+if not os.path.exists(parent_python_path):
+    raise Exception("Sgtk: Cannot find referenced core location '%s'" % parent_python_path)
+
+# set up an env var to track the current pipeline configuration
+# this is to help the tank core API figure out for example tank.tank_from_path()
+# when using multiple work dev areas.
+os.environ["TANK_CURRENT_PC"] = current_folder
+
+# ok we got the parent location
+# prepend this to the python path and reload the module
+# this way we will load the 'real' tank! 
+os.sys.path.insert(0, parent_python_path)
+reload(sys.modules["sgtk"])
+
