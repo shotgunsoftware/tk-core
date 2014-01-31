f995778c160f9f9a2cb8612a8fde9ff5bd2a74ee
manne ohrstrom <manne@shotgunsoftware.com>
2014-01-31 20:07:19 +0000

    #21766 Added filter_by alpha to template key definitions. It is now possible to define a template key which only accepts alpha rather than alphanumeric. This is be useful in some naming convention situations.

--- python/tank/templatekey.py
+++ python/tank/templatekey.py
@@ -161,8 +161,8 @@ class StringKey(TemplateKey):
         :param name: Name by which the key will be refered.
         :param default: Default value for the key.
         :param choices: List of possible values for this key.
-        :parma filter_by: Name of filter type to limit values for string. Currently
-                          only accepted values are 'alphanumeric' and None.
+        :param filter_by: Name of filter type to limit values for string. Currently
+                          only accepted values are 'alphanumeric', 'alpha' and None.
         :param shotgun_entity_type: For keys directly linked to a shotgun field, the entity type.
         :param shotgun_field_name: For keys directly linked to a shotgun field, the field name.
         :param exclusions: List of forbidden values.
@@ -170,10 +170,19 @@ class StringKey(TemplateKey):
         :param length: int, should this key be fixed length
         """
         self.filter_by = filter_by
+
+        # Build regexes for alpha and alphanumeric filter_by clauses
+        #
+        # Note that we cannot use a traditional [^a-zA-Z0-9] regex since we want
+        # to support unicode and not just ascii. \W covers "Non-word characters",
+        # which is basically the international equivalent of 7-bit ascii 
+        #        
         if self.filter_by == "alphanumeric":
-            # build regex to search for all non-alphanumeric 
-            # characters or undeerscores
-            self._filter_regex_u = re.compile(u"[\W_]", re.UNICODE)#[^a-zA-Z0-9]")
+            self._filter_regex_u = re.compile(u"[\W_]", re.UNICODE)
+        
+        elif self.filter_by == "alpha":
+            self._filter_regex_u = re.compile(u"[\W_0-9]", re.UNICODE)
+        
         else: 
             self._filter_regex_u = None
 
