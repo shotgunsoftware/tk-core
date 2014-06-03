25b5d8c8ba9d91802c85a3296d050d08998357f5
Alan Dann <alan.dann@shotgunsoftware.com>
2014-06-03 14:47:24 +0100

    For #25587 - Spaces in Windows PC & Python paths are now supported. Spaces are now supported in paths for Pipeline Configurations on Windows when using the latest version of the tank.bat file.  Additionally, Python can now be installed into a path with spaces in (e.g. C:\Program Files) if needed. Closes #83.

--- scripts/tank_cmd.bat
+++ scripts/tank_cmd.bat
@@ -14,21 +14,28 @@ setlocal EnableExtensions
 
 rem -- this script is called by the main tank script
 rem -- the first parameter contains the path to the pipeline config root
-rem -- additional pameters are passed into the python script
+rem -- additional parameters are passed into the python script
+
+rem -- grab the pipeline configuration root and remove any trailing slash
+set PC_ROOT=%~1
+IF %PC_ROOT:~-1%==\ SET PC_ROOT=%PC_ROOT:~0,-1%
+
 rem -- now add tank to the pythonpath
-set PYTHONPATH=%1install\core\python;%PYTHONPATH%
+set PYTHONPATH=%PC_ROOT%\install\core\python;%PYTHONPATH%
 
 rem -- now figure out which interpreter to use for Tank
 rem -- this is stored in a config file
-set INTERPRETER_CONFIG_FILE=%1config\core\interpreter_Windows.cfg
+set INTERPRETER_CONFIG_FILE=%PC_ROOT%\config\core\interpreter_Windows.cfg
 IF NOT EXIST "%INTERPRETER_CONFIG_FILE%" GOTO NO_INTERPRETER_CONFIG
 
 rem -- now get path to python interpreter by reading config file
-for /f "tokens=*" %%G in (%INTERPRETER_CONFIG_FILE%) do (SET PYTHON_INTERPRETER=%%G)
-IF NOT EXIST %PYTHON_INTERPRETER% GOTO NO_INTERPRETER
+rem -- 'usebackq' is used to allow quoting of the path which could potentially contain spaces
+rem -- 'tokens=*' is used to ensure each line of the file doesn't get split on whitespace
+for /f "usebackq tokens=*" %%G in ("%INTERPRETER_CONFIG_FILE%") do (SET PYTHON_INTERPRETER=%%G)
+IF NOT EXIST "%PYTHON_INTERPRETER%" GOTO NO_INTERPRETER
 
 rem -- execute the python script which does the actual work.
-%PYTHON_INTERPRETER% "%1install\core\scripts\tank_cmd.py" %*
+"%PYTHON_INTERPRETER%" "%PC_ROOT%\install\core\scripts\tank_cmd.py" %*
 
 rem -- pass along the return code
 exit /b %ERRORLEVEL%
