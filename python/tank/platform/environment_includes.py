7967db0cac639d79be5123c538e7a51c727adbbb
Manne Öhrström <manne@shotgunsoftware.com>
2014-06-14 12:37:48 +0100

    #25657 include statement now supportes environment variable expansion. This allows paths declared via the include or includes statements in the templates.yml or environment files to contain environment variables.

--- python/tank/platform/environment_includes.py
+++ python/tank/platform/environment_includes.py
@@ -97,8 +97,9 @@ def _resolve_includes(file_name, data, context):
                 # skip - these paths are optional always
                 continue       
         
-        elif "/" in include and not include.startswith("/"):
-            # relative path!
+        elif "/" in include and not include.startswith("/") and not include.startswith("$"):
+            # relative path: foo/bar.yml or ./foo.bar.yml
+            # note the $ check to avoid paths beginning with env vars to fall into this branch
             adjusted = include.replace("/", os.path.sep)
             full_path = os.path.join(os.path.dirname(file_name), adjusted)
             # make sure that the paths all exist
@@ -111,7 +112,7 @@ def _resolve_includes(file_name, data, context):
             if sys.platform != "win32":
                 # ignore this on other platforms
                 continue
-            full_path = include
+            full_path = os.path.expandvars(include)
             # make sure that the paths all exist
             if not os.path.exists(full_path):
                 raise TankError("Include Resolve error in %s: Included path %s "
@@ -122,7 +123,7 @@ def _resolve_includes(file_name, data, context):
             if sys.platform == "win32":
                 # ignore this on other platforms
                 continue
-            full_path = include                    
+            full_path = os.path.expandvars(include)                    
             # make sure that the paths all exist
             if not os.path.exists(full_path):
                 raise TankError("Include Resolve error in %s: Included path %s "
