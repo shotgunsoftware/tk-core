ea534c1e6f2cdafe66be3cfd031113c1b30e2d19
Alan Dann <alan.dann@shotgunsoftware.com>
2015-03-13 12:57:21 +0000

    For #28647 - Added Yaml caching and context.from_entities() method - Added a method to allow construction of a context from a pre-determined list of entities.  This avoids the need to perform a Shotgun query when the entities are already known and there are no additional entities specified by the additional_context_entities hook - Added low-level caching of the yaml environment files to avoid them being unnecessarily parsed from disk.

--- python/tank/api.py
+++ python/tank/api.py
@@ -520,6 +520,25 @@ class Tank(object):
         """
         return context.from_entity(self, entity_type, entity_id)
 
+    def context_from_entities(self, project=None, entity=None, step=None, task=None, user=None):
+        """
+        Derive a context from a set of known Shotgun entities.
+        
+        Note: It is the responsibility of the calling code to ensure that both the entities and the
+        relationships between them are valid.  
+        
+        This only results in a Shotgun query if context_additional_entities are specified in the
+        configuration!        
+        
+        :param project: The project entity dictionary to use when constructing the context
+        :param entity:  The entity entity dictionary to use when constructing the context
+        :param step:    The step entity dictionary to use when constructing the context
+        :param task:    The task entity dictionary to use when constructing the context
+        :param user:    The user entity dictionary to use when constructing the context
+        :returns:       A Context constructed from the specified entities.        
+        """
+        return context.from_entities(self, project, entity, step, task, user)
+
     def synchronize_filesystem_structure(self, full_sync=False):
         """
         Ensures that the filesystem structure on this machine is in sync
