cb78c1b5fa9367899246fc161332d0ada9aa9f16
Manne Öhrström <manne@shotgunsoftware.com>
2015-09-28 09:12:45 +0100

    Cleared branch in preparation for docs.

--- python/tank_vendor/shotgun_api3/__init__.py
+++
@@ -1,5 +0,0 @@
-from shotgun import (Shotgun, ShotgunError, ShotgunFileDownloadError, Fault, 
-                     AuthenticationFault, MissingTwoFactorAuthenticationFault,
-                     ProtocolError, ResponseError, Error, __version__)
-from shotgun import SG_TIMEZONE as sg_timezone
-
--- tests/run_tests.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-
-python %~dp0\run_tests.py %*
