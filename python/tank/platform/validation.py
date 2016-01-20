shotgunsoftware/tk-core

commit 0faf0bd7773cb4483666f3e30a51475bf4a83674

#34166 fixed bugs with context validation
Fixes a bug preventing the required_context directive in info.yml to work correctly. Previously the
validation would always fail.

Closes #222

python/tank/platform/validation.py
@@ -48,18 +48,20 @@ def validate_context(descriptor, context):
     will work with it. Raises a tankerror if not.
     """
     # check that the context contains all the info that the app needs
-    context_check_ok = True
+    # this returns list of strings, e.g. ["user", "entity"]
     req_ctx = descriptor.get_required_context()
-    for req_ctx_item in req_ctx:
-        context_check_ok &= (req_ctx_item == "user" and context.user is None)
-        context_check_ok &= (req_ctx_item == "entity" and context.entity is None)
-        context_check_ok &= (req_ctx_item == "project" and context.project is None)
-        context_check_ok &= (req_ctx_item == "step" and context.step is None)
-        context_check_ok &= (req_ctx_item == "task" and context.task is None)
+
+    context_check_ok = True
+    for context_attr_name in ["user", "entity", "project", "step", "task"]:
+        # get context attribute
+        ctx_attr = getattr(context, context_attr_name)
+        if context_attr_name in req_ctx and ctx_attr is None:
+            context_check_ok = False
+
     if not context_check_ok:
         raise TankError("The item requires the following "
                         "items in the context: %s. The current context is missing one "
-                        "or more of these items: %s" % (req_ctx, context) )
+                        "or more of these items: %r" % (req_ctx, context))
     
 def validate_platform(descriptor):
     """

tests/fixtures/config/bundles/test_app/info.yml
@@ -148,3 +148,4 @@ configuration:
 # the Shotgun fields that this app needs in order to operate correctly
 requires_shotgun_fields:
 
+required_context: [project, entity]

commit d7ead989ee4ebd5e8b4c7c23baabd2f9f9e65916

#34420 Fixes a regression in the context validation.
This fixes a regression that was introduced in
0faf0bd7773cb4483666f3e30a51475bf4a83674. The regression forced the
context to evaluate the context.user property which in turn caused
the context to connect to Shotgun to run a lookup. This is not
supported in all states where Toolkit is executing so when the
actions where being generated by the Shotgun engine, errors would
appear due to the user evaluation happening. Evaluating user also has
performance implications. The new code does lazy evaluation and only
checks the context when a constraint has been defined in a manifest.

Closes #228

python/tank/platform/validation.py
@@ -52,17 +52,22 @@ def validate_context(descriptor, context):
     req_ctx = descriptor.get_required_context()
 
     context_check_ok = True
-    for context_attr_name in ["user", "entity", "project", "step", "task"]:
-        # get context attribute
-        ctx_attr = getattr(context, context_attr_name)
-        if context_attr_name in req_ctx and ctx_attr is None:
-            context_check_ok = False
+    if "user" in req_ctx and context.user is None:
+        context_check_ok = False
+    if "entity" in req_ctx and context.entity is None:
+        context_check_ok = False
+    if "project" in req_ctx and context.project is None:
+        context_check_ok = False
+    if "step" in req_ctx and context.step is None:
+        context_check_ok = False
+    if "task" in req_ctx and context.task is None:
+        context_check_ok = False
 
     if not context_check_ok:
         raise TankError("The item requires the following "
                         "items in the context: %s. The current context is missing one "
                         "or more of these items: %r" % (req_ctx, context))
-    
+
 def validate_platform(descriptor):
     """
     Validates that the given bundle is compatible with the 