ca40564fe2d9f71d04ac81ef3888ff94ee7e8d7d
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2016-01-27 16:17:16 -0500

    Added a core hook 'log_metrics' for internal analytics
    * currently accepts N metrics in anticipation of batching
    * minor tweak to metrics logger
    * still very much WIP

---
+++ hooks/log_metrics.py
@@ -0,0 +1,27 @@
+# Copyright (c) 2016 Shotgun Software Inc.
+# 
+# CONFIDENTIAL AND PROPRIETARY
+# 
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your 
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+"""Hook that gets executed every time Toolkit logs a user metric."""
+
+from tank import Hook
+ 
+
+class LogMetrics(Hook):
+    
+    def execute(self, tk, metrics):
+        """Called when Toolkit logs a user metric.
+        
+        :param tk: Toolkit instance 
+        :param list metrics: list of `ToolkitMetric` objects that were logged.
+        
+        """
+        pass
+
+
