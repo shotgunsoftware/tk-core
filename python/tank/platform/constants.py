fd8d0f8da40127daddd5d77b6a28d81c7396cd09
Manne Öhrström <manne@shotgunsoftware.com>
2016-02-29 22:38:25 +0000

    removed unused constant.

--- python/tank/platform/constants.py
+++ python/tank/platform/constants.py
@@ -51,13 +51,11 @@ BUNDLE_PYTHON_FOLDER = "python"
 # app store approvals mode
 APP_STORE_QA_MODE_ENV_VAR = "TANK_QA_ENABLED"
 
-# use new experimental yaml parser
-PRESERVE_YAML_ENV_VAR = "TK_PRESERVE_YAML"
 # force use old, non-structure preseving parser
 USE_PYYAML_ENV_VAR = "TK_USE_PYYAML"
+
 # flag to pass for legacy yaml parser
 LEGACY_YAML_PARSER_FLAG = "--use-pyyaml"
-
 
 # the file to look for that defines and bootstraps an engine
 ENGINE_FILE = "engine.py"
