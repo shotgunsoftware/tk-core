5f34e1083ee3bcbe7f475d306d39ce7addb10fab
Manne Öhrström <manne@shotgunsoftware.com>
2016-04-28 13:41:46 +0100

    updated deploy tests.

---
+++ tests/descriptor_tests/test_descriptors.py
@@ -0,0 +1,145 @@
+# Copyright (c) 2013 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+import os
+import sgtk
+
+from tank_test.tank_test_base import *
+from tank.errors import TankError
+
+class TestDescriptorSupport(TankTestBase):
+
+    def setUp(self, parameters=None):
+
+        super(TestDescriptorSupport, self).setUp()
+
+        self.install_root = os.path.join(
+            self.tk.pipeline_configuration.get_install_location(),
+            "install"
+        )
+
+    def _create_info_yaml(self, path):
+        """
+        create a mock info.yml
+        """
+        sgtk.util.filesystem.ensure_folder_exists(path)
+        fh = open(os.path.join(path, "info.yml"), "wt")
+        fh.write("foo")
+        fh.close()
+
+    def test_app_store_descriptor_location(self):
+        """
+        Tests an appstore descriptor bundle path for the given bundle type and location.
+        """
+
+        location = {"type": "app_store", "version": "v0.1.2", "name": "tk-bundle"}
+        path = os.path.join(self.install_root, "app_store", "tk-bundle", "v0.1.2")
+        self._create_info_yaml(path)
+
+        d = self.tk.pipeline_configuration.get_app_descriptor(location)
+        self.assertEqual(d.get_path(), path)
+
+        d = self.tk.pipeline_configuration.get_engine_descriptor(location)
+        self.assertEqual(d.get_path(), path)
+
+        d = self.tk.pipeline_configuration.get_framework_descriptor(location)
+        self.assertEqual(d.get_path(), path)
+
+
+    def test_manual_descriptor_location(self):
+        """
+        Tests a manual descriptor bundle path for the given bundle type and location.
+        """
+
+        location = {"type": "manual", "version": "v0.1.2", "name": "tk-bundle"}
+        path = os.path.join(self.install_root, "manual", "tk-bundle", "v0.1.2")
+        self._create_info_yaml(path)
+
+        d = self.tk.pipeline_configuration.get_app_descriptor(location)
+        self.assertEqual(d.get_path(), path)
+
+        d = self.tk.pipeline_configuration.get_engine_descriptor(location)
+        self.assertEqual(d.get_path(), path)
+
+        d = self.tk.pipeline_configuration.get_framework_descriptor(location)
+        self.assertEqual(d.get_path(), path)
+
+
+    def test_dev_descriptor_location(self):
+        """
+        Tests a dev descriptor bundle path
+        """
+        path = os.path.join(self.tk.pipeline_configuration.get_path(), "bundle")
+        self._create_info_yaml(path)
+
+        d = self.tk.pipeline_configuration.get_app_descriptor({"type": "dev", "path": "{PIPELINE_CONFIG}/bundle"})
+        self.assertEqual(d.get_path(), path)
+
+        d = self.tk.pipeline_configuration.get_app_descriptor({"type": "dev", "path": path})
+        self.assertEqual(d.get_path(), path)
+
+
+    def _test_git_descriptor_location_with_repo(self, repo):
+        """
+        Tests a git descriptor bundle path for the given bundle type and location and a given
+        repo.
+        """
+        path = os.path.join(self.install_root, "git", os.path.basename(repo), "v0.1.2")
+        self._create_info_yaml(path)
+
+        d = self.tk.pipeline_configuration.get_app_descriptor({"type": "git", "path": repo, "version": "v0.1.2"})
+        self.assertEqual(d.get_path(), path)
+
+    def test_git_descriptor_location(self):
+        """
+        Tests a git descriptor bundle path for the given bundle type and location for all
+        supported repo naming convention.
+
+        :param bundle_type: One of descriptor.AppDescriptor.{APP,ENGINE,FRAMEWORK}
+        :param bundle_location: Location in the pipeline configuration where bundles of the given
+            type get installed.
+        """
+        for uri in [
+            "git@github.com:manneohrstrom/tk-hiero-publish.git",
+            "https://github.com/manneohrstrom/tk-hiero-publish.git",
+            "git://github.com/manneohrstrom/tk-hiero-publish.git",
+            "/full/path/to/local/repo.git"
+        ]:
+            self._test_git_descriptor_location_with_repo(uri)
+
+
+    def test_git_version_logic(self):
+        """
+        Test git descriptor version logic
+        """
+        desc = self.tk.pipeline_configuration.get_app_descriptor(
+                {"type": "git", "path": "git@github.com:dummy/tk-multi-dummy.git", "version": "v1.2.3"}
+        )
+
+        # absolute match
+        self.assertEqual(desc._io_descriptor._find_latest_tag_by_pattern(["v1.2.3"], "v1.2.3"), "v1.2.3")
+        self.assertEqual(desc._io_descriptor._find_latest_tag_by_pattern(["v1.2.3", "v1.2.2"], "v1.2.3"), "v1.2.3")
+
+        # simple matches
+        self.assertEqual(desc._io_descriptor._find_latest_tag_by_pattern(["v1.2.3", "v1.2.2"], "v1.2.x"), "v1.2.3")
+        self.assertEqual(desc._io_descriptor._find_latest_tag_by_pattern(["v1.2.3", "v1.2.2"], "v1.2.x"), "v1.2.3")
+        self.assertEqual(desc._io_descriptor._find_latest_tag_by_pattern(["v1.2.3", "v1.2.233", "v1.3.1"], "v1.3.x"), "v1.3.1")
+        self.assertEqual(desc._io_descriptor._find_latest_tag_by_pattern(["v1.2.3", "v1.2.233", "v1.3.1", "v2.3.1"], "v1.x.x"), "v1.3.1")
+
+        # forks
+        self.assertEqual(desc._io_descriptor._find_latest_tag_by_pattern(["v1.2.3", "v1.2.233", "v1.3.1.2.3"], "v1.3.x"), "v1.3.1.2.3")
+        self.assertEqual(desc._io_descriptor._find_latest_tag_by_pattern(["v1.2.3", "v1.2.233", "v1.3.1.2.3", "v1.4.233"], "v1.3.1.x"), "v1.3.1.2.3")
+
+        # invalids
+        self.assertRaisesRegexp(TankError,
+                                "Incorrect version pattern '.*'. There should be no digit after a 'x'",
+                                desc._io_descriptor._find_latest_tag_by_pattern,
+                                ["v1.2.3", "v1.2.233", "v1.3.1"],
+                                "v1.x.2")
