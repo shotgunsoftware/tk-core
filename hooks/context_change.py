8c035318495775633c1de30534eaa855c8548117
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2016-05-11 08:34:09 -0400

    last round of fixes for doc

--- hooks/context_change.py
+++ hooks/context_change.py
@@ -17,22 +17,34 @@ from tank import get_hook_baseclass
 
 class ContextChangeHook(get_hook_baseclass()):
     """
-    Hook that gets executed every single time there is a context change in Toolkit,
-    e.g. when an engine starts or restarts and when sgtk.platform.change_context
-    is invoked.
+    Hook that gets executed every single time there is a context change in Toolkit.
 
-    .. note:
-        If the engine is restarted without changing context or sgtk.platform.change_context
-        is invoked with the same context, the hook will be executed. To detect
-        such situations, simple compare the two contexes.
+        - If an engine **starts up**, the ``current_context`` passed to the hook
+        methods will be ``None`` and the ``next_context`` parameter will be set
+        to the context that the engine is starting in.
+
+        - If an engine is being **reloaded**, in the context of an engine restart
+        for example, the ``current_context`` and ``next_context`` will usually be
+        the same.
+
+        - If a **context switch** is requested, for example when a user switches
+        from project to shot mode in Nuke Studio, ``current_context`` and ``next_context``
+        will contain two different context.
+
+    .. note::
+
+       These hooks are called whenever the context is being set in Toolkit. It is
+       possible that the new context will be the same as the old context. If
+       you want to trigger some behaviour only when the new one is different
+       from the old one, you'll need to compare the two arguments.
     """
 
     def pre_context_change(self, current_context, next_context):
         """
         Called before the context has changed.
 
-        :param current_context: The current context.
-        :param next_context: The context being switching to.
+        :param current_context: The context of the engine.
+        :param next_context: The context the engine is switching to.
         """
         pass
 
@@ -40,7 +52,7 @@ class ContextChangeHook(get_hook_baseclass()):
         """
         Called after the context has changed.
 
-        :param previous_context: The previous current context.
-        :param current_context: The new current context.
+        :param previous_context: The previous context of the engine.
+        :param current_context: The current context of the engine.
         """
         pass
