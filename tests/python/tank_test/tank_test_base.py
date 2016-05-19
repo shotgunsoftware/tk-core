c02ab211deb3a0354b94df70fa7bb04687b30193
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2016-05-19 15:24:33 -0400

    mocking methods and restoring the original wasn't done correctly and was causing side-effects in other tests when some tests fail spectacularly during setUp

--- tests/python/tank_test/tank_test_base.py
+++ tests/python/tank_test/tank_test_base.py
@@ -23,6 +23,7 @@ import tempfile
 from tank_vendor.shotgun_api3.lib import mockgun
 
 import unittest2 as unittest
+import mock
 
 import sgtk
 import tank
@@ -180,24 +181,20 @@ class TankTestBase(unittest.TestCase):
         self.tank_temp = TANK_TEMP
         self.init_cache_location = os.path.join(self.tank_temp, "init_cache.cache")
 
-
         self.cache_root = os.path.join(self.tank_temp, "cache_root")
 
-        def _get_cache_location_mock():
-            return self.init_cache_location
-
-        self._original_get_cache_location = tank.pipelineconfig_factory._get_cache_location
-        tank.pipelineconfig_factory._get_cache_location = _get_cache_location_mock
+        patch = mock.patch("tank.pipelineconfig_factory._get_cache_location", return_value=self.init_cache_location)
+        patch.start()
+        self.addCleanup(patch.stop)
 
         # Mock this so that authentication manager works even tough we are not in a config.
         # If we don't mock it than the path cache calling get_current_user will fail.
-        def _get_associated_sg_config_data_mock():
-             return {
-                "host": "https://somewhere.shotguntudio.com"
-             }
-
-        self._original_get_associated_sg_config_data = tank.util.shotgun.get_associated_sg_config_data
-        tank.util.shotgun.get_associated_sg_config_data = _get_associated_sg_config_data_mock
+        patch = mock.patch(
+            "tank.util.shotgun.get_associated_sg_config_data",
+            return_value={"host": "https://somewhere.shotguntudio.com"}
+        )
+        patch.start()
+        self.addCleanup(patch.stop)
 
         # define entity for test project
         self.project = {"type": "Project",
@@ -264,19 +261,14 @@ class TankTestBase(unittest.TestCase):
         # fake a version response from the server
         self.mockgun.server_info = {"version": (7, 0, 0)}
 
-        
-        def get_associated_sg_base_url_mocker():
-            return "http://unit_test_mock_sg"
-        
-        def create_sg_connection_mocker():
-            return self.mockgun
-            
-        self._original_get_associated_sg_base_url = tank.util.shotgun.get_associated_sg_base_url
-        tank.util.shotgun.get_associated_sg_base_url = get_associated_sg_base_url_mocker
+        patch = mock.patch("tank.util.shotgun.get_associated_sg_base_url", return_value="http://unit_test_mock_sg")
+        patch.start()
+        self.addCleanup(patch.stop)
 
-        self._original_create_sg_connection = tank.util.shotgun.create_sg_connection
-        tank.util.shotgun.create_sg_connection = create_sg_connection_mocker
-        
+        patch = mock.patch("tank.util.shotgun.create_sg_connection", return_value=self.mockgun)
+        patch.start()
+        self.addCleanup(patch.stop)
+
         # add project to mock sg and path cache db
         self.add_production_path(self.project_root, self.project)
         
@@ -317,11 +309,6 @@ class TankTestBase(unittest.TestCase):
         # important to delete this to free memory
         self.tk = None
 
-        self._original_get_cache_location = tank.pipelineconfig_factory._get_cache_location
-        tank.util.shotgun.get_associated_sg_config_data = self._original_get_associated_sg_config_data
-        tank.util.shotgun.get_associated_sg_base_url = self._original_get_associated_sg_base_url
-        tank.util.shotgun.create_sg_connection = self._original_create_sg_connection
-        
     def setup_fixtures(self, name='config', parameters=None):
         """
         Helper method which sets up a standard toolkit configuration
