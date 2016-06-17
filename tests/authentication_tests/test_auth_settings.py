26cfec6c755a4f72ac6ef0cdb5706244d7218fbe
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2016-06-17 14:18:47 -0400

    finished tests around defaults manager

--- tests/authentication_tests/test_auth_settings.py
+++ tests/authentication_tests/test_auth_settings.py
@@ -17,6 +17,7 @@ from mock import patch, Mock
 
 from tank_test.tank_test_base import *
 
+from tank.util import CoreDefaultsManager
 from tank.authentication import DefaultsManager
 from tank.authentication import GlobalSettings
 
@@ -31,6 +32,12 @@ class DefaultsManagerTest(TankTestBase):
 
     _SESSION_CACHE_USER = "cached_user"
     _CONFIG_USER = "config_user"
+
+    _CONFIG_HTTP_PROXY = "config:proxy@192.168.1.1"
+    _CONFIG_STORE_HTTP_PROXY = "config:app_proxy@192.168.1.1"
+
+    _SHOTGUN_YML_HOST = "https//something.shotgunstudio.com"
+    _SHOTGUN_YML_PROXY = "shotgun:yml@192.168.1.1"
 
     def setUp(self):
         """
@@ -61,3 +68,63 @@ class DefaultsManagerTest(TankTestBase):
         self.assertEqual(dm.get_host(), self._SESSION_CACHE_HOST)
         self.assertEqual(dm.get_login(), self._SESSION_CACHE_USER)
         self.assertIs(dm.get_http_proxy(), None)
+
+    @patch(
+        "tank.authentication.session_cache.get_current_host",
+        return_value=None
+    )
+    @patch(
+        "tank.authentication.session_cache.get_current_user",
+        return_value=None
+    )
+    def test_empty_session_cache(self, *unused_mocks):
+        """
+        Test the behaviour of the defaults manager when the cache is empty
+        and the config file is set.
+        """
+        instance = GlobalSettings._instance = Mock()
+        instance.default_http_proxy = self._CONFIG_HTTP_PROXY
+        instance.default_site = self._CONFIG_HOST
+        instance.default_login = self._CONFIG_USER
+
+        dm = DefaultsManager()
+        self.assertEqual(dm.get_host(), self._CONFIG_HOST)
+        self.assertEqual(dm.get_login(), self._CONFIG_USER)
+        self.assertIs(dm.get_http_proxy(), self._CONFIG_HTTP_PROXY)
+
+    @patch(
+        "tank.util.shotgun.get_associated_sg_config_data",
+        return_value={
+            "host": _SHOTGUN_YML_HOST,
+            "http_proxy": _SHOTGUN_YML_PROXY
+        }
+    )
+    def test_shotgun_yml_over_global(self, *unused_mocks):
+        """
+        Make sure that the session cache always overrides config.ini
+        """
+        instance = GlobalSettings._instance = Mock()
+        instance.default_http_proxy = self._CONFIG_HTTP_PROXY
+        instance.default_site = self._CONFIG_HOST
+        instance.default_login = self._CONFIG_USER
+
+        dm = CoreDefaultsManager()
+        self.assertEqual(dm.get_host(), self._SHOTGUN_YML_HOST)
+        self.assertEqual(dm.get_login(), self._CONFIG_USER)
+        self.assertIs(dm.get_http_proxy(), self._SHOTGUN_YML_PROXY)
+
+    @patch(
+        "tank.util.shotgun.get_associated_sg_config_data",
+        return_value={
+            "host": _SHOTGUN_YML_HOST
+        }
+    )
+    def test_shotgun_yml_no_proxy_uses_global_proxy(self, *unused_mocks):
+        """
+        Make sure that the session cache always overrides config.ini
+        """
+        instance = GlobalSettings._instance = Mock()
+        instance.default_http_proxy = self._CONFIG_HTTP_PROXY
+
+        dm = CoreDefaultsManager()
+        self.assertIs(dm.get_http_proxy(), self._CONFIG_HTTP_PROXY)
