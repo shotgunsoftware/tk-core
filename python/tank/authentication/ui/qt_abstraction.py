6d80bce9e941cfa2708a33e42526014aa0108a94
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2016-06-17 12:38:40 -0400

    Addendum for #36192, pyside2 compatibility layer #288 - Added a constant for the support email address - fixed issues with the login dialog and PySide2 - Short term hack to get the authentication module to detect PySide2 - Instead of updating the PySide2 module, we now create new modules on the fly and nest the PySide2 patches inside it. The original classes are left untouched. - Implemented QDesktopServices.openUrl since it is missing from PySide2 implementations at the moment. - We're not trying to expose QtWidgets anymore. This will be done in the future through a sgtk.platform.qt5 module. Closes https://github.com/shotgunsoftware/tk-core/pull/288

--- python/tank/authentication/ui/qt_abstraction.py
+++ python/tank/authentication/ui/qt_abstraction.py
@@ -12,9 +12,45 @@
 Imports Qt without having to worry whether we are using PyQt4 or PySide.
 """
 try:
-    from PySide import QtCore, QtGui
-except ImportError:
+    from PySide2 import QtCore, QtWidgets, QtGui
+    # FIXME:  We need to refactor the PySide2 patching code from sgtk.platform.qt. This will get
+    # us going in the meantime.
+
+    class _ModuleHack(object):
+        """
+        Merges the QtGui, QtCore and QtWidgets together so we don't have to tailor code
+        for the authentication module for differences between Qt4 and Qt5 class layout.
+        """
+
+        def __init__(self, core, gui, widgets):
+            """
+            Constructor.
+            """
+            self._core = core
+            self._gui = gui
+            self._widgets = widgets
+            widgets.QApplication.UnicodeUTF8 = 1
+
+        def __getattr__(self, name):
+            """
+            Looks in every Qt module for the requested class.
+            """
+            if hasattr(self._core, name):
+                return getattr(self._core, name)
+            if hasattr(self._gui, name):
+                return getattr(self._gui, name)
+            if hasattr(self._widgets, name):
+                return getattr(self._widgets, name)
+            return None
+
+    hack = _ModuleHack(QtCore, QtGui, QtWidgets)
+    QtCore = hack
+    QtGui = hack
+except:
     try:
-        from PyQt4 import QtCore, QtGui
-    except:
-        pass
+        from PySide import QtCore, QtGui
+    except ImportError:
+        try:
+            from PyQt4 import QtCore, QtGui
+        except:
+            pass
