59497f8e6fc97f0c2997b65c39a85ad512d89665
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2016-08-24 08:22:10 -0400

    cleanup and better comments

--- python/tank/authentication/ui/qt5_like_line_edit.py
+++ python/tank/authentication/ui/qt5_like_line_edit.py
@@ -1,8 +1,22 @@
+# Copyright (c) 2016 Shotgun Software Inc.
+#
+# CONFIDENTIAL AND PROPRIETARY
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
+# Source Code License included in this distribution package. See LICENSE.
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
+# not expressly granted therein are reserved by Shotgun Software Inc.
+
+"""
+Qt5-like QLineEdit
+"""
+
+
 from .qt_abstraction import QtGui, QtCore, qt_version_tuple
 
 
 # If we're on Qt4, we want the placeholder in the login dialog.
-print qt_version_tuple
 if qt_version_tuple[0] == 4:
     class Qt5LikeLineEdit(QtGui.QLineEdit):
         """
@@ -18,18 +32,20 @@ if qt_version_tuple[0] == 4:
             """
             Paints the line editor and adds a placeholder on top when the string is empty, even if focused.
             """
-
             # Draws the widget. If the widget is out of focus, it will draw the placeholder.
             QtGui.QLineEdit.paintEvent(self, paint_event)
 
             # This code is based on the C++ implementation of the paint event.
             # http://code.metager.de/source/xref/lib/qt/src/gui/widgets/qlineedit.cpp#1889
+            #
+            # The translated code keeps the flow and the naming as close as possible to the original
+            # in order to easily map one section and variable to another, unless preserving such
+            # consistency actually made the code harder to read.
 
             # If the box is empty and focused, draw the placeholder
             if self.hasFocus() and not self.text() and self.placeholderText():
 
                 p = QtGui.QPainter(self)
-                r = self.rect()
                 pal = self.palette()
 
                 panel = QtGui.QStyleOptionFrameV2()
@@ -47,11 +63,12 @@ if qt_version_tuple[0] == 4:
 
                 fm = self.fontMetrics()
 
-                va = QtGui.QStyle.visualAlignment(self.layoutDirection(), QtCore.Qt.AlignLeft) & QtCore.Qt.AlignVertical_Mask
+                visual_alignment = QtGui.QStyle.visualAlignment(self.layoutDirection(), QtCore.Qt.AlignLeft)
+                vertical_alignment = visual_alignment & QtCore.Qt.AlignVertical_Mask
 
-                if va == QtCore.Qt.AlignBottom:
+                if vertical_alignment == QtCore.Qt.AlignBottom:
                     vscroll = r.y() + r.height() - fm.height() - self._vertical_margin
-                elif va == QtCore.Qt.AlignTop:
+                elif vertical_alignment == QtCore.Qt.AlignTop:
                     vscroll = r.y() + self._vertical_margin
                 else:
                     vscroll = r.y() + (r.height() - fm.height() + 1) / 2
@@ -72,7 +89,7 @@ if qt_version_tuple[0] == 4:
                 line_rect.adjust(min_left_bearing, 0, 0, 0)
 
                 elided_text = fm.elidedText(self.placeholderText(), QtCore.Qt.ElideRight, line_rect.width())
-                p.drawText(line_rect, va, elided_text)
+                p.drawText(line_rect, vertical_alignment, elided_text)
                 p.setPen(oldpen)
 else:
     # Qt5 always has the placeholder.
