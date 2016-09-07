669dc8c14a9aa1cd083bd1b7c68074d426a4a01f
Manne Öhrström <manne@shotgunsoftware.com>
2016-09-07 14:51:09 +0100

    [minor] Bug fixes and unit tests - Added unit tests for resolver class - Fixed an issue in the cached descriptor logic, causing it not to raise - Moved developer scripts into new developer folder - Added comments and feedback to internal api update script - Fixed issue with resolver for site config scenarios

--- developer/shotgun_api_update.sh
+++ developer/shotgun_api_update.sh
@@ -10,6 +10,40 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
+echo "This script will update the local tk-core with the given version of the Shotgun API."
+echo ""
+echo "Script syntax: $0 shotgun_api_tag"
+echo "For example:   $0 v3.0.31"
+echo ""
+echo ""
+echo "A commit_id file will be written to the tk-core shotgun distribution to "
+echo "indicate which version of the shotgun API is being bundled with core."
+echo ""
+echo "This script is intended to be used by developers and maintainers of the tk-core API."
+echo ""
+echo ""
+echo ""
+
+# ask user to continue
+read -p "Ok to proceed? [Y/N] " -n 1 -r
+echo    # (optional) move to a new line
+if [[ ! $REPLY =~ ^[Yy]$ ]]
+then
+    echo "exiting."
+    exit 1
+fi
+
+# check that there is a tag argument
+if [ $# -eq 0 ]
+  then
+    echo ""
+    echo "ERROR: No shotgun tag provided! Check script syntax above. Exiting."
+    exit 1
+fi
+
+echo ""
+echo ""
+
 # Stops the script
 set -e
 
@@ -39,27 +73,45 @@ ZIP=$ROOT/tk-core.zip
 UNZIPPED=$ROOT/unzipped
 SHOTGUN_API_SRC=$UNZIPPED/shotgun_api3
 # Destination relative to this script for the files
-DEST=`pwd`/python/tank_vendor/shotgun_api3
+DEST=`pwd`/../python/tank_vendor/shotgun_api3
 
 # Recreate the folder structure
 mkdir $ROOT
 mkdir $DEST_REPO
+
 # Strip files from the destination
 rm -rf $DEST
+
 # Clone the repo
+echo "Cloning the Shotgun API into a temp location, hang on..."
 git clone $SRC_REPO $DEST_REPO
+
 # Generate the zip
+echo "Archiving tag '$1'..."
 git archive --format zip --output $ZIP --remote $DEST_REPO $1
+
 # Unzip the files except for the tests.
+echo "unpacking tag..."
 unzip $ZIP -d $UNZIPPED
+
 # Move to the git repo to generate the sha and write it to the $DEST
+echo "writing commit_id file..."
 pushd $DEST_REPO
 git rev-parse HEAD > $SHOTGUN_API_SRC/commit_id
 popd
+
+echo "copying files to target location..."
 cp -R $SHOTGUN_API_SRC $DEST
+
 # Put files in the staging area.
+echo "adding new files to git..."
 git add -A $DEST
+
 # Cleanup!
+echo "cleaning up..."
 rm -rf $ROOT
 
+echo ""
+echo "All done! The shotgun API in $DEST has been updated to $1."
+echo "The changes have been added to git and are ready to be committed."
 trap : 0
