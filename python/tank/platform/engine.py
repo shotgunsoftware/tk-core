shotgunsoftware/tk-core

commit 0efd9a5f58365a261b2c5b5bfa1eb98f2551fe41

For #36848, upgrade Desktop to work with 0.18 logging. Introduces a new way to initialize the log file location by giving an absolute path to the log file. LogManager methods therefore now result completes paths to the previous log file instead. This does break compatibility with the previous version of Toolkit, but it is so recent that this part of the API probably hasn't received widespread use yet and we are therefore comfortable with making that change. Closes https://github.com/shotgunsoftware/tk-core/pull/300

python/tank/platform/engine.py
@@ -125,11 +125,11 @@ class Engine(TankBundle):
         # (and the rest of the sgtk logging ) to the user
         self.__log_handler = self.__initialize_logging()
 
-        # check general debug log setting and if this flag is turned on,
-        # adjust the global setting
-
-        if self.get_setting("debug_logging", False):
-            LogManager().global_debug = True
+        # check general debug log setting and update the global debug flag accordingly. Do not set this
+        # flag only when the debug_logging is "true" because the global_debug flag is... global... and it
+        # needs to be reset during engine instantiation if the debug_logging setting suddenly turns false.
+        LogManager().global_debug = self.get_setting("debug_logging", False)
+        if LogManager().global_debug:
             self.log_debug("Engine config flag 'debug_logging' detected, turning on debug output.")
 
         # check that the context contains all the info that the app needs

python/tank/bootstrap/import_handler.py
@@ -54,7 +54,7 @@ class CoreImportHandler(object):
         # and associated with the singleton as these will be lost
         # use local imports to ensure a fresh cut of the code
         from ..log import LogManager
-        prev_log_name = LogManager().uninitialize_base_file_handler()
+        prev_log_file = LogManager().uninitialize_base_file_handler()
         # logging to file is now disabled and will be renamed after the
         # main tank import of the new code.
 
@@ -104,13 +104,13 @@ class CoreImportHandler(object):
         # and re-init our disk logging based on the new code
         # access it from the new tank instance to ensure we get the new code
         try:
-            if prev_log_name:
-                tank.LogManager().initialize_base_file_handler(prev_log_name)
+            if prev_log_file:
+                tank.LogManager().initialize_base_file_handler_from_path(prev_log_file)
         except AttributeError, e:
             # older versions of the API may not have this defined.
             log.warning(
                 "Switching to a version of the core API that doesn't "
-                "have a LogManager.initialize_base_file_handler method defined."
+                "have a LogManager.initialize_base_file_handler_from_path method defined."
             )
 
 

python/tank/log.py
@@ -251,7 +251,7 @@ class LogManager(object):
             # a global and standard rotating log file handler
             # for writing generic toolkit logs to disk
             instance._std_file_handler = None
-            instance._std_file_handler_log_name = None
+            instance._std_file_handler_log_file = None
 
             # collection of weak references to handlers
             # that were created via the log manager.
@@ -513,25 +513,24 @@ class LogManager(object):
         """
         Uninitialize base file handler created with :meth:`initialize_base_file_handler`.
 
-        :returns: The name of the previous log_name that is being switched away from,
+        :returns: The path to the previous log file that is being switched away from,
                   None if no base logger was previously active.
         """
         if self._std_file_handler is None:
-            base_log_name = None
+            return None
 
-        else:
-            base_log_name = self._std_file_handler_log_name
+        base_log_file = self._std_file_handler_log_file
 
-            # there is a log handler, so terminate it
-            log.debug(
-                "Tearing down existing log handler '%s' (%s)" % (base_log_name, self._std_file_handler)
-            )
-            self._root_logger.removeHandler(self._std_file_handler)
-            self._std_file_handler = None
-            self._std_file_handler_log_name = None
+        # there is a log handler, so terminate it
+        log.debug(
+            "Tearing down existing log handler '%s' (%s)" % (base_log_file, self._std_file_handler)
+        )
+        self._root_logger.removeHandler(self._std_file_handler)
+        self._std_file_handler = None
+        self._std_file_handler_log_file = None
 
-        # return the previous base log name
-        return base_log_name
+        # return the previous base log file path.
+        return base_log_file
 
     def initialize_base_file_handler(self, log_name):
         """
@@ -554,28 +553,48 @@ class LogManager(object):
 
         :param log_name: Name of logger to create. This will form the
                          filename of the log file.
-        :returns: The name of the previous log_name that is being switched away from,
+
+        :returns: The path to the previous log file that is being switched away from,
+                  None if no base logger was previously active.
+        """
+        # avoid cyclic references
+        from .util import filesystem
+
+        return self.initialize_base_file_handler_from_path(
+            os.path.join(
+                self.log_folder,
+                "%s.log" % filesystem.create_valid_filename(log_name)
+            )
+        )
+
+    def initialize_base_file_handler_from_path(self, log_file):
+        """
+        Create a file handler and attach it to the sgtk base logger.
+
+        This method is there for legacy Toolkit applications and shouldn't be used. Use
+        ``initialize_base_file_handler`` instead.
+
+        :param log_file: Path of the file to write the logs to.
+
+        :returns: The path to the previous log file that is being switched away from,
                   None if no base logger was previously active.
         """
         # shut down any previous logger
-        previous_log_name = self.uninitialize_base_file_handler()
+        previous_log_file = self.uninitialize_base_file_handler()
 
-        log.debug("Switching file based std logger from %s to %s" % (previous_log_name, log_name))
+        log_folder, log_file_name = os.path.split(log_file)
+        log_name, _ = os.path.splitext(log_file_name)
+
+        log.debug("Switching file based std logger from '%s' to '%s'.", previous_log_file, log_file)
 
         # store new log name
-        self._std_file_handler_log_name = log_name
+        self._std_file_handler_log_file = log_file
 
         # avoid cyclic references
         from .util import filesystem
 
         # set up logging root folder
-        filesystem.ensure_folder_exists(self.log_folder)
-
-        # generate log path
-        log_file = os.path.join(
-            self.log_folder,
-            "%s.log" % filesystem.create_valid_filename(log_name)
-        )
+        filesystem.ensure_folder_exists(log_folder)
 
         # create a rotating log file with a max size of 5 megs -
         # this should make all log files easily attachable to support tickets.
@@ -604,8 +623,7 @@ class LogManager(object):
         log.debug("Writing to log standard log file %s" % log_file)
 
         # return previous log name
-        return previous_log_name
-
+        return previous_log_file
 
 # the logger for logging messages from this file :)
 log = LogManager.get_logger(__name__)

commit 2e42f294e578b366c44eddde6583522e383a943f

For #38747, global logging turned off by engine start Fixes a regression introduced by https://github.com/shotgunsoftware/tk-core/commit/0efd9a5f58365a261b2c5b5bfa1eb98f2551fe41 Closes https://github.com/shotgunsoftware/tk-core/pull/346/

python/tank/platform/engine.py
@@ -127,11 +127,10 @@ class Engine(TankBundle):
         # (and the rest of the sgtk logging ) to the user
         self.__log_handler = self.__initialize_logging()
 
-        # check general debug log setting and update the global debug flag accordingly. Do not set this
-        # flag only when the debug_logging is "true" because the global_debug flag is... global... and it
-        # needs to be reset during engine instantiation if the debug_logging setting suddenly turns false.
-        LogManager().global_debug = self.get_setting("debug_logging", False)
-        if LogManager().global_debug:
+        # check general debug log setting and if this flag is turned on,
+        # adjust the global setting
+        if self.get_setting("debug_logging", False):
+            LogManager().global_debug = True
             self.log_debug(
                 "Detected setting 'config/env/%s.yml:%s.debug_logging: true' "
                 "in your environment configuration. Turning on debug output." % (env.name, engine_instance_name)