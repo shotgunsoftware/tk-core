aecc653c3ee82f51cadad1f4f10ff1fda8fe2043
Jean-François Boismenu <jfboismenu@users.noreply.github.com>
2016-10-31 12:50:00 -0400

    Update yaml_cache.py

--- python/tank/util/yaml_cache.py
+++ python/tank/util/yaml_cache.py
@@ -257,14 +257,12 @@ class YamlCache(object):
         """
         path = item.path
         try:
-            fh = open(path, "r")
-            raw_data = yaml.load(fh)
+            with open(path, "r") as fh:
+                raw_data = yaml.load(fh)
         except IOError:
             raise TankFileDoesNotExistError("File does not exist: %s" % path)
         except Exception, e:
             raise TankError("Could not open file '%s'. Error reported: '%s'" % (path, e))
-        finally:
-            fh.close()
         # Populate the item's data before adding it to the cache.
         item.data = raw_data
 
