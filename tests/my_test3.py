9ba07f13ef5c560d5a4a7dcf8c281a9fa61f5f38
Patrick Hubert <patrick.hubert@autodesk.com>
2016-11-02 11:30:22 -0400

    Cherry picking og the work done in branch ticket/41731_update_to_support_sso

---
+++ tests/my_test3.py
@@ -0,0 +1,46 @@
+#!/Applications/Shotgun.app/Contents/Frameworks/Python/bin/python
+"""TBD."""
+
+# Import Toolkit so we can access to Toolkit specific features.
+from PySide import QtGui
+
+# Import the ShotgunAuthenticator from the tank_vendor.shotgun_authentication
+# module. This class allows you to authenticate either programmatically or, in this # noqa
+# case, interactively.
+from tank_vendor.shotgun_authentication import ShotgunAuthenticator
+# from tank.authentication import interactive_authentication
+
+# import sgtk
+# import logging
+# log = logging.getLogger('patate')
+# loglevel = logging.DEBUG
+# log.setLevel(loglevel)
+# log_handler = logging.StreamHandler()
+# log.addHandler(log_handler)
+
+# sgtk.LogManager().initialize_base_file_handler("rv-patate")
+# sgtk.LogManager().initialize_custom_handler(log_handler)
+# log_handler.setLevel(loglevel)
+
+# Instantiate the CoreDefaultsManager. This allows the ShotgunAuthenticator to
+# retrieve the site, proxy and optional script_user credentials from shotgun.yml # noqa
+# cdm = sgtk.util.CoreDefaultsManager()
+
+# Instantiate the authenticator object, passing in the defaults manager.
+# authenticator = ShotgunAuthenticator(cdm)
+authenticator = ShotgunAuthenticator()
+
+authenticator.clear_default_user()
+app = QtGui.QApplication([])
+user = authenticator.get_user()
+print "User is '%s'" % user
+
+defaultUser = authenticator.get_default_user()
+print "DefaultUser is '%s'" % defaultUser
+
+# # Tells Toolkit which user to use for connecting to Shotgun. Note that this should # noqa
+# # always take place before creating a Sgtk instance.
+# sgtk.set_authenticated_user(user)
+
+
+# authenticator.clear_default_user()
