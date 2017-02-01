02f12bf84aee2697963fdd8fc61584226463ab51
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-02-01 15:20:59 -0500

    app store proxy settings weren't being picked up correctly

--- python/tank/descriptor/io_descriptor/appstore.py
+++ python/tank/descriptor/io_descriptor/appstore.py
@@ -735,10 +735,7 @@ class IODescriptorAppStore(IODescriptorBase):
         try:
             config_data = shotgun.get_associated_sg_config_data()
         except UnresolvableCoreConfigurationError:
-            # This core is not part of a pipeline configuration, we're probably bootstrapping,
-            # so skip the check and simply return the regular proxy settings.
-            log.debug("No core configuration was found, using the current connection's proxy setting.")
-            return self._sg_connection.config.raw_http_proxy
+            config_data = None
 
         if config_data and constants.APP_STORE_HTTP_PROXY in config_data:
             return config_data[constants.APP_STORE_HTTP_PROXY]
