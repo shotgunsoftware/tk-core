103a78db31d25ee80264636159504db33104f518
Jeff Beeland <jeff.beeland@autodesk.com>
2017-03-10 17:44:47 -0800

    #34446: PNG file profiles have been stripped.
    This will silence errors from libpng by way of Qt on png file load for icons.

--- python/tank/platform/qt/build_resources.sh
+++ python/tank/platform/qt/build_resources.sh
@@ -13,6 +13,9 @@
 # The path to where the PySide binaries are installed
 PYTHON_BASE="/Applications/Shotgun.app/Contents/Resources/Python"
 
+# Clear up potential png profile problems.
+for f in *png;do convert -strip $f $f; done
+
 # The path to output all built .py files to: 
 UI_PYTHON_PATH=.
 
