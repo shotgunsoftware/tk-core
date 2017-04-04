63a4e4465965010da4fd92f888d4fa6afe7bf2bc
Philip Scadding <pscadding@users.noreply.github.com>
2017-04-04 11:47:37 +0100

    Ticket #41112 Updated list of entity types available on tank command
    The hard coded list of entity types available for use as context in a tank command, has been updated to match the list of available entity types provided on new sites. 44 new entity types added. The list has also been sorted alphabetically to make it easier to look over.
    
    Update the ranges of the custom entity matches during tank command entity type check. Ranges have been increased to reflect the newer available custom entities numbers.
    Before this calls like update `tank CustomEntity50 1234` would fail.
    closes  (#415)

--- scripts/tank_cmd.py
+++ scripts/tank_cmd.py
@@ -49,30 +49,33 @@ ARG_SCRIPT_NAME = "script-name"
 ARG_SCRIPT_KEY = "script-key"
 ARG_CREDENTIALS_FILE = "credentials-file"
 
-SHOTGUN_ENTITY_TYPES = ['Playlist', 'AssetSceneConnection', 'Note', 'TaskDependency', 'PageHit',
-                        'ActionMenuItem', 'Attachment', 'AssetMocapTakeConnection',
-                        'Department', 'Group', 'PlaylistVersionConnection',
-                        'Booking', 'CutVersionConnection', 'CameraMocapTakeConnection',
-                        'AssetElementConnection', 'ReleaseTicketConnection',
-                        'RevisionRevisionConnection', 'MocapTakeRangeShotConnection', 'TimeLog',
-                        'Step', 'AssetBlendshapeConnection', 'PerformerMocapTakeConnection',
-                        'Phase', 'Ticket', 'AssetShotConnection', 'TicketTicketConnection',
-                        'Icon', 'PageSetting', 'Status', 'Reply', 'Task', 'ApiUser',
-                        'ProjectUserConnection', 'LaunchShotConnection', 'ShotShotConnection',
-                        'PerformerRoutineConnection', 'AppWelcomeUserConnection', 'HumanUser',
-                        'Project', 'LocalStorage', 'TaskTemplate', 'RevisionTicketConnection',
-                        'PerformerShootDayConnection', 'PipelineConfiguration', 'LaunchSceneConnection',
-                        'GroupUserConnection', 'AssetSequenceConnection', 'Page',
-                        'ShootDaySceneConnection', 'TankType', 'PhysicalAssetMocapTakeConnection',
-                        'Shot', 'TankPublishedFile', 'Sequence', 'BannerUserConnection',
-                        'AssetAssetConnection', 'Version', 'ElementShotConnection',
-                        'PermissionRuleSet', 'EventLogEntry', 'TankDependency',
-                        'PublishedFile', 'PublishedFileType', 'PublishedFileDependency',
-                        'AssetShootDayConnection', 'Asset']
+SHOTGUN_ENTITY_TYPES = ['ActionMenuItem', 'ApiUser', 'AppWelcomeUserConnection', 'Asset', 'AssetAssetConnection',
+                        'AssetBlendshapeConnection', 'AssetElementConnection', 'AssetEpisodeConnection',
+                        'AssetLevelConnection', 'AssetLibrary', 'AssetMocapTakeConnection', 'AssetSceneConnection',
+                        'AssetSequenceConnection', 'AssetShootDayConnection', 'AssetShotConnection', 'Attachment',
+                        'BannerUserConnection', 'Blendshape', 'Booking', 'Camera', 'CameraMocapTakeConnection',
+                        'Candidate', 'ClientUser', 'Cut', 'CutItem', 'CutVersionConnection', 'Delivery',
+                        'DeliveryTarget', 'Delivery_sg_assets_Connection', 'Delivery_sg_shots_Connection',
+                        'Delivery_sg_versions_Connection', 'Department', 'Element', 'ElementShotConnection', 'Episode',
+                        'EventLogEntry', 'FilesystemLocation', 'Group', 'GroupUserConnection', 'HumanUser', 'Icon',
+                        'Launch', 'LaunchSceneConnection', 'LaunchShotConnection', 'Level', 'LocalStorage', 'MocapPass',
+                        'MocapSetup', 'MocapTake', 'MocapTakeRange', 'MocapTakeRangeShotConnection', 'Note', 'Page',
+                        'PageHit', 'PageSetting', 'Performer', 'PerformerMocapTakeConnection',
+                        'PerformerRoutineConnection', 'PerformerShootDayConnection', 'PermissionRuleSet', 'Phase',
+                        'PhysicalAsset', 'PhysicalAssetMocapTakeConnection', 'PipelineConfiguration', 'Playlist',
+                        'PlaylistShare', 'PlaylistVersionConnection', 'Project', 'ProjectUserConnection',
+                        'PublishEvent', 'PublishedFile', 'PublishedFileDependency', 'PublishedFileType', 'Reel',
+                        'Release', 'ReleaseTicketConnection', 'Reply', 'Revision', 'RevisionRevisionConnection',
+                        'RevisionTicketConnection', 'Routine', 'RvLicense', 'Scene', 'Sequence', 'ShootDay',
+                        'ShootDaySceneConnection', 'Shot', 'ShotShotConnection', 'Shot_sg_animations_Connection',
+                        'Shot_sg_deliverables_Connection', 'Slate', 'SourceClip', 'Status', 'Step', 'TankAction',
+                        'TankContainer', 'TankDependency', 'TankPublishedFile', 'TankType', 'Task', 'TaskDependency',
+                        'TaskTemplate', 'TemerityNode', 'Ticket', 'TicketTicketConnection',
+                        'Ticket_sg_related_assets_Connection', 'TimeLog', 'Tool', 'Version', 'WatermarkingPreset']
 
-SHOTGUN_ENTITY_TYPES.extend(["CustomEntity%02d"%x for x in range(1, 31)])
-SHOTGUN_ENTITY_TYPES.extend(["CustomNonProjectEntity%02d"%x for x in range(1, 16)])
-SHOTGUN_ENTITY_TYPES.extend(["CustomThreadedEntity%02d"%x for x in range(1, 6)])
+SHOTGUN_ENTITY_TYPES.extend(["CustomEntity%02d"%x for x in range(1, 51)])
+SHOTGUN_ENTITY_TYPES.extend(["CustomNonProjectEntity%02d"%x for x in range(1, 31)])
+SHOTGUN_ENTITY_TYPES.extend(["CustomThreadedEntity%02d"%x for x in range(1, 16)])
 
 ###############################################################################################
 # Helpers and General Stuff
