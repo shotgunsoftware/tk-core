1a515bbc17040f2d3fdaef0828f19fb70b76108e
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-07-26 14:42:36 -0400

    added reauthenticate...

--- python/tank_vendor/shotgun_api3/shotgun.py
+++ python/tank_vendor/shotgun_api3/shotgun.py
@@ -2771,7 +2771,7 @@ class Shotgun(object):
                 {
                     "root_path": root_path,
                     "seed_entity_field": seed_entity_field,
-                    "search_criteria": {"entity": entity }
+                    "search_criteria": {"entity": entity}
                 }
         )
 
@@ -2791,13 +2791,26 @@ class Shotgun(object):
         if self.config.session_token:
             return self.config.session_token
 
+        session_token = self._create_session_token
+
+        self.config.session_token = session_token
+
+        return session_token
+
+    def _create_session_token(self):
         rv = self._call_rpc("get_session_token", None)
         session_token = (rv or {}).get("session_id")
         if not session_token:
             raise RuntimeError("Could not extract session_id from %s", rv)
-        self.config.session_token = session_token
-
         return session_token
+
+    def reauthenticate(self):
+        if self.config.session_token:
+            session_token = self._create_session_token()
+            return Shotgun(self.base_url, session_token=session_token)
+        else:
+            # FIXME: handle other cases here.
+            pass
 
     def _build_opener(self, handler):
         """
