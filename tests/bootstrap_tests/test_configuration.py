d8d518c1f3a6d9874d185379f603d877bbc56788
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-07-27 15:34:17 -0400

    fixes CI

--- tests/bootstrap_tests/test_configuration.py
+++ tests/bootstrap_tests/test_configuration.py
@@ -22,21 +22,36 @@ import sgtk
 
 class TestConfiguration(TankTestBase):
 
+    def _create_session_user(self, name):
+        """
+        Shorthand to create a session user.
+        """
+        return ShotgunAuthenticator().create_session_user(
+            name, session_token=name[::-1], host="https://test.shotgunstudio.com"
+        )
+
+    def _create_script_user(self, api_script):
+        """
+        Shorthand to create a script user.
+        """
+        return ShotgunAuthenticator().create_script_user(
+            api_script, api_key=api_script[::-1], host="https://test.shotgunstudio.com"
+        )
+
     def test_login_based_authentication(self):
         """
         Ensure the configuration will always pick the user passed in when there is no script user.
         """
         configuration = Configuration(None, None)
-        sg_auth = ShotgunAuthenticator()
 
-        default_user = sg_auth.create_session_user("default_user", "abcd")
+        default_user = self._create_session_user("default_user")
 
         # Create a default user.
         with patch(
             "tank.authentication.ShotgunAuthenticator.get_default_user",
             return_value=default_user
         ):
-            current_user = sg_auth.create_session_user("current_user", "1234")
+            current_user = self._create_session_user("current_user")
             configuration._set_authenticated_user(current_user, sgtk.authentication.serialize_user)
 
             self.assertEqual(sgtk.get_authenticated_user().login, current_user.login)
@@ -47,9 +62,8 @@ class TestConfiguration(TankTestBase):
         Ensure the configuration will recover if the user can't be serialized/unserialized.
         """
         configuration = Configuration(None, None)
-        sg_auth = ShotgunAuthenticator()
 
-        default_user = sg_auth.create_session_user("default_user", "abcd")
+        default_user = self._create_session_user("default_user")
 
         # Create a default user.
         with patch(
@@ -59,7 +73,7 @@ class TestConfiguration(TankTestBase):
             serialize_mock = MagicMock()
             serialize_mock.side_effect = Exception("This will be raised.")
 
-            current_user = sg_auth.create_session_user("current_user", "1234")
+            current_user = self._create_session_user("current_user")
             configuration._set_authenticated_user(current_user, serialize_mock)
 
             serialize_mock.assert_called_with(current_user)
@@ -72,16 +86,15 @@ class TestConfiguration(TankTestBase):
         Ensure the configuration will always pick the script user when the configuration uses one.
         """
         configuration = Configuration(None, None)
-        sg_auth = ShotgunAuthenticator()
 
-        script_user = sg_auth.create_script_user("api_script", "api_key")
+        script_user = self._create_script_user("api_script")
 
         # Create a default user.
         with patch(
             "tank.authentication.ShotgunAuthenticator.get_default_user",
             return_value=script_user
         ):
-            current_user = sg_auth.create_session_user("current_user", "1234")
+            current_user = self._create_session_user("current_user")
             configuration._set_authenticated_user(current_user, sgtk.authentication.serialize_user)
 
             self.assertEqual(id(sgtk.get_authenticated_user()), id(script_user))
