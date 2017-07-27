82c07d6eeb1623693b1526d599e5cc9b10b0b0b0
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-07-27 15:22:45 -0400

    rewrote the configuration tests to use real shotgun users so we actually serialized something. added a test that ensures that is serialize/deserialization fails than we fell back on the original user instance.

--- tests/bootstrap_tests/test_configuration.py
+++ tests/bootstrap_tests/test_configuration.py
@@ -16,50 +16,70 @@ from tank_test.tank_test_base import setUpModule # noqa
 from tank_test.tank_test_base import TankTestBase
 
 from sgtk.bootstrap.configuration import Configuration
+from sgtk.authentication import ShotgunAuthenticator
 import sgtk
 
 
 class TestConfiguration(TankTestBase):
-
-    def setUp(self):
-        """
-        Overrides get_default_user so it returns something.
-        """
-        super(TestConfiguration, self).setUp()
-
-        self._mock_default_user = MagicMock()
-
-        patcher = patch(
-            "tank.authentication.ShotgunAuthenticator.get_default_user",
-            return_value=self._mock_default_user
-        )
-        patcher.start()
-        self.addCleanup(patcher.stop)
 
     def test_login_based_authentication(self):
         """
         Ensure the configuration will always pick the user passed in when there is no script user.
         """
         configuration = Configuration(None, None)
+        sg_auth = ShotgunAuthenticator()
 
-        self._mock_default_user.login = "user"
+        default_user = sg_auth.create_session_user("default_user", "abcd")
 
-        user = MagicMock(login="another_user")
-        configuration._set_authenticated_user(user)
+        # Create a default user.
+        with patch(
+            "tank.authentication.ShotgunAuthenticator.get_default_user",
+            return_value=default_user
+        ):
+            current_user = sg_auth.create_session_user("current_user", "1234")
+            configuration._set_authenticated_user(current_user, sgtk.authentication.serialize_user)
 
-        self.assertEqual(sgtk.get_authenticated_user(), user)
+            self.assertEqual(sgtk.get_authenticated_user().login, current_user.login)
+            self.assertNotEqual(id(sgtk.get_authenticated_user()), id(current_user))
+
+    def test_fail_reinstantiating(self):
+        """
+        Ensure the configuration will recover if the user can't be serialized/unserialized.
+        """
+        configuration = Configuration(None, None)
+        sg_auth = ShotgunAuthenticator()
+
+        default_user = sg_auth.create_session_user("default_user", "abcd")
+
+        # Create a default user.
+        with patch(
+            "tank.authentication.ShotgunAuthenticator.get_default_user",
+            return_value=default_user
+        ):
+            serialize_mock = MagicMock()
+            serialize_mock.side_effect = Exception("This will be raised.")
+
+            current_user = sg_auth.create_session_user("current_user", "1234")
+            configuration._set_authenticated_user(current_user, serialize_mock)
+
+            self.assertEqual(sgtk.get_authenticated_user().login, current_user.login)
+            self.assertEqual(id(sgtk.get_authenticated_user()), id(current_user))
 
     def test_script_based_authentication(self):
         """
         Ensure the configuration will always pick the script user when the configuration uses one.
         """
         configuration = Configuration(None, None)
+        sg_auth = ShotgunAuthenticator()
 
-        self._mock_default_user.login = None
+        script_user = sg_auth.create_script_user("api_script", "api_key")
 
-        user = MagicMock(login="another_user")
-        configuration._set_authenticated_user(user)
+        # Create a default user.
+        with patch(
+            "tank.authentication.ShotgunAuthenticator.get_default_user",
+            return_value=script_user
+        ):
+            current_user = sg_auth.create_session_user("current_user", "1234")
+            configuration._set_authenticated_user(current_user, sgtk.authentication.serialize_user)
 
-        self.assertEqual(
-            sgtk.get_authenticated_user(), self._mock_default_user
-        )
+            self.assertEqual(id(sgtk.get_authenticated_user()), id(script_user))
