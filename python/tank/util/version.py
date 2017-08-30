42078e3e201e3cb9189d30fd882f5679e305ca7b
Nicolas Montmarquette <nicolas.montmarquette@autodesk.com>
2017-08-30 09:56:56 -0400

    Add version string parsing utility class

--- python/tank/util/version.py
+++ python/tank/util/version.py
@@ -11,6 +11,78 @@
 import re
 
 from distutils.version import LooseVersion
+import re
+
+class Version(object):
+
+    def __init__(self, version_string):
+
+        self._app_name = None
+        self._version_string = None
+        self._version_major = -1
+        self._version_minor = -1
+
+        if version_string:
+            # Matching only the numbering part not potential appplication name
+            match = re.search(r'([\d.]+.*)',version_string)
+            if match:
+
+                # group(0) returns only the number part
+                tmp_version_string = match.group(0)
+
+                # Shop off the version string and also remove leading and trailing whitespaces
+                self._app_name = version_string.replace(tmp_version_string, "").rstrip().lstrip()
+                # If empty, replace with None for consistency with other init situation
+                if len(self._app_name) == 0:
+                    self._app_name = None
+
+                # Shop off trailing "." from number version if not following by a number
+                if tmp_version_string[-1]==".":
+                    tmp_version_string = tmp_version_string[:-1]
+
+                self._version_string = tmp_version_string
+
+                number_list = self._version_major = tmp_version_string.split(" ")[0].split(".")
+                if number_list:
+                    if len(number_list)>0:
+                        if number_list[0].isdigit():
+                            self._version_major = int(number_list[0])
+                    if len(number_list)>1:
+                        if number_list[1].isdigit():
+                            self._version_minor = int(number_list[1])
+            else:
+                self._app_name = version_string
+
+    def __repr__(self):
+        """Official str representation of an application version."""
+        return "%s" % (self._version_string)
+
+    def __str__(self):
+        """Readable str representation of the version object."""
+        return "%s: %s, major:%d, minor:%d, app name:%s" % (self.__class__,
+                                               repr(self),
+                                               self.major,
+                                               self.minor,
+                                               self.app_name
+                                               )
+
+    @property
+    def app_name(self):
+        return self._app_name
+
+    @property
+    def version_string(self):
+        return self._version_string
+
+    @property
+    def major(self):
+        return int(self._version_major)
+
+    @property
+    def minor(self):
+        return int(self._version_minor)
+
+
 
 def is_version_head(version):
     """
--- tests/util_tests/test_version_compare.py
+++ tests/util_tests/test_version_compare.py
@@ -19,7 +19,7 @@ from tank import TankError
 from tank_test.tank_test_base import *
 from tank.template import TemplatePath
 from tank.templatekey import SequenceKey
-
+from tank.util.version import Version as Version
 
 
 class TestVersionCompare(TankTestBase):
@@ -81,4 +81,96 @@ class TestVersionCompare(TankTestBase):
         self.assertTrue( is_version_older("v0.12.3", "0.12.4") )
         
         self.assertFalse( is_version_older("1.0.0", "1.0.0") )
-        
+
+
+class TestVersion(TankTestBase):
+
+    def test_init_with_misc_test_strings(self):
+        """ Simply testing for unhandled exception when supplying different kind of strings."""
+        Version("My Super App 2.1")
+        Version("My Super App 2.")
+        Version("My Super App 2")
+        Version("My Super App")
+        Version("")
+        Version(None)
+        Version("2.3.4.5.")
+        Version("i2348y7982y4")
+        Version("&?%*?&%$?&$&%?$&?%$&?")
+        Version(" .. . . . . ")
+        Version("      \n\t\r   ")
+        Version("")
+        Version(None)
+
+    def test_init_with_single_word_app_name(self):
+        """ Simply testing various strings and making sure we're not causing unexpected exception."""
+
+        obj = Version("Maya 2017")
+        self.assertEqual(obj.app_name, "Maya")
+        self.assertEqual(repr(obj), "2017")
+        self.assertEqual(obj.major, 2017)
+        self.assertEqual(obj.minor, -1)
+
+        obj = Version("Maya   2017")
+        self.assertEqual(obj.app_name, "Maya")
+        self.assertEqual(repr(obj), "2017")
+        self.assertEqual(obj.major, 2017)
+        self.assertEqual(obj.minor, -1)
+
+        obj = Version(" Maya  2017.2")
+        self.assertEqual(obj.app_name, "Maya")
+        self.assertEqual(repr(obj), "2017.2")
+        self.assertEqual(obj.major, 2017)
+        self.assertEqual(obj.minor, 2)
+
+    def test_init_with_app_name_and_version_numbers(self):
+
+        obj = Version("My Super App 2.17.300b")
+        self.assertEqual(obj.app_name, "My Super App")
+        self.assertEqual(repr(obj), "2.17.300b")
+        self.assertEqual(obj.major, 2)
+        self.assertEqual(obj.minor, 17)
+
+        # Check that trailing '.' is removed
+        obj = Version("My Super App 2.")
+        self.assertEqual(obj.app_name, "My Super App")
+        self.assertEqual(repr(obj), "2")
+        self.assertEqual(obj.major, 2)
+        self.assertEqual(obj.minor, -1)
+
+        obj = Version("My Super App 2")
+        self.assertEqual(obj.app_name, "My Super App")
+        self.assertEqual(repr(obj), "2")
+        self.assertEqual(obj.major, 2)
+        self.assertEqual(obj.minor, -1)
+
+        obj = Version("My Super App")
+        self.assertEqual(obj.app_name, "My Super App")
+        self.assertEqual(repr(obj), "None")
+        self.assertEqual(obj.major, -1)
+        self.assertEqual(obj.minor, -1)
+
+    def test_init_without_app_names(self):
+
+        obj = Version("2.17.300b")
+        self.assertEqual(obj.app_name, None)
+        self.assertEqual(repr(obj), "2.17.300b")
+        self.assertEqual(obj.major, 2)
+        self.assertEqual(obj.minor, 17)
+
+    def test_init_with_none(self):
+
+        obj = Version(None)
+        self.assertEqual(obj.app_name, None)
+        self.assertEqual(repr(obj), "None")
+        self.assertEqual(obj.major, -1)
+        self.assertEqual(obj.minor, -1)
+
+    def test_init_with_empty_strimg(self):
+
+        obj = Version("")
+        self.assertEqual(obj.app_name, None)
+        self.assertEqual(repr(obj), "None")
+        self.assertEqual(obj.major, -1)
+        self.assertEqual(obj.minor, -1)
+
+
