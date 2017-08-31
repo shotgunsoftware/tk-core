d41c3b73a5fb6d31c45a5941a6960e899316cc45
Nicolas Montmarquette <nicolas.montmarquette@autodesk.com>
2017-08-31 15:59:25 -0400

    For #44051 deprecate metric attribute logging in util.shotgun.download

--- python/tank/util/shotgun/download.py
+++ python/tank/util/shotgun/download.py
@@ -26,7 +26,7 @@ from ...errors import TankError
 from ...log import LogManager
 from ..zip import unzip_file
 from .. import filesystem
-from ..metrics import log_user_attribute_metric
+from ..metrics import EventMetric
 
 log = LogManager.get_logger(__name__)
 
@@ -177,8 +177,12 @@ def download_and_unpack_attachment(sg, attachment_id, target, retries=5):
             broadband_speed_bps = file_size * 8.0 / time_to_download
             broadband_speed_mibps = broadband_speed_bps / (1024 * 1024)
             log.debug("Download speed: %4f Mbit/s" % broadband_speed_mibps)
-            log_user_attribute_metric("Tk attachment download speed", "%4f Mbit/s" % broadband_speed_mibps)
 
+            EventMetric.log(EventMetric.GROUP_TOOLKIT, "Downloaded File",
+                properties={
+                    "Tk attachment download speed": "%4f Mbit/s" % (broadband_speed_mibps)
+                }
+            )
 
             log.debug("Unpacking %s bytes to %s..." % (file_size, target))
             filesystem.ensure_folder_exists(target)
