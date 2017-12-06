3b211cf110ba269e4d2b1d30145c8e8d109112a4
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-12-06 15:56:57 -0500

    restoring config folder backup

--- python/tank/bootstrap/configuration_writer.py
+++ python/tank/bootstrap/configuration_writer.py
@@ -143,17 +143,18 @@ class ConfigurationWriter(object):
 
     def move_to_backup(self, undo_on_error):
         """
-        Move any existing core to a backup location.
+        Move any existing config and core to a backup location.
 
-        After this method has been executed, there is no install/core folder
-        present in the configuration scaffold. It has been moved into it's
-        backup location.
+        After this method has been executed, there is no config and
+        no install/core folder present in the configuration scaffold.
+        Both have been moved into their respective backup locations.
 
         :param bool undo_on_error: If True, the move to backup will be undone if there is an error during
             the backup process.
 
-        :returns: The location where the core was backed up. ``None`` will be returned it nothing
-                  was carried over.
+        :returns: (config_backup_path, core_backup_path) where the paths
+                  can be None in case nothing was carried over.
+
         """
 
         # The move guard's role is to keep track of every move operation that has happened
@@ -162,13 +163,39 @@ class ConfigurationWriter(object):
         # code sections. Its also a great way to avoid having to deal with variables that haven't been
         # defined yet when dealing with the exceptions.
         with MoveGuard(undo_on_error) as guard:
+            config_backup_path = None
             core_backup_path = None
 
             # get backup root location
             config_path = self._path.current_os
+            configuration_payload = os.path.join(config_path, "config")
 
             # timestamp for rollback backup folders
             timestamp = datetime.datetime.now().strftime("%Y%m%d_%H%M%S")
+
+            if os.path.exists(configuration_payload):
+
+                config_backup_root = os.path.join(config_path, "install", "config.backup")
+
+                # make sure we have a backup folder present
+                # sometimes, execution and rollback is so quick that several backup folders
+                # are created in a single second. In that case, append a suffix
+                config_backup_path = os.path.join(config_backup_root, timestamp)
+                counter = 0
+                while os.path.exists(config_backup_path):
+                    # that backup path already exists. Try another one
+                    counter += 1
+                    config_backup_path = os.path.join(config_backup_root, "%s.%d" % (timestamp, counter))
+
+                # now that we have found a spot for our backup, make sure folder exists
+                # and then move the existing config *into* this folder.
+                filesystem.ensure_folder_exists(config_backup_path)
+
+                log.debug("Moving config %s -> %s" % (configuration_payload, config_backup_path))
+                backup_target_path = os.path.join(config_backup_path, os.path.basename(configuration_payload))
+                guard.move(configuration_payload, backup_target_path)
+                log.debug("Backup complete.")
+                config_backup_path = backup_target_path
 
             # now back up the core API
             core_payload = os.path.join(config_path, "install", "core")
@@ -194,7 +221,7 @@ class ConfigurationWriter(object):
                 log.debug("Backup complete.")
                 core_backup_path = core_backup_path
 
-            return core_backup_path
+            return (config_backup_path, core_backup_path)
 
     @filesystem.with_cleared_umask
     def create_tank_command(self, executable=sys.executable, prefix=sys.prefix):
