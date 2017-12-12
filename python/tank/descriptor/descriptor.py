d7db9042056ea226dff01c2b4ce16da4be548278
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2017-12-12 09:51:40 -0500

    For #40825, bootstrap from cache This removes the need to copy the content of a configuration into the unmanaged configuration when there is an update.
    The PipelineConfiguration class was refactored so it could use the descriptor member to locate the configuration bundle on disk.
    
    This also introduces a `features` API that allows the bootstrap process to introspect the core it is about to install and switch to without having to actually import the module. This is done with the helpof the `info.yml`. We're not marking the API as public for now and since it's only being used by the bootstrapper at the moment we don't have to worry about breaking changes to the API if we decide to tweak it.
    Closes https://github.com/shotgunsoftware/tk-core/pull/565

--- python/tank/descriptor/descriptor.py
+++ python/tank/descriptor/descriptor.py
@@ -17,6 +17,10 @@ from .errors import TankDescriptorError
 from ..util import LocalFileStorageManager
 from . import constants
 
+from ..log import LogManager
+
+logger = LogManager.get_logger(__name__)
+
 
 def create_descriptor(
         sg_connection,
@@ -106,10 +110,14 @@ def create_descriptor(
         return FrameworkDescriptor(sg_connection, io_descriptor)
 
     elif descriptor_type == Descriptor.CONFIG:
-        return CachedConfigDescriptor(io_descriptor)
+        return CachedConfigDescriptor(
+            sg_connection, bundle_cache_root_override, fallback_roots, io_descriptor
+        )
 
     elif descriptor_type == Descriptor.INSTALLED_CONFIG:
-        return InstalledConfigDescriptor(io_descriptor)
+        return InstalledConfigDescriptor(
+            sg_connection, bundle_cache_root_override, fallback_roots, io_descriptor
+        )
 
     elif descriptor_type == Descriptor.CORE:
         return CoreDescriptor(io_descriptor)
