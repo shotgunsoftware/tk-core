1dc2a65fa396bcf9bcf44ee2221ad7ee4030fb88
Nicolas Montmarquette <nicolas.montmarquette@autodesk.com>
2017-12-14 17:02:55 -0500

    Revert changes unnecessary white change to appstore.py file

--- python/tank/descriptor/io_descriptor/appstore.py
+++ python/tank/descriptor/io_descriptor/appstore.py
@@ -871,4 +871,3 @@ class IODescriptorAppStore(IODescriptorDownloadable):
             log.debug("...could not establish connection: %s" % e)
             can_connect = False
         return can_connect
-
