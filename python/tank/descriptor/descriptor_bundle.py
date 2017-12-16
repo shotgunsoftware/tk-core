dc95602dbf48749c2617d80197b736f3fba03506
Nicolas Montmarquette <nicolas.montmarquette@autodesk.com>
2017-12-16 10:57:56 -0500

    Remove unused os import

--- python/tank/descriptor/descriptor_bundle.py
+++ python/tank/descriptor/descriptor_bundle.py
@@ -8,7 +8,6 @@
 # agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
-import os
 from .descriptor import Descriptor
 from .errors import TankDescriptorError, CheckVersionConstraintsError
 from . import constants
