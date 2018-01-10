7df0a5c2c304d7c35afd819677ce28cc8a49455c
Patrick Hubert <patrick.hubert@autodesk.com>
2018-01-10 17:22:08 -0500

    For #46182: fixed usage of dictionaries in default argument value + fixed docstrings

--- python/tank/authentication/shotgun_shared/authentication_session_data.py
+++ python/tank/authentication/shotgun_shared/authentication_session_data.py
@@ -32,15 +32,14 @@ class AuthenticationSessionData(object):
         user_id:            A string for the user id.
     """
 
-    def __init__(self, settings={}):
+    def __init__(self, settings=None):
         """
         Constructor.
 
-        Args:
-            settings (dict):    Dictionary of element to add to the settings.
-                                Non-used key/value pairs will be discarded
-                                silently.
+        :param settings: Dictionary of element to add to the settings.
+                         Non-used key/value pairs will be silently discarded.
         """
+        settings = settings or {}
         self._cookies = None
         self._csrf_key = None
         self._csrf_value = None
@@ -54,7 +53,7 @@ class AuthenticationSessionData(object):
 
     def __repr__(self):
         """
-        Returns a string reprensentation of the session.
+        Returns a string reprensentation of the session. For debugging purposes.
 
         :returns: A string containing all of the session data.
         """
@@ -69,10 +68,8 @@ class AuthenticationSessionData(object):
         """
         Merge new settings with existing ones.
 
-        Args:
-            settings (dict):    Dictionary of element to merge to the settings.
-                                Non-used key/value pairs will be discarded
-                                silently.
+        :param settings: Dictionary of element to merge to the settings.
+                         Non-used key/value pairs will be silently discarded.
         """
         for key, value in settings.iteritems():
             _key = "_%s" % key
@@ -81,90 +78,165 @@ class AuthenticationSessionData(object):
 
     @property
     def cookies(self):
-        """String R/W property."""
+        """
+        Getter for the session cookies.
+
+        :returns: An encoded string describing the session cookies. Not meant
+                  for public consumption.
+        """
         return str(self._cookies or "")
 
     @cookies.setter
     def cookies(self, value):
+        """
+        Setter for the session cookies.
+
+        :param value: An encoded string describing the session cookies.
+        """
         self._cookies = value
 
     @property
     def csrf_key(self):
         """
-        String R/W property.
+        Getter for the session csrf_key.
 
-        This is the key name of the CRSF token, which will include a unique ID.
-        The ID corresponds to the Shogun user ID.
+        :returns: The key name of the CRSF token, which will include a unique ID.
+                  The ID corresponds to the Shogun user ID.
         """
         return str(self._csrf_key or "")
 
     @csrf_key.setter
     def csrf_key(self, value):
+        """
+        Setter for the session csrf_key.
+
+        :param value: The key name of the CRSF token, which will include a unique ID.
+                      The ID corresponds to the Shogun user ID.
+        """
         self._csrf_key = value
 
     @property
     def csrf_value(self):
         """
-        String R/W property.
+        Getter for the session csrf_value.
 
-        This is the value of the Cross-Site Request Forgery token.
+        :returns: The value of the Cross-Site Request Forgery token.
         """
         return str(self._csrf_value or "")
 
     @csrf_value.setter
     def csrf_value(self, value):
+        """
+        Setter for the session csrf_value.
+
+        :param value: The value of the Cross-Site Request Forgery token.
+        """
         self._csrf_value = value
 
     @property
     def error(self):
-        """String R/W property."""
+        """
+        Getter for the session error, if any.
+
+        :returns: The error string if any, an empty string otherwise.
+        """
         return str(self._error or "")
 
     @error.setter
     def error(self, value):
+        """
+        Setter for the session error.
+
+        :param value: The error string if any, an empty string otherwise.
+        """
         self._error = value
 
     @property
     def host(self):
-        """String R/W property."""
+        """
+        Getter for the session hostname.
+
+        :returns: The session hostname.
+        """
         return str(self._host or "")
 
     @host.setter
     def host(self, value):
+        """
+        Setter for the session hostname.
+
+        :param value: The session hostname.
+        """
         self._host = value
 
     @property
     def product(self):
-        """String R/W property."""
+        """
+        Getter for the session product name.
+
+        :returns: The session product name, or "undefined".
+        """
         return str(self._product or "undefined")
 
     @product.setter
     def product(self, value):
+        """
+        Setter for the session product name.
+
+        :param value: The session product name (e.g. "rv", "shotgun", etc.).
+        """
         self._product = value
 
     @property
     def session_expiration(self):
-        """Int R/W property."""
+        """
+        Getter for the session expiration.
+
+        :returns: The session expiration, in seconds since January 1st 1970 UTC.
+        """
         return int(self._session_expiration or 0)
 
     @session_expiration.setter
     def session_expiration(self, value):
+        """
+        Setter for the session expiration.
+
+        :param value: The session expiration, in seconds since January 1st 1970 UTC.
+        """
         self._session_expiration = int(value)
 
     @property
     def session_id(self):
-        """String R/W property."""
+        """
+        Getter for the session id.
+
+        :returns: The session unique id.
+        """
         return str(self._session_id or "")
 
     @session_id.setter
     def session_id(self, value):
+        """
+        Setter for the session id.
+
+        :param value: The session unique id.
+        """
         self._session_id = value
 
     @property
     def user_id(self):
-        """String R/W property."""
+        """
+        Getter for the session user id.
+
+        :returns: The session user id.
+        """
         return str(self._user_id or "")
 
     @user_id.setter
     def user_id(self, value):
+        """
+        Setter for the session user id.
+
+        :param value: The session user id.
+        """
         self._user_id = value
