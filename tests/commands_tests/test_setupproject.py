c26ae98b6d94125e39e00d789f2a27f41d84a22d
Josh Tomlinson <josh.tomlinson@shotgunsoftware.com>
2018-01-11 09:31:18 -0500

    #35379 Adds localization of config during project setup if `core_api.yml` exists  (#579)

--- tests/commands_tests/test_setupproject.py
+++ tests/commands_tests/test_setupproject.py
@@ -11,7 +11,6 @@
 """
 Unit tests tank setup_project.
 """
-
 from __future__ import with_statement
 
 import os
@@ -44,14 +43,6 @@ class TestSetupProject(TankTestBase):
         patcher = patch_app_store()
         self._mock_store = patcher.start()
         self.addCleanup(patcher.stop)
-        self.second_project = {
-            "type": "Project",
-            "id": 7777,
-            "code": "another_project",
-        }
-        self.add_to_sg_mock_db(self.second_project)
-        project_root = os.path.join(self.tank_temp, "test_setup_project")
-        os.makedirs(project_root)
 
         # Make sure we have a version in the app store for all bundles.
         self._mock_store.add_engine("tk-engine", "v1.0.0")
@@ -61,32 +52,139 @@ class TestSetupProject(TankTestBase):
         self._mock_store.add_framework("tk-framework-test", "v1.0.0")
         self._mock_store.add_framework("tk-framework-2nd-level-dep", "v1.0.0")
 
+        # the std fixtures do not have a full core installation so ensure
+        # that we mock one that localize can pick up
+        self._fake_core_install = os.path.join(self.tank_temp, "fake_core_install")
+        if not os.path.exists(self._fake_core_install):
+            os.makedirs(os.path.join(self._fake_core_install, "install"))
+            os.makedirs(os.path.join(self._fake_core_install, "install", "core"))
+            os.makedirs(os.path.join(self._fake_core_install, "install", "core", "fake_core"))
+            os.makedirs(os.path.join(self._fake_core_install, "config"))
+            cfg_core = os.path.join(self._fake_core_install, "config", "core")
+            os.makedirs(cfg_core)
+            self.create_file(os.path.join(cfg_core, "shotgun.yml"), "{host: http://unit_test_mock_sg}")
+            self.create_file(os.path.join(cfg_core, "interpreter_Darwin.cfg"), "")
+            self.create_file(os.path.join(cfg_core, "interpreter_Linux.cfg"), "")
+            self.create_file(os.path.join(cfg_core, "interpreter_Windows.cfg"), "")
+
+
     @patch("tank.pipelineconfig_utils.resolve_all_os_paths_to_core")
     def test_setup_project(self, mocked=None):
         """
         Test setting up a Project.
         """
-        new_config_root = os.path.join(self.tank_temp, "test_setup_project_%s" % "config")
-
         def mocked_resolve_core_path(core_path):
             return {
                 "linux2": core_path,
                 "darwin": core_path,
                 "win32": core_path,
             }
-
         mocked.side_effect = mocked_resolve_core_path
+
+        # create new project
+        new_project = {
+            "type": "Project",
+            "id": 1234,
+            "code": "new_project_1234",
+        }
+        self.add_to_sg_mock_db(new_project)
+        # location where the config will be installed
+        new_config_root = os.path.join(self.tank_temp, "new_project_1234_config")
+        # location where the data will be installed
+        os.makedirs(os.path.join(self.tank_temp, "new_project_1234"))
+
         command = self.tk.get_command("setup_project")
         command.set_logger(logging.getLogger("/dev/null"))
         # Test we can setup a new project and it does not fail.
         command.execute({
-            "project_id": self.second_project["id"],
-            "project_folder_name": "test_setup_project",
+            "project_id": new_project["id"],
+            "project_folder_name": "new_project_1234",
             "config_uri": self.project_config,
             "config_path_mac": new_config_root if sys.platform == "darwin" else None,
             "config_path_win": new_config_root if sys.platform == "win32" else None,
             "config_path_linux": new_config_root if sys.platform.startswith("linux") else None,
         })
-        new_pc = tank.pipelineconfig_factory.from_path(new_config_root)
+
         # Check we get back our custom primary root name
+        new_pc = tank.pipelineconfig_factory.from_path(new_config_root)
         self.assertEqual(new_pc.get_data_roots().keys(), ["setup_project_root"])
+
+        # make sure the fake core didn't get copied across, e.g. that
+        # we didn't localize the setup
+        self.assertFalse(os.path.exists(
+            os.path.join(new_config_root, "install", "core", "bad_path")
+        ))
+
+        # make sure we have the core location files for this unlocalized setup
+        self.assertTrue(os.path.exists(
+            os.path.join(new_config_root, "install", "core", "core_Darwin.cfg")
+        ))
+
+    @patch("tank.pipelineconfig.PipelineConfiguration.get_install_location")
+    @patch("tank.pipelineconfig_utils.resolve_all_os_paths_to_core")
+    def test_setup_project_with_external_core(self, resolve_all_os_paths_to_core_mock, get_install_location_mock):
+        """
+        Test setting up a Project config that has a core/core_api.yml file included.
+        """
+        def mocked_resolve_core_path(core_path):
+            return {
+                "linux2": core_path,
+                "darwin": core_path,
+                "win32": core_path,
+            }
+        resolve_all_os_paths_to_core_mock.side_effect = mocked_resolve_core_path
+
+        def mocked_get_install_location():
+            return self._fake_core_install
+        get_install_location_mock.side_effect = mocked_get_install_location
+
+        # add a core_api.yml to our config that we are installing from, telling the
+        # setup project command to use this when running the localize portion of the setup.
+        core_api_path = os.path.join(self.project_config, "core", "core_api.yml")
+        with open(core_api_path, "wt") as fp:
+            fp.write("location:\n")
+            fp.write("   type: dev\n")
+            fp.write("   path: %s\n" % self.tank_source_path)
+
+        try:
+            # create new project
+            new_project = {
+                "type": "Project",
+                "id": 1235,
+                "code": "new_project_1235",
+            }
+            self.add_to_sg_mock_db(new_project)
+            new_config_root = os.path.join(self.tank_temp, "new_project_1235_config")
+            # location where the data will be installed
+            os.makedirs(os.path.join(self.tank_temp, "new_project_1235"))
+
+            command = self.tk.get_command("setup_project")
+            command.set_logger(logging.getLogger("/dev/null"))
+            # Test we can setup a new project and it does not fail.
+            command.execute({
+                "project_id": new_project["id"],
+                "project_folder_name": "new_project_1235",
+                "config_uri": self.project_config,
+                "config_path_mac": new_config_root if sys.platform == "darwin" else None,
+                "config_path_win": new_config_root if sys.platform == "win32" else None,
+                "config_path_linux": new_config_root if sys.platform.startswith("linux") else None,
+            })
+
+            # Check we get back our custom primary root name
+            new_pc = tank.pipelineconfig_factory.from_path(new_config_root)
+            self.assertEqual(new_pc.get_data_roots().keys(), ["setup_project_root"])
+
+            # the 'fake' core that we mocked earlier has a 'bad_path' folder
+            self.assertFalse(os.path.exists(
+                os.path.join(new_config_root, "install", "core", "bad_path")
+            ))
+
+            # instead we expect a full installl
+            self.assertTrue(os.path.exists(
+                os.path.join(new_config_root, "install", "core", "python", "tank", "errors.py")
+            ))
+
+        finally:
+            os.remove(core_api_path)
+
+
