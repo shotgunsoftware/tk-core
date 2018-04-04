75c4987bcc496412d632171ccae6bda66be7a314
Manne Öhrström <manne.ohrstrom@autodesk.com>
2018-04-04 14:35:42 +0100

    #46891 Support for listfields in symlinks (#631)
    Adds support for list field token back references in symlinks, allowing for the creation for for example symlinks referring to asset types which are part of the folder hierarchy. The syntax for these back references are '$EntityType.list_field_name', e.g. for example '$Asset.sg_asset_type'.

--- tests/folder_tests/test_symlinks.py
+++ tests/folder_tests/test_symlinks.py
@@ -10,37 +10,44 @@
 
 import os
 import sys
-import unittest
-import shutil
-from mock import Mock
-import tank
-from tank_vendor import yaml
-from tank import TankError
-from tank import hook
 from tank import folder
 from tank_test.tank_test_base import *
 
          
 class TestSymlinks(TankTestBase):
     """Test Symbolic link support."""
+
     def setUp(self):
         
         super(TestSymlinks, self).setUp()
         self.setup_fixtures(parameters = {"core": "core.override/symlinks_core"})
         
-        self.shot_aaa = {"type": "Shot", 
-                         "id": 1,
-                         "code": "aaa",
-                         "project": self.project}
+        self.shot_aaa = {
+            "type": "Shot",
+            "id": 1,
+            "code": "aaa",
+            "project": self.project
+        }
 
+        self.asset_bbb = {
+            "type": "Asset",
+            "id": 1,
+            "code": "bbb",
+            "sg_asset_type": "vehicle",
+            "project": self.project
+        }
 
-        self.add_to_sg_mock_db([self.shot_aaa])
+        self.add_to_sg_mock_db(
+            [self.shot_aaa, self.asset_bbb]
+        )
 
         self.aaa = os.path.join(self.project_root, "aaa")
         self.aaa_work = os.path.join(self.project_root, "aaa", "work")
         self.aaa_link = os.path.join(self.project_root, "aaa", "foo")
-        
 
+        self.bbb = os.path.join(self.project_root, "vehicle", "bbb")
+        self.bbb_work = os.path.join(self.project_root, "vehicle", "bbb", "work")
+        self.bbb_link = os.path.join(self.project_root, "vehicle", "bbb", "test")
 
     def test_create_symlink(self):
         """
@@ -49,13 +56,14 @@ class TestSymlinks(TankTestBase):
         self.assertFalse(os.path.exists(self.aaa))
         self.assertFalse(os.path.exists(self.aaa_work))
         self.assertFalse(os.path.exists(self.aaa_link))
-        
 
-        folder.process_filesystem_structure(self.tk, 
-                                            self.shot_aaa["type"], 
-                                            self.shot_aaa["id"], 
-                                            preview=False,
-                                            engine=None)
+        folder.process_filesystem_structure(
+            self.tk,
+            self.shot_aaa["type"],
+            self.shot_aaa["id"],
+            preview=False,
+            engine=None
+        )
 
         self.assertTrue(os.path.exists(self.aaa))
         self.assertTrue(os.path.exists(self.aaa_work))
@@ -66,8 +74,29 @@ class TestSymlinks(TankTestBase):
         else:
             # no support on windows
             self.assertFalse(os.path.exists(self.aaa_link))
-        
 
+    def test_list_field_token(self):
+        """
+        Test that we can reference list field tokens in the symlink definition
+        """
+        self.assertFalse(os.path.exists(self.bbb))
+        self.assertFalse(os.path.exists(self.bbb_work))
+        self.assertFalse(os.path.exists(self.bbb_link))
 
+        folder.process_filesystem_structure(
+            self.tk,
+            self.asset_bbb["type"],
+            self.asset_bbb["id"],
+            preview=False,
+            engine=None
+        )
 
-        
+        self.assertTrue(os.path.exists(self.bbb))
+        self.assertTrue(os.path.exists(self.bbb_work))
+        if sys.platform != "win32":
+            self.assertTrue(os.path.lexists(self.bbb_link))
+            self.assertTrue(os.path.islink(self.bbb_link))
+            self.assertEquals(os.readlink(self.bbb_link), "../Stuff/project_code/vehicle/bbb")
+        else:
+            # no support on windows
+            self.assertFalse(os.path.exists(self.bbb_link))
