e815d2f0033b766656150b62c1cb07d6a6dc8d97
Adrian Krupa <adrian.krupa91@gmail.com>
2018-06-11 12:39:45 +0200

    improve git descriptor performance

--- python/tank/descriptor/io_descriptor/git.py
+++ python/tank/descriptor/io_descriptor/git.py
@@ -58,7 +58,7 @@ class IODescriptorGit(IODescriptorDownloadable):
         self._sanitized_repo_path = self._path.replace(os.path.sep, "/")
 
     @LogManager.log_timing
-    def _clone_then_execute_git_commands(self, target_path, commands):
+    def _clone_then_execute_git_commands(self, target_path, commands, depth=None, ref=None):
         """
         Clones the git repository into the given location and
         executes the given list of git commands::
@@ -83,6 +83,8 @@ class IODescriptorGit(IODescriptorDownloadable):
 
         :param target_path: path to clone into
         :param commands: list git commands to execute, e.g. ['checkout x']
+        :param depth: depth of the clone, allows shallow clone
+        :param ref: git ref to checkout - it can be commit, tag or branch
         :returns: stdout and stderr of the last command executed as a string
         :raises: TankGitError on git failure
         """
@@ -111,7 +113,9 @@ class IODescriptorGit(IODescriptorDownloadable):
         # complications in cleanup scenarios and with file copying. We want
         # each repo that we clone to be completely independent on a filesystem level.
         log.debug("Git Cloning %r into %s" % (self, target_path))
-        cmd = "git clone --no-hardlinks -q \"%s\" \"%s\"" % (self._path, target_path)
+        depth = '--depth %s' % depth if depth else ''
+        ref = '-b %s' % ref if ref else ''
+        cmd = "git clone --no-hardlinks -q \"%s\" %s \"%s\" %s" % (self._path, ref, target_path, depth)
 
         # Note that we use os.system here to allow for git to pop up (in a terminal
         # if necessary) authentication prompting. This DOES NOT seem to be possible
@@ -177,7 +181,7 @@ class IODescriptorGit(IODescriptorDownloadable):
         # return the last returned stdout/stderr
         return output
 
-    def _tmp_clone_then_execute_git_commands(self, commands):
+    def _tmp_clone_then_execute_git_commands(self, commands, depth=None, ref=None):
         """
         Clone into a temp location and executes the given
         list of git commands.
@@ -190,7 +194,7 @@ class IODescriptorGit(IODescriptorDownloadable):
         clone_tmp = os.path.join(tempfile.gettempdir(), "sgtk_clone_%s" % uuid.uuid4().hex)
         filesystem.ensure_folder_exists(clone_tmp)
         try:
-            return self._clone_then_execute_git_commands(clone_tmp, commands)
+            return self._clone_then_execute_git_commands(clone_tmp, commands, depth, ref)
         finally:
             log.debug("Cleaning up temp location '%s'" % clone_tmp)
             shutil.rmtree(clone_tmp, ignore_errors=True)
@@ -218,7 +222,7 @@ class IODescriptorGit(IODescriptorDownloadable):
         try:
             log.debug("%r: Probing if a connection to git can be established..." % self)
             # clone repo into temp folder
-            self._tmp_clone_then_execute_git_commands([])
+            self._tmp_clone_then_execute_git_commands([], depth=1)
             log.debug("...connection established")
         except Exception as e:
             log.debug("...could not establish connection: %s" % e)
--- python/tank/descriptor/io_descriptor/git_tag.py
+++ python/tank/descriptor/io_descriptor/git_tag.py
@@ -9,6 +9,7 @@
 # not expressly granted therein are reserved by Shotgun Software Inc.
 import os
 import copy
+import re
 
 from .git import IODescriptorGit
 from ..errors import TankDescriptorError
@@ -147,8 +148,7 @@ class IODescriptorGitTag(IODescriptorGit):
         """
         try:
             # clone the repo, checkout the given tag
-            commands = ["checkout -q \"%s\"" % self._version]
-            self._clone_then_execute_git_commands(destination_path, commands)
+            self._clone_then_execute_git_commands(destination_path, [], depth=1, ref=self._version)
         except Exception as e:
             raise TankDescriptorError(
                 "Could not download %s, "
@@ -204,8 +204,14 @@ class IODescriptorGitTag(IODescriptorGit):
         try:
             # clone the repo, list all tags
             # for the repository, across all branches
-            commands = ["tag"]
-            git_tags = self._tmp_clone_then_execute_git_commands(commands).split("\n")
+            commands = ["ls-remote -q --tags %s" % self._path]
+            tags = self._tmp_clone_then_execute_git_commands(commands, depth=1).split("\n")
+            regex = re.compile('.*refs/tags/([^^]*)$')
+            git_tags = []
+            for tag in tags:
+                m = regex.match(tag)
+                if m:
+                    git_tags.append(m.group(1))
 
         except Exception as e:
             raise TankDescriptorError(
