d5b178bd6481d3417eee482d6d42bc1a238b887b
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2018-09-13 15:56:38 -0400

    more docs in the hook

--- hooks/bootstrap.py
+++ hooks/bootstrap.py
@@ -25,10 +25,8 @@ class BootstrapHook(get_hook_baseclass()):
 
     def init(self, shotgun, pipeline_configuration_id, configuration_descriptor, **kwargs):
         """
-        This method is invoked when the hook is instantiated.
-
-        It can be used to cache information so it can be reused, for example a
-        shotgun connection to external site.
+        This method is invoked when the hook is instantiated and provides some information
+        about the site and the configuration that are being bootstrapped into.
 
         The ``kwargs`` is there for forward compatibility with future versions of Toolkit.
 
@@ -83,45 +81,45 @@ class BootstrapHook(get_hook_baseclass()):
 # The following is an example of how you can use this hook to download
 # applications that are uploaded to a custom entity in Shotgun. Copy this
 # file into your core/hooks folder and
-
+#
 # class BootstrapHook(get_hook_baseclass()):
 #     """
 #     This is an example bootstrap hook. If allows to download certain bundles from
 #     Shotgun instead of official source for the bundle.
 #     """
-
+#
 #     # Change this entity name to one that is unused on your Shotgun site. This custom
 #     # non project entity type will be used to upload bundles to Shotgun.
 #     CUSTOM_ENTITY  = "CustomNonProjectEntity01"
-
+#
 #     def can_cache_bundle(self, descriptor):
 #         """
 #         Returns true if the descriptor has been cached in Shotgun.
-
+#
 #         :param descriptor: Descriptor of the bundle that needs to be cached.
 #         :type descriptor: :class:`~sgtk.descriptor.Descriptor`
-
+#
 #         :returns: ``True`` if the bundle is cached in Shotgun, ``False`` otherwise.
 #         """
 #         if descriptor.get_dict()["type"] in ["app_store", "git"] and self._get_bundle_attachment(descriptor):
 #             return True
 #         else:
 #             return False
-
+#
 #     def _get_bundle_attachment(self, descriptor):
 #         """
 #         Retrieves the attachment associated to this descriptor in Shotgun.
-
+#
 #         :param descriptor: Descriptor of the bundle that needs to be cached.
 #         :type descriptor: :class:`~sgtk.descriptor.Descriptor`
-
+#
 #         :returns: The attachment entity dict.
 #         :rtype: dict
 #         """
 #         # This method gets invoked twice by the bootstrap hook, but will only be
 #         # invoked if the bundle is missing from disk so it is not worth putting a
 #         # caching system in place for the method.
-
+#
 #         # We're using the descriptor uri, i.e. sgtk:descriptor:app_store?name=tk-core&version=v0.18.150,
 #         # as the code for the entity. If an entry is found and there is an attachment,
 #         # the bundle can be downloaded from Shotgun.
@@ -130,7 +128,7 @@ class BootstrapHook(get_hook_baseclass()):
 #         # all bundles that need to be cached into Shotgun and pushes them to
 #         # Shotgun. Part of this workflow can be automated with the developer/populate_bundle_cache.py
 #         # script, which will download locally every single bundle for a given config.
-
+#
 #         entity = self.shotgun.find_one(
 #             self.CUSTOM_ENTITY,
 #             [["code", "is", descriptor.get_uri()]], ["sg_uploaded_bundle"]
@@ -139,15 +137,15 @@ class BootstrapHook(get_hook_baseclass()):
 #             return entity["sg_uploaded_bundle"]
 #         else:
 #             return None
-
+#
 #     def populate_bundle_cache_entry(self, destination, descriptor, **kwargs):
 #         """
 #         This method will retrieve the bundle from the Shotgun site and unpack it
 #         in the destination folder.
-
+#
 #         :param destination: Folder where the bundle needs to be written. Note that this is not
 #             the final destination folder inside the bundle cache.
-
+#
 #         :param descriptor: Descriptor of the bundle that needs to be cached.
 #         :type descriptor: :class:`~sgtk.descriptor.Descriptor`
 #         """
