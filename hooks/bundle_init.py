f8a08963319bf19f9120fef44aead78749875a8a
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2018-09-20 12:38:39 -0400

    documented all the core hooks

--- hooks/bundle_init.py
+++ hooks/bundle_init.py
@@ -1,26 +1,30 @@
-# Copyright (c) 2013 Shotgun Software Inc.
-# 
+# Copyright (c) 2018 Shotgun Software Inc.
+#
 # CONFIDENTIAL AND PROPRIETARY
-# 
-# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit 
+#
+# This work is provided "AS IS" and subject to the Shotgun Pipeline Toolkit
 # Source Code License included in this distribution package. See LICENSE.
-# By accessing, using, copying or modifying this work you indicate your 
-# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights 
+# By accessing, using, copying or modifying this work you indicate your
+# agreement to the Shotgun Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Shotgun Software Inc.
 
 """
-Hook that gets executed every time an bundle has fully initialized.
-
+Hook that gets executed every time a bundle is fully initialized.
 """
 
 from tank import Hook
-import os
- 
+
+
 class BundleInit(Hook):
-    
+
     def execute(self, bundle, **kwargs):
         """
-        Gets executed when the Toolkit bundle super class __init__
-        is fully initialized.
+        Executed when the Toolkit bundle is fully initialized.
+
+        The default implementation does nothing.
+
+        :param bundle: The Toolkit bundle that has been initialized.
+        :type bundle: :class:`~sgtk.platform.Engine`, :class:`~sgtk.platform.Framework`
+            or :class:`~sgtk.platform.Application`
         """
         pass
