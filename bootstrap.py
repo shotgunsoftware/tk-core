02ec1159239c1b501923f6554efb8778c3cd5e12
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2019-03-14 23:36:26 -0400

    launching maya 2019

--- bootstrap.py
+++ bootstrap.py
@@ -1,12 +1,16 @@
 from __future__ import print_function
 import sgtk
+import sys
 
-sgtk.LogManager().initialize_custom_handler()
+if "--debug" in sys.argv:
+    sgtk.LogManager().initialize_custom_handler()
 
 user = sgtk.authentication.ShotgunAuthenticator().get_user()
 manager = sgtk.bootstrap.ToolkitManager(user)
-manager.do_shotgun_config_lookup = False
-manager.base_configuration = "sgtk:descriptor:path?path=~/gitlocal/tk-config-default2"
+manager.plugin_id = "basic.very"
+manager.base_configuration = "sgtk:descriptor:app_store?name=tk-config-default2"
 
 manager.progress_callback = lambda value, msg: print("%s%% - %s" % (int(value * 100), msg))
 engine = manager.bootstrap_engine("tk-shell", user.create_sg_connection().find_one("Project", [["name", "is", "Demo: Game"]]))
+
+engine.commands["maya_2019"]["callback"]()
