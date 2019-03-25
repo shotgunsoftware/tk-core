9a51a0bc09c937d09c5770f67173c2d424e749a3
Will Cavanagh <will.cavanagh@autodesk.com>
2019-03-25 17:24:44 -0400

    Refactor, cleanup and update docstrings.

--- python/tank/descriptor/io_descriptor/github_release.py
+++ python/tank/descriptor/io_descriptor/github_release.py
@@ -11,6 +11,7 @@ from ...util.shotgun import download
 
 import json
 import os
+import re
 import urllib2
 
 from .downloadable import IODescriptorDownloadable
@@ -22,45 +23,7 @@ log = LogManager.get_logger(__name__)
 
 class IODescriptorGithubRelease(IODescriptorDownloadable):
     """
-    Represents a shotgun entity to which apps have been attached.
-    This can be an attachment field on any entity. Typically it will be
-    a pipeline configuration. In that configuration, the descriptor represents
-    a 'cloud based configuration'. It could also be a custom entity or non-project
-    entity in the case you want to store a descriptor (app, engine or config)
-    that can be easily accessed from any project.
-
-    There are two ways of addressing an entity.
-
-    {
-     type: shotgun,
-     entity_type: CustomEntity01,   # entity type
-     name: tk-foo,                  # name of the record in shotgun (i.e. 'code' field)
-     project_id: 123,               # optional project id. If omitted, name is assumed to be unique.
-     field: sg_config,              # attachment field where payload can be found
-     version: 456                   # attachment id of particular attachment
-    }
-
-    or
-
-    {
-     type: shotgun,
-     entity_type: CustomEntity01,   # entity type
-     id: 123,                       # id of the record in shotgun (i.e. 'id' field)
-     field: sg_config,              # attachment field where payload can be found
-     version: 456                   # attachment id of particular attachment
-    }
-
-    This can for example be used for attaching items to a pipeline configuration.
-    Create an attachment field named sg_config, upload a zip file, and use the following
-    descriptor:
-
-    {type: shotgun, entity_type: PipelineConfiguration,
-     name: primary, project_id: 123, field: sg_config, version: 1341}
-
-    When a new zip file is uploaded, the attachment id (e.g. version) changes, resulting in
-    a new descriptor.
-
-    The latest version is defined as the current record available in Shotgun.
+    Represents a Github Release.
     """
 
     def __init__(self, descriptor_dict, sg_connection):
@@ -121,9 +84,6 @@ class IODescriptorGithubRelease(IODescriptorDownloadable):
         :param destination_path: The directory path to which the shotgun entity is to be
         downloaded to.
         """
-        # @todo: should we use Github API to get the zipball URL in case this URL changes in
-        # the future?  would mean an additional transaction (to get URL) but would mean
-        # only API changes would cause this to stop working.
         url = "https://github.com/{organization}/{system_name}/archive/{version}.zip"
         url = url.format(
             organization=self._organization,
@@ -140,91 +100,137 @@ class IODescriptorGithubRelease(IODescriptorDownloadable):
                 "Failed to download %s from %s. Error: %s" % (self, url, e)
             )
 
-    def _get_github_release_versions(self, latest_only=False):
-        url = "https://api.github.com/repos/{organization}/{system_name}/releases"
-        url = url.format(organization=self._organization, system_name=self.get_system_name())
+    def _get_github_releases(self, latest_only=False, url=None):
+        """
+        Helper method for interacting witht he Github API.  Finds releases using the
+        Github API for the organization and repo of this Descriptor instance, and builds
+        a list of tag names.  If the results are paginated and there is an additional page after
+        the current page, that page's URL will also be returned.
+
+        :param latest_only: If True asks the Github API for the latest release only.
+        :param url: If specified, gets releases from this URL instead of building one.  This is
+            useful for following pagination links.
+        :return: tuple of list, string or None.  The list will contain a list of strings for the
+            tag names (versions) that were found.  The second position will contain a URL for the
+            next page if the results were paginated, or None if this is the last page of results.
+        """
+        if not url:
+            # If no URL was provided, build one for this descriptor.
+            url = "https://api.github.com/repos/{organization}/{system_name}/releases"
+            url = url.format(organization=self._organization, system_name=self.get_system_name())
+        if self._sg_connection.config.proxy_handler:
+            # Grab proxy server settings from the shotgun API.
+            opener = urllib2.build_opener(self._sg_connection.config.proxy_handler)
+            urllib2.install_opener(opener)
         if latest_only:
             url += "/latest"
-        
+        # Find the "next" rel link from the headers, if one is present, and return it, so
+        # that it can be followed if need be.
+        next_link = None
         try:
             response = urllib2.urlopen(url)
-            # @todo this will only load a max of 30 releases -- if more releases exist
-            # a ?page=2 url will be included in the "link" field in the response
-            # headers.  We can continue to get more as needed when applying a constraint
-            # pattern, working under the assumption that release ordering chronologically
-            # and numerically will be identical.
             response_data = json.load(response)
-        except urllib2.URLError as e:
-            raise TankDescriptorError("Unable to contact Github API: %s" % e)
+            m = re.search(r"<(.+)>; rel=\"next\"", response.headers.get("link", ""))
+            if m:
+                next_link = m.group(1)
         except urllib2.HTTPError as e:
             if e.code == 404:
-                # Github API gives a 404 when no releases have been published
-                return []
+                # Github API gives a 404 when no releases have been published.  Additionally,
+                # 404 could mean a non-existant or private repo, but this should have been caught
+                # by has_remote_access().
+                return ([], None)
             else:
-                raise TankDescriptorError("Error communicatingh with Github API: %s" % e)
+                raise TankDescriptorError("Error communicating with Github API: %s" % e)
+        except urllib2.URLError as e:
+            raise TankDescriptorError("Unable to contact Github API: %s" % e)
         # zipballs are stored under the tag name, not the release name,
         # so that's the "version" name we want
         if latest_only:
-            return [response_data["tag_name"]]
-        return [release["tag_name"] for release in response_data]
+            return ([response_data["tag_name"]], next_link)
+        return ([release["tag_name"] for release in response_data], next_link)
 
     def get_latest_version(self, constraint_pattern=None):
         """
         Returns a descriptor object that represents the latest version.
 
-        .. note:: The concept of constraint patterns doesn't apply to
-                  shotgun attachment ids and any data passed via the
-                  constraint_pattern argument will be ignored by this
-                  method implementation.
+        :param constraint_pattern: If this is specified, the query will be constrained
+               by the given pattern. Version patterns are on the following forms:
 
-        :param constraint_pattern: This parameter is unused and remains here to be compatible
-            with the expected signature for this method.
+                - v0.1.2, v0.12.3.2, v0.1.3beta - a specific version
+                - v0.12.x - get the highest v0.12 version
+                - v1.x.x - get the highest v1 version
 
-        :returns: IODescriptorShotgunEntity object
+        :returns: IODescriptorGithubRelease object
         """
         if constraint_pattern:
-            versions = self._get_github_release_versions()
-            version = self._find_latest_tag_by_pattern(versions, constraint_pattern)
+            # get the list of releases from github API.  If we don't find a match on
+            # the first request, and there was an additional page linked, follow the link
+            # and see if we find a match.  Repeat until a match is found, or there are no
+            # more pages.
+            can_fetch_more = True
+            next_url = None
+            version = None
+            while not version and can_fetch_more:
+                versions, next_url = self._get_github_releases(url=next_url)
+                version = self._find_latest_tag_by_pattern(versions, constraint_pattern)
+                can_fetch_more = next_url is not None
         else:
-            version = self._get_github_release_versions(latest_only=True)
+            # Otherwise, we can ask for the latest version from the github api
+            # directly.
+            versions, _ = self._get_github_releases(latest_only=True)
+            version = versions[0] if versions else None
         if version is None or version == self.get_version():
-            # there is no latest release, or the latest release is this one, so return this descriptor.
+            # There is no latest release, or the latest release is this one, so return this descriptor.
             return self
-        # otherwise generate a descriptor for the version we found
+        # If a release was found, generate a descriptor for that version and return it.
         descriptor_dict = {
             "organization": self._organization,
             "repository": self.get_system_name(),
             "version": version,
+            "type": "github_release",
         }
-        return IODescriptorGithubRelease(descriptor_dict, self._sg_connection)
+        desc = IODescriptorGithubRelease(descriptor_dict, self._sg_connection)
+        desc.set_cache_roots(self._bundle_cache_root, self._fallback_roots)
+        return desc
 
     def get_latest_cached_version(self, constraint_pattern=None):
         """
         Returns a descriptor object that represents the latest version
         that is locally available in the bundle cache search path.
 
-        .. note:: The concept of constraint patterns doesn't apply to
-                  shotgun attachment ids and any data passed via the
-                  constraint_pattern argument will be ignored by this
-                  method implementation.
+        :param constraint_pattern: If this is specified, the query will be constrained
+               by the given pattern. Version patterns are on the following forms:
 
-        :param constraint_pattern: This parameter is unused and remains here to be compatible
-            with the expected signature for this method.
+                - v0.1.2, v0.12.3.2, v0.1.3beta - a specific version
+                - v0.12.x - get the highest v0.12 version
+                - v1.x.x - get the highest v1 version
 
         :returns: instance deriving from IODescriptorBase or None if not found
         """
-        if constraint_pattern:
-            log.warning("%s does not support version constraint patterns." % self)
+        all_versions = self._get_locally_cached_versions()
+        version_numbers = all_versions.keys()
 
-        # not possible to determine what 'latest' means in this case
-        # so check if the current descriptor exists on disk and in this
-        # case return it
-        if self.get_path():
-            return self
-        else:
-            # no cached version exists
+        if not version_numbers:
             return None
-        
+
+        version_to_use = self._find_latest_tag_by_pattern(version_numbers, constraint_pattern)
+        if version_to_use is None:
+            return None
+
+        # make a descriptor dict
+        descriptor_dict = {
+            "organization": self._organization,
+            "repository": self.get_system_name(),
+            "version": version_to_use,
+            "type": "github_release",
+        }
+
+        # and return a descriptor instance
+        desc = IODescriptorGithubRelease(descriptor_dict, self._sg_connection)
+        desc.set_cache_roots(self._bundle_cache_root, self._fallback_roots)
+
+        log.debug("Latest cached version resolved to %r" % desc)
+        return desc
 
     def has_remote_access(self):
         """
@@ -235,14 +241,40 @@ class IODescriptorGithubRelease(IODescriptorDownloadable):
 
         :return: True if a remote is accessible, false if not.
         """
-        # check if we can connect to Shotgun
+        # check if we can make api request for the specified repo
         can_connect = True
+        url = "https://api.github.com/repos/{organization}/{system_name}"
+        url = url.format(organization=self._organization, system_name=self.get_system_name())
+        if self._sg_connection.config.proxy_handler:
+            # Grab proxy server settings from the shotgun API
+            opener = urllib2.build_opener(self._sg_connection.config.proxy_handler)
+            urllib2.install_opener(opener)
         try:
             log.debug("%r: Probing if a connection to Github can be established..." % self)
-            pass
-            # @todo: check GH connection
-            log.debug("...connection established!")
-        except Exception as e:
+            # ensure we get response code 200
+            response_code = urllib2.urlopen(url).getcode()
+            # Unfortunately, to prevent probing private repos, GH API also gives a 404 response
+            # for private repos accessed without a token, so there's no way to helpfully warn the
+            # user if they try to download from a private repo.
+            can_connect = response_code == 200
+            # @todo should we deal with redirects?
+            # 301 Permanent redirection. The URI you used to make the request has been superseded by the one specified in the Location header field. This and all future requests to this resource should be directed to the new URI.
+            # 302, 307 Temporary redirection. The request should be repeated verbatim to the URI specified in the Location header field but clients should continue to use the original URI for future requests.
+            if can_connect:
+                log.debug("...connection established!")
+            else:
+                log.debug("...got unexpected response code %s" % response_code)
+        except urllib2.URLError as e:
             log.debug("...could not establish connection: %s" % e)
             can_connect = False
         return can_connect
+
+
+
+
+
+
+
+
+
+
