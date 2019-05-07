08c5e60730a2a880da0f241b59753d7678c408ae
Philip Scadding <philip.scadding@autodesk.com>
2019-05-07 14:45:36 +0100

    add white space

--- python/tank/descriptor/io_descriptor/base.py
+++ python/tank/descriptor/io_descriptor/base.py
@@ -145,7 +145,7 @@ class IODescriptorBase(object):
         required_set = set(required)
         # Add deny_platforms and disabled to the list of optional parameters as these are globally supported across
         # all descriptors and are used by the environment code to check if an item is disabled.
-        optional_set = set(optional + ["deny_platforms","disabled"])
+        optional_set = set(optional + ["deny_platforms", "disabled"])
 
         if not required_set.issubset(desc_keys_set):
             missing_keys = required_set.difference(desc_keys_set)
