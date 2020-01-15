8cc0454108c5bf507b218a11263d23201a8fdd6f
Jean-François Boismenu <jean.francois.boismenu@autodesk.com>
2020-01-15 15:01:18 -0500

    fixing prints

--- tests/fixtures/mockgun/update_schemas.py
+++ tests/fixtures/mockgun/update_schemas.py
@@ -12,6 +12,7 @@
 This script will update the Shotgun schema for Mockgun.
 """
 
+from __future__ import print_function
 import sys
 
 sys.path.append("../../../python")
@@ -20,9 +21,9 @@ from sgtk.authentication import ShotgunAuthenticator
 from tank_vendor.shotgun_api3.lib import mockgun
 
 # Log on a site.
-print "This script will update the Shotgun schema for Mockgun."
-print "Please enter your credentials for the site you wish to clone the schema from. Ideally this would be "\
-    "a site that has a clean schema like a new site."
+print("This script will update the Shotgun schema for Mockgun.")
+print("Please enter your credentials for the site you wish to clone the schema from. Ideally this would be "\
+    "a site that has a clean schema like a new site.")
 user = ShotgunAuthenticator().get_user_from_prompt()
 
 # Retrieve the schema folder validation
@@ -30,7 +31,7 @@ sg = user.create_sg_connection()
 schema = sg.schema_read()
 schema_entity = sg.schema_entity_read()
 
-print "Validating schema..."
+print("Validating schema...")
 errors = []
 
 # Dictionary of entities that need to be present.
@@ -53,10 +54,10 @@ for entity, fields in schema_requirements.iteritems():
             errors.append("Missing field '%s' on entity 'PipelineConfiguration'." % field)
 
 if errors:
-    print "There are problems with the schema:"
+    print("There are problems with the schema:")
     for e in errors:
-        print "-", e
+        print("-", e)
 else:
-    print "Saving schema..."
+    print("Saving schema...")
     mockgun.generate_schema(sg, "schema.pickle", "schema_entity.pickle")
-    print "Schema cloning completed!"
+    print("Schema cloning completed!")
