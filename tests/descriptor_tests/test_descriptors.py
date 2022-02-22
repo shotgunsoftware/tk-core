4edbdd932b0798f2d5130e72537f7b165d3de469
Norberto Moreno <norbermv.dev@gmail.com>
2022-02-21 21:24:20 -0500

    Fixing dynamic paths.

--- tests/descriptor_tests/test_descriptors.py
+++ tests/descriptor_tests/test_descriptors.py
@@ -643,24 +643,39 @@ class TestDescriptorSupport(TankTestBase):
         """
         Test that shallow git clones are not enabled with git_branch descriptors.
         """
+        self.git_repo_uri = os.path.join(self.fixtures_root, "tk-multi-shotgupanel.git")
+        target_path = os.path.join("tmp", "foo")
         desc = self.tk.pipeline_configuration.get_app_descriptor(
             {
                 "type": "git_branch",
-                "path": "https://github.com/shotgunsoftware/tk-multi-shotgunpanel.git",
+                "path": self.git_repo_uri,
                 "branch": "master",
                 "version": "6547378",
             }
         )
+        # import sys
+        # sys.path.append(
+        #     r"/Applications/PyCharm.app/Contents/debug-eggs/pydevd-pycharm.egg")
+        # import pydevd
+        # pydevd.settrace('localhost', port=5490, stdoutToServer=True,
+        #                 stderrToServer=True)
+
         self.assertEqual(
             desc._io_descriptor._validate_git_commands(
-                "/tmp/foo", depth=1, ref="master"
+                target_path, depth=1, ref="master"
             ),
-            'git clone --no-hardlinks -q "https://github.com/shotgunsoftware/tk-multi-shotgunpanel.git" -b master "/tmp/foo" ',
-        )
+            'git clone --no-hardlinks -q "%s" %s "%s" ' % (
+            self.git_repo_uri,
+            "-b master",
+            target_path,
+        ))
         self.assertEqual(
-            desc._io_descriptor._validate_git_commands("/tmp/foo", ref="master"),
-            'git clone --no-hardlinks -q "https://github.com/shotgunsoftware/tk-multi-shotgunpanel.git" -b master "/tmp/foo" ',
-        )
+            desc._io_descriptor._validate_git_commands(target_path, ref="master"),
+            'git clone --no-hardlinks -q "%s" %s "%s" ' % (
+                self.git_repo_uri,
+                "-b master",
+                target_path,
+            ))
 
 
 class TestConstraintValidation(unittest2.TestCase):
--- tests/descriptor_tests/test_io_descriptors.py
+++ tests/descriptor_tests/test_io_descriptors.py
@@ -221,12 +221,13 @@ class TestIODescriptors(ShotgunTestBase):
         """
         Tests that Short integer commit hashes descriptors are converted to a str type.
         """
+        self.git_repo_uri = os.path.join(self.fixtures_root, "tk-multi-shotgunpanel.git")
         sg = self.mockgun
         location = {
             "type": "git_branch",
             "version": 6547378,
             "branch": "master",
-            "path": "https://github.com/shotgunsoftware/tk-multi-shotgunpanel.git",
+            "path": self.git_repo_uri
         }
         d = sgtk.descriptor.create_descriptor(
             sg, sgtk.descriptor.Descriptor.APP, location,
@@ -235,8 +236,8 @@ class TestIODescriptors(ShotgunTestBase):
         # f._io_descriptor._get_bundle_cache_path("/full/path/to/bundle_cache/"))
         # Short hashes works with integer commit descriptors
         self.assertEqual(
-            d._io_descriptor._get_bundle_cache_path("/full/path/to/bundle_cache/"),
-            "/full/path/to/bundle_cache/gitbranch/tk-multi-shotgunpanel.git/6547378",
+            d._io_descriptor._get_bundle_cache_path(self.cache_root),
+            os.path.join(self.cache_root, "gitbranch", "tk-multi-shotgunpanel.git", "6547378")
         )
 
     def test_download_receipt(self):
