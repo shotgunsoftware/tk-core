3e91d85bbe2f8649f6fd4857973e282baad82487
Norberto Moreno <norbermv.dev@gmail.com>
2022-03-30 16:11:55 -0500

    Auto-updated documentation based on branch/tag master

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: 'v0.20.12',
+    VERSION: 'master',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
