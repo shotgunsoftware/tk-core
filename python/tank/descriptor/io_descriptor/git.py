41dd74b498096fdd428f1bff152e196650ffcafe
Mathieu <923463-mathbou@users.noreply.gitlab.com>
2023-08-24 01:25:29 +0200

    🎨 black

--- python/tank/descriptor/io_descriptor/git.py
+++ python/tank/descriptor/io_descriptor/git.py
@@ -266,10 +266,12 @@ class IODescriptorGit(IODescriptorDownloadable, metaclass=_IODescriptorGitCache)
         # make sure item exists locally
         self.ensure_local()
         # copy descriptor into target.
-        shutil.copytree(self.get_path(),
-                        target_path,
-                        ignore=shutil.ignore_patterns(*(skip_list or [])),
-                        dirs_exist_ok=True)
+        shutil.copytree(
+            self.get_path(),
+            target_path,
+            ignore=shutil.ignore_patterns(*(skip_list or [])),
+            dirs_exist_ok=True,
+        )
 
     def _get_git_clone_commands(
         self, target_path, depth=None, ref=None, is_latest_commit=None
