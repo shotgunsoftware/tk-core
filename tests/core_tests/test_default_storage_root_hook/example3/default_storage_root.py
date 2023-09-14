35f89bee69263b47fbaaf827fd9e6b708067f99f
Norberto Moreno Velez <norbermv.dev@gmail.com>
2023-09-14 15:48:04 -0500

    Modify the Project custom field from default_storage_root example3 hook to make sense with the hook approach.

--- tests/core_tests/test_default_storage_root_hook/example3/default_storage_root.py
+++ tests/core_tests/test_default_storage_root_hook/example3/default_storage_root.py
@@ -10,8 +10,8 @@ log = sgtk.LogManager.get_logger(__name__)
 class DefaultStorageRoot(Hook):
     def execute(self, storage_roots, project_id=None, metadata=None):
         """
-        Custom implementation sets default root to project-specific storage root stored
-        in a custom project field on ShotGrid site called "Storage Root Name"
+        Custom implementation sets default root to project-specific storage root path stored
+        in a custom project field on ShotGrid site called "projects root"
         """
         if not project_id:
             return
@@ -20,16 +20,16 @@ class DefaultStorageRoot(Hook):
         sg_data = self.parent.shotgun.find_one(
             "Project",
             [["id", "is", project_id]],
-            ["sg_storage_root_name"],
+            ["sg_projects_root"],
         )
 
         # check if custom field was set and filled
-        if not sg_data or not sg_data.get("sg_storage_root_name"):
+        if not sg_data or not sg_data.get("sg_projects_root"):
             log.info("No project root for project configured, using global storage root.")
             return
 
         # Stores the Windows drive for the project, e.g "P:"
-        drive_root = sg_data["sg_storage_root_name"]
+        drive_root = sg_data["sg_projects_root"]
         # check if local storage exists on SG site
         local_storage = self.parent.shotgun.find(
             "LocalStorage", [['windows_path', 'is', drive_root]], ['code']
