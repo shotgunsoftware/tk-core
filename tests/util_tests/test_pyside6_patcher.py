51e95de6ae33588fbc2cf6ed05feb3b3110060d9
unknown <stacey.oue@autodesk.com>
2023-09-26 10:34:24 -0700

    Use decorator to skip if PySide6 is not available, instead of try/except block.

--- tests/util_tests/test_pyside6_patcher.py
+++ tests/util_tests/test_pyside6_patcher.py
@@ -8,42 +8,42 @@
 # agreement to the ShotGrid Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Autodesk.
 
+# import sys
+# sys.path.append("C:\\python_libs")
+# import ptvsd
+# ptvsd.enable_attach()
+# ptvsd.wait_for_attach()
+
 from tank_test.tank_test_base import setUpModule  # noqa
 from tank_test.tank_test_base import (
     TankTestBase,
+    skip_if_pyside6,
 )
 
 from tank.util import pyside6_patcher
 
-
+@skip_if_pyside6(found=False)
 class PySide6PatcherTests(TankTestBase):
     """Tests PySide6 patcher functionality."""
 
     def test_patch(self):
         """Test the PySide6Patcher patch method that patches PySide6 as PySide."""
 
-        core, gui = None, None
-        try:
-            core, gui = pyside6_patcher.PySide6Patcher.patch()
-            # Assert the core ang gui modules are created and returned
-            assert core
-            assert gui
-            # Assert QtCore attributes
-            assert core.Qt.MidButton == core.Qt.MiddleButton
-            assert core.QRegExp == core.QRegularExpression
-            # Assert QtGui attributes
-            assert gui.QApplication.desktop
-            assert gui.QAbstractButton.animateClick
-            assert gui.QSortFilterProxyModel.filterRegExp == gui.QSortFilterProxyModel.filterRegularExpression
-            assert gui.QSortFilterProxyModel.setFilterRegExp == gui.QSortFilterProxyModel.setFilterRegularExpression
-            assert gui.QDesktopWidget == gui.QScreen
-            assert gui.QFontMetrics.width == gui.QFontMetrics.horizontalAdvance
-            assert gui.QFont.setWeight == gui.QFont.setLegacyWeight
-            assert gui.QHeaderView.setResizeMode == gui.QHeaderView.setSectionResizeMode
-            assert gui.QPainter.HighQualityAntialiasing == gui.QPainter.Antialiasing
-            assert gui.QPalette.Background == gui.QPalette.Window
-        except ImportError:
-            # PySide6 not available. Until unit tests fully support PySide6, we cannot use a
-            # decorator to skip this test when PySide6 is not available
-            assert core is None
-            assert gui is None
+        core, gui = pyside6_patcher.PySide6Patcher.patch()
+        # Assert the core ang gui modules are created and returned
+        assert core
+        assert gui
+        # Assert QtCore attributes
+        assert core.Qt.MidButton == core.Qt.MiddleButton
+        assert core.QRegExp == core.QRegularExpression
+        # Assert QtGui attributes
+        assert gui.QApplication.desktop
+        assert gui.QAbstractButton.animateClick
+        assert gui.QSortFilterProxyModel.filterRegExp == gui.QSortFilterProxyModel.filterRegularExpression
+        assert gui.QSortFilterProxyModel.setFilterRegExp == gui.QSortFilterProxyModel.setFilterRegularExpression
+        assert gui.QDesktopWidget == gui.QScreen
+        assert gui.QFontMetrics.width == gui.QFontMetrics.horizontalAdvance
+        assert gui.QFont.setWeight == gui.QFont.setLegacyWeight
+        assert gui.QHeaderView.setResizeMode == gui.QHeaderView.setSectionResizeMode
+        assert gui.QPainter.HighQualityAntialiasing == gui.QPainter.Antialiasing
+        assert gui.QPalette.Background == gui.QPalette.Window
