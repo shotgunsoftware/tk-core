9076637506356452e54a849b9dd8ceba208035c3
Norberto Moreno Velez <norbermv.dev@gmail.com>
2023-12-26 14:07:57 -0500

    Fix PySide import.

--- python/tank/authentication/resources/build_resources.sh
+++ python/tank/authentication/resources/build_resources.sh
@@ -28,7 +28,7 @@ function build_qt {
     $1 $2 > $UI_PYTHON_PATH/$3.py
 
     # replace PySide imports with sgtk.platform.qt imports and remove line containing Created by date
-    sed -i "" -e "s/from PySide import/from .qt_abstraction import/g" -e "/# Created:/d" $UI_PYTHON_PATH/$3.py
+    sed -i "" -e "s/from PySide import/from . import/g" -e "/# Created:/d" $UI_PYTHON_PATH/$3.py
 }
 
 function build_ui {
@@ -48,6 +48,6 @@ function build_res {
 echo "building user interfaces..."
 build_ui login_dialog
 
-## build resources
+# build resources
 echo "building resources..."
 build_res resources
