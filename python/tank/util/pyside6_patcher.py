d7d7f59bc874035a68ddd2bc067b706d7be76a62
Norberto Moreno Velez <norbermv.dev@gmail.com>
2024-01-24 08:43:02 -0500

    Unwrap the signal, since we've already wrapped the entire class..

--- python/tank/util/pyside6_patcher.py
+++ python/tank/util/pyside6_patcher.py
@@ -404,8 +404,7 @@ class PySide6Patcher(PySide2Patcher):
                     state = QtCore.Qt.CheckState(state)
                 self.stateChanged.emit(state)
 
-        QtGui.QCheckBox = QCheckBox_stateChanged
-        QtGui.QCheckBox.stateChanged = QCheckBox_stateChanged.stateChanged
+        QtGui.QCheckBox = _QCheckBox
 
     @classmethod
     def _patch_QCoreApplication_flush(cls, QtCore):
