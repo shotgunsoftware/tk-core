526a3fc8b7bc35ee233b3433d83c3364d35f48e4
Carlos Villavicencio <carlos.villavicencio@autodesk.com>
2024-02-05 16:07:33 -0500

    Update unit test

--- tests/authentication_tests/test_interactive_authentication.py
+++ tests/authentication_tests/test_interactive_authentication.py
@@ -1361,7 +1361,7 @@ class LoadInformationInfoTests(ShotgunTestBase):
                 self.assertEqual(site_i._url, None)
                 self.assertEqual(site_i._infos, {})
                 self.assertEqual(len(cm.output), 1)
-                self.assertIn("Infos for site 'https' not in cache or expired", cm.output[0])
+                self.assertIn("Invalid ShotGrid URL https", cm.output[0])
 
     def test_reload_wrong_site(self, *unused_mocks):
         with self.assertLogs("sgtk.core.authentication.site_info", level="DEBUG") as cm:
