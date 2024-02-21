b9a7a384f0332a9b6a4a04bc6fbb33d79c421773
Julien Langlois <16244608+julien-lang@users.noreply.github.com>
2024-02-20 17:28:37 -0800

    SG-34358 more logs and helpers (#958)
    * Provide more debug logs when an ASL exception is raised
    
    * Add special case for redirection
    
    * Add log for proxy
    
    * Add missing code in main

--- python/tank/authentication/app_session_launcher.py
+++ python/tank/authentication/app_session_launcher.py
@@ -38,6 +38,27 @@ class AuthenticationError(errors.AuthenticationError):
         self.payload = payload
         self.parent_exception = parent_exception
 
+    def format(self):
+        """
+        Provide a STR format of all given parameters
+        """
+
+        info = []
+        if self.asl_errno:
+            info.append("errno: {}".format(self.asl_errno))
+
+        if self.payload:
+            info.append("payload: {}".format(self.payload))
+
+        if self.parent_exception:
+            info.append("parent: {}".format(self.parent_exception))
+
+        message = str(self)
+        if info:
+            message += " ({})".format("; ".join(info))
+
+        return message
+
 
 def process(
     sg_url,
@@ -60,6 +81,13 @@ def process(
     if http_proxy:
         proxy_addr = _build_proxy_addr(http_proxy)
         sg_url_parsed = urllib.parse.urlparse(sg_url)
+
+        logger.debug(
+            "Set HTTP Proxy handler for {scheme} to {url}".format(
+                url=proxy_addr,
+                scheme=sg_url_parsed.scheme,
+            )
+        )
 
         url_handlers.append(
             urllib.request.ProxyHandler(
@@ -207,6 +235,20 @@ def process(
                 parent_exception=getattr(response, "exception", None),
             )
 
+        elif response_code_major == 3:
+            location = response.headers.get("location", None)
+
+            logger.debug("Request redirected: http code: {code}; redirect to: {location}".format(
+                code=response.code,
+                location=location,
+            ))
+
+            raise AuthenticationError(
+                "Request redirected",
+                payload="HTTP Redirect to {}".format(location),
+                parent_exception=getattr(response, "exception", None),
+            )
+
         elif response_code_major == 4:
             logger.debug(
                 "HTTP response {code}: {data}".format(
@@ -227,6 +269,9 @@ def process(
             )
 
         elif response.code != http_client.OK:
+            logger.debug("Request denied: http code is: {code}".format(
+                code=response.code,
+            ))
             raise AuthenticationError(
                 "Request denied",
                 payload=getattr(response, "json", response),
@@ -440,16 +485,28 @@ def build_user_agent():
 
 if __name__ == "__main__":
     import argparse
+    import logging
     import webbrowser
+
+    lh = logging.StreamHandler()
+    lh.setLevel(logging.DEBUG)
+    lh.setFormatter(logging.Formatter(
+        "%(asctime)s - %(levelname)s - %(message)s",
+    ))
+
+    logger.addHandler(lh)
+    print()
 
     parser = argparse.ArgumentParser()
     parser.add_argument("sg_url", help="Provide a ShotGrid URL")
+    parser.add_argument("--http-proxy", "-p", help="Set a proxy URL")
     args = parser.parse_args()
 
     result = process(
         args.sg_url,
         product="Test Script",
         browser_open_callback=lambda u: webbrowser.open(u),
+        http_proxy=args.http_proxy,
     )
     if not result:
         print("The web authentication failed. Please try again.")
