ae40e850bc37c925dd6fc70a122aa5da40bd8cf0
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-05-28 10:59:47 -0500

    comments fixed

--- python/tank/authentication/resources/build_resources.sh
+++ python/tank/authentication/resources/build_resources.sh
@@ -13,19 +13,19 @@ set -e
 
 # The path to output all built .py files to:
 UI_PYTHON_PATH=../ui
-if [ -z "${PYTHON_BASE}" ]; then
-    PYTHON_BASE="/Applications/Shotgun.app/Contents/Resources/Python"
-fi
 
 # Helper functions to build UI files
 function build_qt {
     # compile ui to python
     echo "$1 $2 > $UI_PYTHON_PATH/$3.py"
     $1 $2 > $UI_PYTHON_PATH/$3.py
-    # replace PySide2 imports with local imports and remove line containing Created by date
-    sed -i"" -E "/^from PySide2.QtWidgets(\s.*)?$/d; /^\s*$/d" $UI_PYTHON_PATH/$3.py
-    sed -i"" -E "s/^(from PySide.\.)(\w*)(.*)$/from .qt_abstraction import \2\nfor name, cls in \2.__dict__.items():\n    if isinstance(cls, type): globals()[name] = cls\n/g" $UI_PYTHON_PATH/$3.py
-    sed -i"" -E "s/from PySide2 import/from .qt_abstraction import/g" $UI_PYTHON_PATH/$3.py
+    # replace PySide2 imports with .qt_abstraction and then added code to set
+    # global variables for each new import.
+    sed -i"" -E \
+        -e "/^from PySide2.QtWidgets(\s.*)?$/d; /^\s*$/d" \
+        -e "s/^(from PySide.\.)(\w*)(.*)$/from .qt_abstraction import \2\nfor name, cls in \2.__dict__.items():\n    if isinstance(cls, type): globals()[name] = cls\n/g" \
+        -e "s/from PySide2 import/from .qt_abstraction import/g" \
+        $UI_PYTHON_PATH/$3.py
 }
 
 function build_ui {
