508eec9b13974dc2ef2864013ed13c2a04a43a88
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-07-08 11:49:54 -0500

    black formatting applied

--- build_resources.py
+++ build_resources.py
@@ -5,29 +5,46 @@ import os
 
 # Set the Python environment variable. If 'PYTHONPATH' is not set, use the default path "/usr/bin/"
 # or you can override the Python environment path for a specific virtual environment
-PYTHON_ENV = os.getenv('PYTHONPATH', "/usr/bin/")
+PYTHON_ENV = os.getenv("PYTHONPATH", "/usr/bin/")
 
 # List of commands to build QT resources
 COMMANDS = [
     [
         "tk-build-qt-resources",
-        "-p", PYTHON_ENV,
-        "-py", "python/tank/authentication/ui",
-        "-q", "python/tank/authentication/resources",
-        "-uf", "login_dialog",
-        "-rf", "resources",
-        "-i", ".qt_abstraction"
+        "-p",
+        PYTHON_ENV,
+        "-py",
+        "python/tank/authentication/ui",
+        "-q",
+        "python/tank/authentication/resources",
+        "-uf",
+        "login_dialog",
+        "-rf",
+        "resources",
+        "-i",
+        ".qt_abstraction",
     ],
     [
         "tk-build-qt-resources",
-        "-p", PYTHON_ENV,
-        "-py", "python/tank/platform/qt",
-        "-q", "python/tank/platform/qt",
-        "-uf", "tank_dialog", "item", "busy_dialog",
-        "-ufn", "ui_tank_dialog", "ui_item", "ui_busy_dialog",
-        "-rf", "resources",
-        "-i", "."
-    ]
+        "-p",
+        PYTHON_ENV,
+        "-py",
+        "python/tank/platform/qt",
+        "-q",
+        "python/tank/platform/qt",
+        "-uf",
+        "tank_dialog",
+        "item",
+        "busy_dialog",
+        "-ufn",
+        "ui_tank_dialog",
+        "ui_item",
+        "ui_busy_dialog",
+        "-rf",
+        "resources",
+        "-i",
+        ".",
+    ],
 ]
 
 
@@ -36,21 +53,28 @@ def install_package(package):
         subprocess.run([sys.executable, "-m", "pip", "install", package], check=True)
         print(f"Package {package} installed successfully.")
     except subprocess.CalledProcessError as e:
-        print(f"An error occurred while installing {package}.")
-        print("Error output:", e.stderr)
+        print(f"An error occurred while installing {package}. Error output: {e.stderr}")
 
 
 def run_commands():
     for command in COMMANDS:
         try:
-            result = subprocess.run(command, check=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE, text=True)
-            print("Command executed successfully.")
-            print("Output:", result.stdout) 
+            result = subprocess.run(
+                command,
+                check=True,
+                stdout=subprocess.PIPE,
+                stderr=subprocess.PIPE,
+                text=True,
+            )
+            print(f"Command executed successfully. Output:, {result.stdout}")
         except subprocess.CalledProcessError as e:
-            print("An error occurred while executing the command.")
-            print("Error output:", e.stderr)
+            print(
+                f"An error occurred while executing the command. Error output: {e.stderr}"
+            )
 
 
 if __name__ == "__main__":
-    install_package("git+https://github.com/shotgunsoftware/tk-toolchain.git#egg=tk-toolchain")
+    install_package(
+        "git+https://github.com/shotgunsoftware/tk-toolchain.git#egg=tk-toolchain"
+    )
     run_commands()
