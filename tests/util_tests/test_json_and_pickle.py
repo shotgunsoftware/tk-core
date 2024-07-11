ef843e51495bf10f595b90628392524c6b49a9ae
Eduardo Chauca <eduardo.chauca@autodesk.com>
2024-07-11 10:28:06 -0500

    unused lines tests

--- tests/util_tests/test_json_and_pickle.py
+++ tests/util_tests/test_json_and_pickle.py
@@ -268,13 +268,6 @@ class PickleTests(Impl.SerializationTests):
         "pickled_saved_with_python_2_protocol_0.pickle",
     )
 
-    def test_reload_protocol_2_pickle(self):
-        with open(self.protocol_2_file_location, "rb") as fh:
-            self.assertEqual(self.load(fh), self.dict_with_unicode)
-
-        with open(self.protocol_2_file_location, "rb") as fh:
-            self.assertEqual(self.loads(fh.read()), self.dict_with_unicode)
-
 
 if __name__ == "__main__":
     # Generates the test files. From the folder this file is in run
