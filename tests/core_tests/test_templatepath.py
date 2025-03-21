c04d8617b2316f71d98fbc7582a9421e4062acb9
Hector Clement <clement.hector@gmail.com>
2025-03-21 17:26:36 +0100

    Fix formatting in test_templatepath.py for consistency

--- tests/core_tests/test_templatepath.py
+++ tests/core_tests/test_templatepath.py
@@ -1198,6 +1198,7 @@ class TestParent(TestTemplatePath):
         self.assertEqual(parent_expected_definitions, parent._definitions)
         self.assertEqual(parent_expected__repr_def, parent._repr_def)
 
+
 class TestDirname(TestTemplatePath):
     def test_dirname_with_word(self):
         definition = "shots/{Sequence}/{Shot}/{Step}/work"
@@ -1215,7 +1216,7 @@ class TestDirname(TestTemplatePath):
         self.assertEqual("shots/{Sequence}/{Shot}", template.dirname)
 
     def test_dirname_with_optional_word_and_key(self):
-        definition =  "shots/{Sequence}/{Shot}/[abc_{Step}]"
+        definition = "shots/{Sequence}/{Shot}/[abc_{Step}]"
         template = TemplatePath(definition, {}, root_path=self.project_root)
         self.assertEqual("", template.dirname)
 
