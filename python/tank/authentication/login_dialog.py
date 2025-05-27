shotgunsoftware/tk-core

commit e266c6e2a47212934c2ebf9eab3fb4ff8e6f769a

SG-38470 Prevent QtImporter from raising an exception if QtWebEngine is not available (#1012)
* Fixup import mock module from unittest

* Remove useless code introduced in #925

* Add missing documentation about SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT

* Add unit test for SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT

* Fixup respect SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT behaviour

* Apply review

* Fixup _import_module_by_name wrongly returned PySide6 base module

Once requesting a PySide6 mode that does not exists, the function
was returning the base PySide6 module instead of None

python/tank/util/qt_importer.py
@@ -87,13 +87,6 @@ class QtImporter(object):
         return self._modules["QtWebEngineWidgets"] if self._modules else None
 
     @property
-    def QtWebEngineCore(self):
-        """
-        :returns: QtWebEngineCore module, if available.
-        """
-        return self._modules["QtWebEngineCore"] if self._modules else None
-
-    @property
     def binding(self):
         """
         :returns: The PySide* or PyQt* root module.
@@ -155,14 +148,13 @@ class QtImporter(object):
 
         :returns: The module loaded, or None if it could not be loaded.
         """
-        module = None
+
         try:
             module = __import__(parent_module_name, globals(), locals(), [module_name])
-            module = getattr(module, module_name)
+            return getattr(module, module_name)
         except Exception as e:
             logger.debug("Unable to import module '%s': %s", module_name, e)
-            pass
-        return module
+
 
     def _import_pyside2(self):
         """
@@ -258,9 +250,12 @@ class QtImporter(object):
         QtCore, QtGui = PySide2Patcher.patch(QtCore, QtGui, QtWidgets, PySide2)
         QtNetwork = self._import_module_by_name("PySide2", "QtNetwork")
         QtWebKit = self._import_module_by_name("PySide2.QtWebKitWidgets", "QtWebKit")
-        QtWebEngineWidgets = self._import_module_by_name(
-            "PySide2.QtWebEngineWidgets", "QtWebEngineWidgets"
-        )
+
+        QtWebEngineWidgets = None
+        if "SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT" not in os.environ:
+            QtWebEngineWidgets = self._import_module_by_name(
+                "PySide2.QtWebEngineWidgets", "QtWebEngineWidgets"
+            )
 
         return (
             "PySide2",
@@ -288,7 +283,18 @@ class QtImporter(object):
         import shiboken6
         from .pyside6_patcher import PySide6Patcher
 
-        QtCore, QtGui, QtWebEngineWidgets = PySide6Patcher.patch()
+        QtWebEngineWidgets, QtWebEngineCore = None, None
+        if "SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT" not in os.environ:
+            QtWebEngineWidgets = self._import_module_by_name(
+                "PySide6", "QtWebEngineWidgets"
+            )
+            QtWebEngineCore = self._import_module_by_name("PySide6", "QtWebEngineCore")
+
+        QtCore, QtGui, QtWebEngineWidgets = PySide6Patcher.patch(
+            QtWebEngineWidgets,
+            QtWebEngineCore,
+        )
+
         QtNetwork = self._import_module_by_name("PySide6", "QtNetwork")
         QtWebKit = self._import_module_by_name("PySide6.QtWebKitWidgets", "QtWebKit")
 
@@ -318,6 +324,12 @@ class QtImporter(object):
         import shiboken6
 
         sub_modules = pkgutil.iter_modules(PySide6.__path__)
+
+        if "SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT" in os.environ:
+            sub_modules = [
+                m for m in sub_modules if not m.name.startswith("QtWebEngine")
+            ]
+
         modules_dict = {}
         # Add shiboken6 to the modules dict
         modules_dict["shiboken"] = shiboken6

python/tank/util/pyside6_patcher.py
@@ -443,7 +443,11 @@ class PySide6Patcher(PySide2Patcher):
         QtCore.QCoreApplication.flush = flush
 
     @classmethod
-    def patch(cls):
+    def patch(
+            cls,
+            QtWebEngineWidgets,
+            QtWebEngineCore,
+    ):
         """
         Patch the PySide6 modules, classes and function to conform to the PySide interface.
 
@@ -465,14 +469,17 @@ class PySide6Patcher(PySide2Patcher):
             QtGui,
             QtWidgets,
             QtOpenGL,
-            QtWebEngineWidgets,
-            QtWebEngineCore,
         )
 
         # First create new modules to act as the PySide modules
         qt_core_shim = imp.new_module("PySide.QtCore")
         qt_gui_shim = imp.new_module("PySide.QtGui")
-        qt_web_engine_widgets_shim = imp.new_module("PySide.QtWebEnginWidgets")
+
+        qt_web_engine_widgets_shim = None
+        if QtWebEngineWidgets:
+            qt_web_engine_widgets_shim = imp.new_module(
+                "PySide.QtWebEnginWidgets",
+            )
 
         # Move everything from QtGui and QtWidgets to the QtGui shim since they belonged there
         # in PySide.
@@ -491,8 +498,13 @@ class PySide6Patcher(PySide2Patcher):
         # https://doc.qt.io/qt-6/gui-changes-qt6.html#opengl-classes
         cls._move_attributes(qt_gui_shim, QtOpenGL, cls._opengl_to_gui)
 
-        # Move everything from QtWebEngineWidgets to the QtWebEngineWidgets shim
-        cls._move_attributes(qt_web_engine_widgets_shim, QtWebEngineWidgets, dir(QtWebEngineWidgets))
+        if qt_web_engine_widgets_shim:
+            # Move everything from QtWebEngineWidgets to the QtWebEngineWidgets shim
+            cls._move_attributes(
+                qt_web_engine_widgets_shim,
+                QtWebEngineWidgets,
+                dir(QtWebEngineWidgets),
+            )
 
         # Patch classes from PySide6 to PySide, as done for PySide2 (these will call the
         # PySide2 patcher methods.)
@@ -582,9 +594,9 @@ class PySide6Patcher(PySide2Patcher):
         # https://doc.qt.io/qt-5/qpalette.html#ColorRole-enum
         qt_gui_shim.QPalette.Background = qt_gui_shim.QPalette.Window
 
-        # QtWwebEngineWidgets
-        # ----------------------------------------------------------------------
-        qt_web_engine_widgets_shim.QWebEnginePage = QtWebEngineCore.QWebEnginePage
-        qt_web_engine_widgets_shim.QWebEngineProfile = QtWebEngineCore.QWebEngineProfile
+        if qt_web_engine_widgets_shim:
+            # QtWwebEngineWidgets
+            qt_web_engine_widgets_shim.QWebEnginePage = QtWebEngineCore.QWebEnginePage
+            qt_web_engine_widgets_shim.QWebEngineProfile = QtWebEngineCore.QWebEngineProfile
 
         return qt_core_shim, qt_gui_shim, qt_web_engine_widgets_shim

tests/authentication_tests/test_web_login.py
@@ -30,6 +30,9 @@ class WebLoginTests(ShotgunTestBase):
     def test_web_login(self):
         from tank.authentication.ui import qt_abstraction
 
+        if not qt_abstraction.QtWebEngineWidgets:
+            self.skipTest("This tests requires QtWebEngineWidgets")
+
         if qt_abstraction.QtGui.QApplication.instance() is None:
             self._app = qt_abstraction.QtGui.QApplication([])
 
@@ -46,10 +49,3 @@ class WebLoginTests(ShotgunTestBase):
 
         # coverage
         obj._core._view.page().createWindow(None)
-
-    def test_qt6_coverage(self):
-        from tank.util import qt_importer
-
-        qt_imp = qt_importer.QtImporter()
-        with self.assertRaises(KeyError):
-            qt_imp.QtWebEngineCore

tests/util_tests/test_pyside6_patcher.py
@@ -24,7 +24,7 @@ class PySide6PatcherTests(TankTestBase):
     def test_patch(self):
         """Test the PySide6Patcher patch method that patches PySide6 as PySide."""
 
-        core, gui, _ = pyside6_patcher.PySide6Patcher.patch()
+        core, gui, _ = pyside6_patcher.PySide6Patcher.patch(None, None)
         # Assert the core ang gui modules are created and returned
         assert core
         assert gui

tests/util_tests/test_qt_importer.py
@@ -8,6 +8,8 @@
 # agreement to the ShotGrid Pipeline Toolkit Source Code License. All rights
 # not expressly granted therein are reserved by Autodesk.
 
+import types
+import unittest.mock
 
 from tank_test.tank_test_base import setUpModule  # noqa
 from tank_test.tank_test_base import (
@@ -37,8 +39,16 @@ class QtImporterTests(TankTestBase):
         assert qt.QtNetwork
         assert qt.shiboken
         assert qt.shiboken.__name__ == "shiboken2"
-        # We need one or the other
-        assert qt.QtWebKit or qt.QtWebEngineWidgets
+
+        assert (
+            qt.QtWebKit is None
+            or isinstance(qt.QtWebKit, types.ModuleType)
+        )
+
+        assert (
+            qt.QtWebEngineWidgets is None
+            or isinstance(qt.QtWebEngineWidgets, types.ModuleType)
+        )
 
         # Expect PySide2 as the binding
         assert qt.binding_name == "PySide2"
@@ -138,3 +148,69 @@ class QtImporterTests(TankTestBase):
         assert qt.base
         assert qt.base["__name__"] is qt.binding_name
         assert qt.base["__version__"] is qt.binding_version
+
+    @skip_if_pyside2(found=False)
+    @skip_if_pyside6(found=True)
+    @unittest.mock.patch(
+        # Ensure the QtWebEngineWidgets module is present
+        "PySide2.QtWebEngineWidgets"
+    )
+    @unittest.mock.patch.dict(
+        # Set the SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT variable
+        "os.environ",
+        {"SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT": "1"}
+    )
+    def test_skip_webengine_qt5(self, *mocks):
+        # Test default Qt interface (Qt4)
+        qt = qt_importer.QtImporter()
+
+        # Check that the qt modules were initialized
+        assert qt.QtCore
+
+        # Ensure the QtWebEngineWidgets module is NOT imported
+        assert qt.QtWebEngineWidgets is None
+
+        # Repeat the test with Qt5 interface
+        qt = qt_importer.QtImporter(
+            interface_version_requested=qt_importer.QtImporter.QT5,
+        )
+
+        # Check that the qt modules were initialized
+        assert qt.QtCore
+
+        # Ensure the QtWebEngineWidgets module is NOT imported
+        with self.assertRaises(KeyError):
+            qt.QtWebEngineWidgets
+
+    @skip_if_pyside6(found=False)
+    @skip_if_pyside2(found=True)
+    @unittest.mock.patch(
+        # Ensure the QtWebEngineWidgets module is present
+        "PySide6.QtWebEngineWidgets"
+    )
+    @unittest.mock.patch.dict(
+        # Set the SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT variable
+        "os.environ",
+        {"SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT": "1"}
+    )
+    def test_skip_webengine_qt6(self, *mocks):
+        # Test default Qt interface (Qt4)
+        qt = qt_importer.QtImporter()
+
+        # Check that the qt modules were initialized
+        assert qt.QtCore
+
+        # Ensure the QtWebEngineWidgets module is NOT imported
+        assert qt.QtWebEngineWidgets is None
+
+        # Repeat the test with Qt6 interface
+        qt = qt_importer.QtImporter(
+            interface_version_requested=qt_importer.QtImporter.QT6,
+        )
+
+        # Check that the qt modules were initialized
+        assert qt.QtCore
+
+        # Ensure the QtWebEngineWidgets module is NOT imported
+        with self.assertRaises(KeyError):
+            qt.QtWebEngineWidgets

commit 435433005be15eb9d19fe83cadc3069ba77f5bb2

SG-38945 web login not working pyside2 (#1021)
* Hide the web login option when QtWebEngineWidgets not available

* Fixup regression introduced by #1012

python/tank/authentication/login_dialog.py
@@ -481,24 +481,27 @@ class LoginDialog(QtGui.QDialog):
         # We only update the GUI if there was a change between to mode we
         # are showing and what was detected on the potential target site.
 
-        # With a SSO site, we have no choice but to use the web to login.
-        can_use_web = self.site_info.sso_enabled
+        can_use_web = self._sso_saml2 is not None
         can_use_asl = self.site_info.app_session_launcher_enabled
 
-        # The user may decide to force the use of the old dialog:
-        # - due to graphical issues with Qt and its WebEngine
-        # - they need to use the legacy login / passphrase to use a PAT with
-        #   Autodesk Identity authentication
-        if os.environ.get("SGTK_FORCE_STANDARD_LOGIN_DIALOG"):
-            logger.info("Using the standard login dialog with the Flow Production Tracking")
-        else:
-            if _is_running_in_desktop():
-                can_use_web = can_use_web or self.site_info.autodesk_identity_enabled
+        if can_use_web:
+            # With a SSO site, we have no choice but to use the web to login.
+            can_use_web = self.site_info.sso_enabled
 
-            # If we have full support for Web-based login, or if we enable it in our
-            # environment, use the Unified Login Flow for all authentication modes.
-            if get_shotgun_authenticator_support_web_login():
-                can_use_web = can_use_web or self.site_info.unified_login_flow_enabled
+            # The user may decide to force the use of the old dialog:
+            # - due to graphical issues with Qt and its WebEngine
+            # - they need to use the legacy login / passphrase to use a PAT with
+            #   Autodesk Identity authentication
+            if os.environ.get("SGTK_FORCE_STANDARD_LOGIN_DIALOG"):
+                logger.info("Using the standard login dialog with the Flow Production Tracking")
+            else:
+                if _is_running_in_desktop():
+                    can_use_web = can_use_web or self.site_info.autodesk_identity_enabled
+
+                # If we have full support for Web-based login, or if we enable it in our
+                # environment, use the Unified Login Flow for all authentication modes.
+                if get_shotgun_authenticator_support_web_login():
+                    can_use_web = can_use_web or self.site_info.unified_login_flow_enabled
 
         if method_selected:
             # Selecting requested mode (credentials, qt_web_login or app_session_launcher)

python/tank/util/qt_importer.py
@@ -254,7 +254,7 @@ class QtImporter(object):
         QtWebEngineWidgets = None
         if "SHOTGUN_SKIP_QTWEBENGINEWIDGETS_IMPORT" not in os.environ:
             QtWebEngineWidgets = self._import_module_by_name(
-                "PySide2.QtWebEngineWidgets", "QtWebEngineWidgets"
+                "PySide2", "QtWebEngineWidgets"
             )
 
         return (