61237c5ac4a5c39caa1ea3b0be76ef8ba9c7e796
Julien Langlois <julien.langlois@autodesk.com>
2025-07-08 14:35:58 +0200

    Test fixup CI

--- tests/util_tests/test_metrics.py
+++ tests/util_tests/test_metrics.py
@@ -234,7 +234,7 @@ class TestMetricsDispatchWorkerThread(TankTestBase):
         self._create_engine()
 
         # Patch & Mock the `urlopen` method
-        self._urlopen_mock = mock.patch("tank_vendor.six.moves.urllib.request.urlopen")
+        self._urlopen_mock = mock.patch("urllib.request.urlopen")
         self._mocked_method = self._urlopen_mock.start()
 
     def setUp(self):
@@ -791,7 +791,7 @@ class TestMetricsDispatchWorkerThread(TankTestBase):
         self._urlopen_mock.stop()
         self._urlopen_mock = None
         self._urlopen_mock = mock.patch(
-            "tank_vendor.six.moves.urllib.request.urlopen",
+            "urllib.request.urlopen",
             side_effect=TestMetricsDispatchWorkerThread._mocked_urlopen_for_test_maximum_batch_size,
         )
         self._mocked_method = self._urlopen_mock.start()
@@ -855,7 +855,7 @@ class TestMetricsDispatchWorkerThread(TankTestBase):
         self._urlopen_mock.stop()
         self._urlopen_mock = None
         self._urlopen_mock = mock.patch(
-            "tank_vendor.six.moves.urllib.request.urlopen",
+            "urllib.request.urlopen",
             side_effect=TestMetricsDispatchWorkerThread._mocked_urlopen_for_test_maximum_batch_size,
         )
         self._mocked_method = self._urlopen_mock.start()
