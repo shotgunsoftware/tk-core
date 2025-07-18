82397602aa7d63028e2d4c3d6e5fd4e4b59e9e71
Julien Langlois <julien.langlois@autodesk.com>
2025-07-18 11:32:35 +0200

    fixup! recommendations added

--- python/tank_vendor/sgutils.py
+++ python/tank_vendor/sgutils.py
@@ -1,31 +1,31 @@
 """
------------------------------------------------------------------------------
-Copyright (c) 2009-2024, Shotgun Software Inc.
+ -----------------------------------------------------------------------------
+ Copyright (c) 2009-2024, Shotgun Software Inc.
 
-Redistribution and use in source and binary forms, with or without
-modification, are permitted provided that the following conditions are met:
+ Redistribution and use in source and binary forms, with or without
+ modification, are permitted provided that the following conditions are met:
 
- - Redistributions of source code must retain the above copyright notice, this
-   list of conditions and the following disclaimer.
+  - Redistributions of source code must retain the above copyright notice, this
+    list of conditions and the following disclaimer.
 
- - Redistributions in binary form must reproduce the above copyright notice,
-   this list of conditions and the following disclaimer in the documentation
-   and/or other materials provided with the distribution.
+  - Redistributions in binary form must reproduce the above copyright notice,
+    this list of conditions and the following disclaimer in the documentation
+    and/or other materials provided with the distribution.
 
- - Neither the name of the Shotgun Software Inc nor the names of its
-   contributors may be used to endorse or promote products derived from this
-   software without specific prior written permission.
+  - Neither the name of the Shotgun Software Inc nor the names of its
+    contributors may be used to endorse or promote products derived from this
+    software without specific prior written permission.
 
-THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
-AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
-IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
-DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
-FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
-DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
-SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
-CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
-OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
-OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 """
 
 import warnings
@@ -39,8 +39,11 @@ def ensure_binary(s, encoding="utf-8", errors="strict"):
       - `bytes` -> `bytes`
     """
     warnings.warn(
-        "The 'ensure_binary' method is deprecated and will be removed by the end of December 2025. "
-        "Use the built-in str.encode() method instead, or ensure your data is already in bytes.",
+        "The 'sgutils' module, including the 'ensure_binary', 'ensure_str', "
+        "and 'ensure_text' methods, is beig deprecated and will be removed by "
+        "the end of January 2026. "
+        "Use the built-in str.encode() and/or bytes.decode() methods instead "
+        "accordingly.",
         DeprecationWarning,
         stacklevel=2,
     )
@@ -60,8 +63,11 @@ def ensure_str(s, encoding="utf-8", errors="strict"):
     - `bytes` -> decoded to `str`
     """
     warnings.warn(
-        "The 'ensure_str' method is deprecated and will be removed by the end of December 2025. "
-        "Use the built-in bytes.decode() method instead, or ensure your data is already a string.",
+        "The 'sgutils' module, including the 'ensure_binary', 'ensure_str', "
+        "and 'ensure_text' methods, is beig deprecated and will be removed by "
+        "the end of January 2026. "
+        "Use the built-in str.encode() and/or bytes.decode() methods instead "
+        "accordingly.",
         DeprecationWarning,
         stacklevel=2,
     )
@@ -82,8 +88,11 @@ def ensure_text(*args, **kwargs):
     This function is also deprecated and will be removed in a future version.
     """
     warnings.warn(
-        "The 'ensure_text' method is deprecated and will be removed by the end of December 2025. "
-        "Use the built-in bytes.decode() method instead, or ensure your data is already a string.",
+        "The 'sgutils' module, including the 'ensure_binary', 'ensure_str', "
+        "and 'ensure_text' methods, is beig deprecated and will be removed by "
+        "the end of January 2026. "
+        "Use the built-in str.encode() and/or bytes.decode() methods instead "
+        "accordingly.",
         DeprecationWarning,
         stacklevel=2,
     )
