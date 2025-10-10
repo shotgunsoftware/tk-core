3bfbfd989889ddfce94400cfd330be91e17bec03
Julien Langlois <julien.langlois@autodesk.com>
2025-10-10 15:40:00 -0700

    Add retro compatibility with tk-nuke <=v0.16.0

--- python/tank/authentication/sso_saml2/core/__init__.py
+++ python/tank/authentication/sso_saml2/core/__init__.py
@@ -10,3 +10,34 @@
 """
 This module contains files which are shared between RV and Toolkit.
 """
+
+def __getattr__(name):
+    """
+    TODO retro compat with tk-nuke .... TODO
+    SG-40049
+
+    To remove when we drop support for tk-nuke vX.Y.Z TODO FIXME
+    """
+
+    try:
+        return object.__getattribute__(__name__, name)
+    except AttributeError:
+        if name != "sso_saml2_core":
+            raise
+
+        import importlib
+        import inspect
+        import warnings
+
+        frame = inspect.currentframe().f_back
+        caller_mod = frame.f_globals.get("__name__", "")
+        is_import = caller_mod.startswith("importlib.") or caller_mod == "builtins"
+
+        if not is_import:
+            warnings.warn(
+                f"Accessing '{__name__}.{name}' directly is deprecated. Please import '{__name__}.{name}' explicitly.",
+                DeprecationWarning,
+                stacklevel=2,
+            )
+
+        return importlib.import_module(f"{__name__}.{name}")
