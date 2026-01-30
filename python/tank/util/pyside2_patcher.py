fbfbebd0df1131ee0aa718cfb7b0dd7ad50df22f
Eduardo Chauca <166560435+eduardoChaucaGallegos@users.noreply.github.com>
2026-01-30 11:56:28 -0500

    SG-39031 removed for outdated reference (#1079)

--- python/tank/util/pyside2_patcher.py
+++ python/tank/util/pyside2_patcher.py
@@ -88,15 +88,13 @@ class PySide2Patcher(object):
         wrapper_class.DefaultCodec = wrapper_class.CodecForTr
 
         @staticmethod
-        def translate(context, source_text, disambiguation=None, encoding=None, n=None):
-            # In PySide2, the encoding argument has been deprecated, so we don't
-            # pass it down. n is still supported however, but has always been optional
-            # in PySide. So if n has been set to something, let's pass it down,
-            # otherwise Qt5 has a default value for it, so we'll use that instead.
-            if n is not None:
-                return original_class.translate(context, source_text, disambiguation, n)
-            else:
-                return original_class.translate(context, source_text, disambiguation)
+        def translate(*args, **kwargs):
+            if "encoding" in kwargs:
+                # The encoding argument has been deprecated and no longer exist in
+                # PySide2 so we don't pass it down.
+                del kwargs["encoding"]
+
+            return original_class.translate(*args, **kwargs)
 
         wrapper_class.translate = translate
 
