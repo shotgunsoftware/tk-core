fa4e3825cc41366c77e4284cde52c9ab6c316093
Yungsiow Yang <yungsiow.yang@autodesk.com>
2026-06-15 12:13:51 -0400

    Added some common utilities
    - BaseInputs base class
    - create_components_for_publish
    - identify_component

--- python/tank/flowam/utils.py
+++ python/tank/flowam/utils.py
@@ -12,7 +12,10 @@
 
 from __future__ import annotations  # needed for python 3.9 support
 
+import fileseq
 import os
+import re
+from dataclasses import dataclass, asdict
 
 from tank import LogManager
 from tank.authentication import flow_auth
@@ -20,12 +23,52 @@ from tank.pipelineconfig import PipelineConfiguration
 from tank.util import yaml_cache
 from tank_vendor.flow_integration_sdk import globals, schema, storage
 from tank_vendor.flow_integration_sdk.exceptions import FlowError
-from tank_vendor.flow_integration_sdk.objects import FlowProject
+from tank_vendor.flow_integration_sdk.publish import (
+    ComponentSpec,
+    CommentComponentSpec,
+    SourceComponentSpec,
+    ThumbnailComponentSpec,
+    TypeComponentSpec,
+    FileSeqComponentSpec,
+)
+from tank_vendor.flow_integration_sdk.objects import FlowProject, FlowRevision
+from tank_vendor.flow_integration_sdk.utils import (
+    cleanpath,
+    trace,
+)
 
 from .constants import FLOW_SCHEMA_CONFIG_PATH
 
 
 logger = LogManager.get_logger(__name__)
+
+
+@dataclass
+class BaseInputs:
+    def format_str(self, tab: int = 0):
+        s = ""
+        for prop, value in self.__dict__.items():
+            s += "\t" * tab + f"{prop} = {value}\n"
+        return s.rstrip()
+
+    def validate(self):
+        """Do validation of input values."""
+        pass
+
+    def asdict(self):
+        return asdict(self)
+
+    def log_intro(self, intro_message: str):
+        """Log an intro message along with the values of
+        input object.
+        """
+
+        msg = f"""
+    ---------------------------------------
+    {intro_message} with inputs:
+{self.format_str(tab=3)}
+    --------------------------------------"""
+        logger.info(msg)
 
 
 def get_config_flow_settings(pipeline_config: PipelineConfiguration) -> dict:
@@ -120,3 +163,172 @@ def init_flow(pipeline_config: PipelineConfiguration, flow_project_id: str):
     storage.set_storage_root(flow_storage_root, create_dir=True)
 
     logger.info("Initialization complete!")
+
+
+@trace
+def identify_component(file_path: str) -> dict | None:
+    """Given a file path, determine if it belongs to an asset, and
+    return information identifying the exact component blob that the path
+    is associated with.
+
+    NOTE: Only paths within primary storage (NFS cache) can be identified.
+    All other paths, including sandbox paths, will return None.
+
+    Args:
+        file_path: Absolute path to a file.
+        ignore_root: If True, root directory does not need to map to
+                     current configured roots. It is recommended to keep
+                     this to True if not trying validate full path for optimal
+                     performance.
+
+    Returns:
+        Dictionary with keys:
+            * asset_id -> Id of asset
+            * revision_id -> Id of revision
+            * version_id -> Id of version
+            * component_name -> Name of component
+            * blob_index -> Index into binary array of component
+
+        or None if the file path cannot be identified.
+    """
+    file_path = cleanpath(file_path)
+
+    # Absolute path pattern (expecting a root)
+    expr = r".*(?P<comp_path>/[^/]+/((r\d+)|(draft))/.+)"
+    m = re.match(expr, file_path)
+    if not m:
+        # Relative path pattern (expecting to begin with storage id)
+        expr = r"(?P<comp_path>[^/]+/((r\d+)|(draft))/.+)"
+        m = re.match(expr, file_path)
+        if not m:
+            # Not an asset path - failed test for overall asset path pattern
+            return None
+    comp_path = m.group("comp_path")
+
+    # add prepended / to match expected asset path pattern if not present
+    if not comp_path.startswith("/"):
+        comp_path = "/" + comp_path
+
+    # Parse expected pieces of asset path
+    try:
+        _, storage_id, rev_num, comp_path = comp_path.split("/", maxsplit=3)
+    except ValueError:
+        # Not an asset path - unable to parse into key path components
+        return None
+
+    # Look up asset based on storage key
+    try:
+        asset_id = storage.storage_key_to_asset_id(storage_id)
+    except FlowError:
+        # Not an asset path - storage key component does not map to an asset
+        return None
+
+    # Convert revision number to integer
+    if rev_num == "draft":
+        # Not an asset path - draft paths don't count
+        return None
+    else:
+        try:
+            rev_num = int(rev_num.strip("r"))
+        except ValueError:
+            # Not an asset path - non-int value for revision number component
+            return None
+
+    revision_id = FlowRevision.get_revision_id(asset_id, rev_num)
+    try:
+        # NOTE: Using accessor method rather than constructing new instance
+        #       because this will return a cached object if it exists
+        #       This is ok since revisions are immutable.
+        revision = FlowRevision.get_revision(revision_id)
+    except FlowError:
+        # Not an asset - revision number is out of range
+        return None
+
+    # Determine component and blob index based on component path
+    # Try and match against existing binary components on revision
+    component = blob_index = None
+    bin_comps = revision.get_binary_components()
+    if "%" in comp_path:
+        # File sequence paths will be stored as a zip file
+        comp_path, _, _ = comp_path.rsplit(".", maxsplit=2)
+        comp_path += ".zip"
+    for comp in bin_comps:
+        for i, blob in enumerate(comp.blobs):
+            if blob.path == comp_path:
+                component = comp
+                blob_index = i
+                break
+    if component is None:
+        # Not an asset path - no component blob matches file
+        return None
+
+    return {
+        "asset_id": asset_id,
+        "revision_id": revision_id,
+        "version_id": revision.version_id,
+        "component_name": component.name,
+        "blob_index": blob_index,
+    }
+
+
+@trace
+def create_components_for_publish(
+    source_paths: list[str],
+    thumbnail_path: str = "",
+    comment: str = "",
+    type_ids: list[str] | None = None,
+) -> list[ComponentSpec]:
+    """Generate the components relevant to publish a new revision.
+
+    Args:
+        source_paths: List of local paths to the source files.
+        thumbnail_path: Optional path to thumbnail file.
+        comment: Generate a comment component with given comment string.
+                 Should only be included if the publish method does not
+                 explicitly accept a `comment` parameter.
+        type_ids: A list of type ids to be converted into type components.
+                  This is only relevant if publishing a new asset direct to remote
+                  (i.e. not going through sandbox).
+    """
+    # Source component contains the source file
+    source_comp = SourceComponentSpec(*source_paths)
+    components: list[ComponentSpec] = [source_comp]
+    # Thumbnail component contains the thumbnail file
+    if thumbnail_path:
+        thumbnail_comp = ThumbnailComponentSpec(thumbnail_path)
+        components.append(thumbnail_comp)
+    # Need to add a special file sequence type component if there are
+    # multiple source files
+    if len(source_paths) > 1:
+        sequences = fileseq.findSequencesInList(source_paths)
+        if len(sequences) > 1:
+            msg = "Ambiguous file sequence provided for publish."
+            msg += " Multiple file sequences have been detected in the"
+            msg += " source path list. Please input files with common"
+            msg += " base name with frame padding (e.g. <name>.####.<ext>)."
+            raise FlowError(msg)
+        # Determine file format
+        pad_len = sequences[0].getPaddingNum(sequences[0].padding())
+        frame_expr = f"%0{pad_len}d"
+        file_format = os.path.basename(sequences[0].frame(frame_expr))
+        components.append(
+            FileSeqComponentSpec(
+                type_id=schema.get_schema_id(globals.FILE_SEQ_TYPE),
+                frame_start=sequences[0].start(),
+                frame_end=sequences[0].end(),
+                frame_set=str(sequences[0].frameSet()),
+                file_format=file_format,
+                name=globals.FILE_SEQ_COMP,
+            )
+        )
+    # Add comment if provided
+    if comment:
+        comment_comp = CommentComponentSpec(comment=comment)
+        components.append(comment_comp)
+    # Add type components if specified
+    type_ids = [] if type_ids is None else type_ids
+    for i, type_id in enumerate(type_ids):
+        # NOTE: component names must be unique!
+        type_comp = TypeComponentSpec(type_id=type_id, name=f"Type {i}")
+        components.append(type_comp)
+    return components
